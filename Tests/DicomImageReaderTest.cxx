335da3f19d0ad9f4f39b7223d45a7ec977b1cc96
Luis Ibanez <luis.ibanez@kitware.com>
2003-12-17 19:27:46 -0500

    Test for the DicomImageReader component.

---
+++ Tests/DicomImageReaderTest.cxx
@@ -0,0 +1,26 @@
+#include "itkImage.h"
+#include "DicomImageReader.h"
+
+int DicomImageReaderTest(int argc, char * argv [])
+{
+  typedef signed short   PixelType;
+
+  typedef unsigned char  VisualizationPixelType;
+
+  typedef itk::Image< PixelType, 3 >                  VolumeType;
+
+  typedef itk::Image< PixelType, 2 >                  ImageType;
+
+  typedef ISIS::DicomImageReader< VolumeType >        DicomReaderType;
+
+  DicomReaderType reader;
+
+  if( argc > 1 ) 
+    {
+    reader.SetDirectory( argv[1] );
+    }
+
+  return EXIT_SUCCESS;
+
+}
+
