0d1b9b4aaff45d650cca956941c21b71178e4aaa
Hui (James) Zhang <zhang@isis.imac.georgetown.edu>
2004-05-10 11:07:13 -0400

    Registration viewer, add sphere for center line display

---
+++ IGMTRegistrationViewer.cxx
@@ -0,0 +1,63 @@
+#include "IGMTRegistrationViewer.h"
+
+#include "vtkProp3DCollection.h"
+#include "vtkProperty.h"
+
+namespace IGSTK
+{
+
+IGMTRegistrationViewer::IGMTRegistrationViewer(void)
+{
+  IGMTVolumeViewer::IGMTVolumeViewer();
+
+  m_FixedPointRadius = 1.0;
+
+  m_FixedPointSetAssembly = vtkAssembly::New();
+  m_MovingPointSetAssembly = vtkAssembly::New();
+
+  m_Root->AddPart(m_FixedPointSetAssembly);
+  m_Root->AddPart(m_MovingPointSetAssembly);
+}
+
+IGMTRegistrationViewer::~IGMTRegistrationViewer(void)
+{
+  IGMTVolumeViewer::~IGMTVolumeViewer();
+}
+
+void IGMTRegistrationViewer::SetFixedPointSet(PointSetType::Pointer pointset)
+{
+  unsigned int i, num = pointset->GetNumberOfPoints();
+  PointSetType::PointType point;
+  vtkSphereSource* sphere;
+  vtkPolyDataMapper* mapper;
+  vtkActor* actor;
+
+  m_FixedPointSetAssembly->GetParts()->RemoveAllItems();
+
+  for (i = 0; i < num; i++)
+  {
+    pointset->GetPoint(i, &point);
+    
+    sphere = vtkSphereSource::New();
+    sphere->SetCenter(point[0], point[1], point[2]);
+    sphere->SetRadius(m_FixedPointRadius);
+
+    mapper = vtkPolyDataMapper::New();
+    mapper->SetInput(sphere->GetOutput());
+
+    actor = vtkActor::New();
+    actor->SetMapper(mapper);
+    actor->GetProperty()->SetOpacity(0.1);
+    actor->GetProperty()->SetColor(0.0, 1.0, 0.0);
+
+    m_FixedPointSetAssembly->AddPart(actor);
+  }
+
+}
+
+void IGMTRegistrationViewer::SetFixedPointRadius(double radius)
+{
+  m_FixedPointRadius = radius;
+}
+
+}
---
+++ IGMTRegistrationViewer.h
@@ -0,0 +1,46 @@
+
+#ifndef _IGSTK_IGMTRegistrationViewer_H_
+#define _IGSTK_IGMTRegistrationViewer_H_
+
+#include "IGMTVolumeViewer.h"
+
+#include "itkPointSet.h"
+
+#include "vtkActor.h"
+#include "vtkAssembly.h"
+
+namespace IGSTK
+{
+
+class IGMTRegistrationViewer :
+  public IGMTVolumeViewer
+{
+public:
+
+  typedef itk::PointSet<double, 3> PointSetType;
+
+public:
+
+  vtkAssembly* m_FixedPointSetAssembly;
+  
+  vtkAssembly* m_MovingPointSetAssembly;
+
+public:
+
+  double m_FixedPointRadius;
+
+public:
+  
+  IGMTRegistrationViewer(void);
+  
+  ~IGMTRegistrationViewer(void);
+
+  void SetFixedPointSet(PointSetType::Pointer pointset);
+
+  void SetFixedPointRadius(double radius);
+
+};
+
+}
+
+#endif
