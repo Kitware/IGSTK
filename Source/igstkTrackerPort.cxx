52084feb5b1c2ad99661ce8fb4ede804201dfa23
Sohan Ranjan <ranjan@isis.imac.georgetown.edu>
2004-10-22 14:12:42 -0400

    ENH: Base Tracker Port class for REQUIREMENT: REQ 06.02.04.

---
+++ Source/igstkTrackerPort.cxx
@@ -0,0 +1,33 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkTrackerPort.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISIS Georgetown University. All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include ".\igstkTrackerPort.h"
+
+namespace igstk
+{
+
+TrackerPort::TrackerPort(void)
+{
+    m_Tools.clear();
+}
+
+TrackerPort::~TrackerPort(void)
+{
+    m_Tools.clear();
+}
+
+}
