759e85857103a39d44c18fda74bf23ca969ffe2b
Sohan Ranjan <ranjan@isis.imac.georgetown.edu>
2004-10-22 15:03:36 -0400

    ENH: Class for logging data for REQUIREMENT: REQ 05.07.01.

---
+++ Source/igstkLogger.cxx
@@ -0,0 +1,30 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkLogger.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISIS Georgetown University. All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include ".\igstkLogger.h"
+
+namespace igstk
+{
+
+Logger::Logger( void )
+{
+    m_PriorityLevel = Logger::NOTSET;
+}
+
+
+}
+
