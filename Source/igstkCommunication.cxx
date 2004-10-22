95540d27429cdad3a20cd35baaf121ba6c2aac97
Sohan Ranjan <ranjan@isis.imac.georgetown.edu>
2004-10-22 14:44:13 -0400

    ENH: Base Communication class for REQUIREMENT: REQ 05.06.01.

---
+++ Source/igstkCommunication.cxx
@@ -0,0 +1,33 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkCommunication.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISIS Georgetown University. All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include ".\igstkCommunication.h"
+
+namespace igstk
+{
+
+Communication::Communication(void)
+{
+}
+
+Communication::~Communication(void)
+{
+}
+
+}
+
+
