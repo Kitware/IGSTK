a9473dd8ebe5dee65e07a7818a38eeeeff2d7209
Sohan Ranjan <ranjan@isis.imac.georgetown.edu>
2004-10-22 15:03:00 -0400

    ENH: Class for logging data for REQUIREMENT: REQ 05.07.01.

---
+++ Source/igstkLogger.h
@@ -0,0 +1,56 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkLogger.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISIS Georgetown University. All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __igstk_Logger_h_
+#define __igstk_Logger_h_
+
+#include "igstkMacros.h"
+#include "igstkMultipleOutput.h"
+
+namespace igstk
+{
+/** \class Logger
+    \brief Class Logger is meant for logging information during a run. 
+    This class has been derived from MultipleOutput class. 
+*/
+
+class Logger : public igstk::MultipleOutput
+{
+public:
+
+    typedef enum 
+    { 
+        FATAL=0, CRITICAL, WARNING, DEBUG, NOTSET 
+    } PriorityLevelType;
+   
+    /** Constructor */
+    Logger( void );
+
+
+    SetMacro( PriorityLevel, PriorityLevelType );
+
+    GetMacro( PriorityLevel, PriorityLevelType );
+
+private:
+
+    PriorityLevelType    m_PriorityLevel;
+
+};
+
+}
+
+#endif //__igstk_Logger_h_
