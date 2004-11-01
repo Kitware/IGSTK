65e01c14bf6951a861877702108b01f13b73e394
Luis Ibanez <luis.ibanez@kitware.com>
2004-11-01 17:00:20 -0500

    BUG: 1315. Low code coverage on the state machine, only 82% according to gcov.            Removing unused method from the Tester1 class.            Adding testing situation for invoking transitions using a state,input            pair for which no transition has been defined.

--- Testing/igstkStateMachineErrorsTest.cxx
+++ Testing/igstkStateMachineErrorsTest.cxx
@@ -35,8 +35,6 @@ public:
 
     // Set the input descriptors
     m_StateMachine.AddInput( "QuarterInserted");
-    m_StateMachine.AddInput( "SelectDrink");
-    m_StateMachine.AddInput( "CancelPurchase");
 
     const ActionType NoAction = 0;
 
@@ -44,18 +42,6 @@ public:
     m_StateMachine.AddTransition( "IdleState",          "QuarterInserted", "OneQuarterCredit",    NoAction );
     m_StateMachine.AddTransition( "OneQuarterCredit",   "QuarterInserted", "TwoQuarterCredit",    NoAction );
     m_StateMachine.AddTransition( "TwoQuarterCredit",   "QuarterInserted", "ThreeQuarterCredit",  NoAction );
-    m_StateMachine.AddTransition( "ThreeQuarterCredit", "QuarterInserted", "IdleState",           & Tester1::CancelAndReturnChange );
-
-    m_StateMachine.AddTransition(  "IdleState",          "SelectDrink",     "IdleState",          & Tester1::NoEnoughChangeMessage );
-    m_StateMachine.AddTransition(  "OneQuarterCredit",   "SelectDrink",     "OneQuarterCredit",   & Tester1::NoEnoughChangeMessage );
-    m_StateMachine.AddTransition(  "TwoQuarterCredit",   "SelectDrink",     "TwoQuarterCredit",   & Tester1::NoEnoughChangeMessage );
-    m_StateMachine.AddTransition(  "ThreeQuarterCredit", "SelectDrink",     "IdleState",          & Tester1::DeliverDrink );
-
-    m_StateMachine.AddTransition(  "IdleState",          "CancelPurchase",  "IdleState",          & Tester1::CancelAndReturnChange );
-    m_StateMachine.AddTransition(  "OneQuarterCredit",   "CancelPurchase",  "IdleState",          & Tester1::CancelAndReturnChange );
-    m_StateMachine.AddTransition(  "TwoQuarterCredit",   "CancelPurchase",  "IdleState",          & Tester1::CancelAndReturnChange );
-    m_StateMachine.AddTransition(  "ThreeQuarterCredit", "CancelPurchase",  "IdleState",          & Tester1::CancelAndReturnChange );
-
 
     m_StateMachine.SelectInitialState( "IdleState");
 
@@ -67,30 +53,6 @@ public:
   {
     m_StateMachine.SelectInitialState("ThisStateDoesntExists");
   }
-
-protected:
-
-  // Reaction methods that will be invoked during the 
-  // state machine transitions. Note that this methods
-  // are protected because they should only be called
-  // from the state machine.
-  void CancelAndReturnChange() 
-    {
-    std::cout << "Cancelling and returning Change" << std::endl;
-    }
-
-
-  void DeliverDrink()
-    {
-    std::cout << "Deliver Drink" << std::endl;
-    }
-
-
-  void NoEnoughChangeMessage()
-    {
-    std::cout << "You have not inserted enough change" << std::endl;
-    }
-
 
 private:
 
@@ -226,6 +188,38 @@ private:
 };
 
 
+class Tester6
+{
+public:
+
+  typedef igstk::StateMachine< Tester6 >   StateMachineType;
+  typedef StateMachineType::TMemberFunctionPointer        ActionType;
+  FriendClassMacro(StateMachineType);
+
+  Tester6()
+    {
+    m_StateMachine.SetOwnerClass( this );
+    m_StateMachine.AddState( "IdleState" );
+    m_StateMachine.SelectInitialState( "IdleState");
+    m_StateMachine.AddInput( "QuarterInserted");
+    m_StateMachine.AddInput( "Cancel");
+    const ActionType NoAction = 0;
+    // On purpose NOT adding any transition: input "Cancel"
+    m_StateMachine.AddTransition( "IdleState", "QuarterInserted", "IdleState", NoAction );
+    m_StateMachine.SetReadyToRun();
+    }
+
+  void InvokeUndefinedStateInputTransition() 
+    {
+    std::cout << "Invoking Cancel" << std::endl;
+    m_StateMachine.ProcessInput( "Cancel" );
+    }
+ 
+private:
+  StateMachineType   m_StateMachine;
+};
+
+
 
 
 int igstkStateMachineErrorsTest(int argc, char * argv [])
@@ -260,6 +254,11 @@ int igstkStateMachineErrorsTest(int argc, char * argv [])
   std::cout << "Invoking  ProcessInput() in a state without transitions defined." << std::endl;
   Tester5 tester5;
   tester5.InvokeUndefinedTransition();
+
+  std::cout << "Invoking  ProcessInput() in a state,input pair without transitions defined." << std::endl;
+  Tester6 tester6;
+  tester6.InvokeUndefinedStateInputTransition();
+
 
   // The following call 
   return EXIT_SUCCESS;
