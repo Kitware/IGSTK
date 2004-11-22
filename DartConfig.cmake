ef821b8b183a5a3391d1f13d4368b6dea6832291
Andy Cedilnik <andy.cedilnik@kitware.com>
2004-11-22 17:19:54 -0500

    STYLE: Add ViewCVS and continuous reporting REQUIREMENT: REQ 05.03.01.

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -12,6 +12,7 @@ ELSE(DROP_METHOD MATCHES http)
   SET (DROP_SITE_USER "ftpuser")
   SET (DROP_SITE_PASSWORD "public")
 ENDIF(DROP_METHOD MATCHES http)
+
 SET (TRIGGER_SITE 
   "http://${DROP_SITE}/cgi-bin/Submit-IGSTK-TestingResults.pl")
 
@@ -20,13 +21,29 @@ SET (TRIGGER_SITE
 
 # Dart server configuration 
 SET (ROLLUP_URL "http://${DROP_SITE}/cgi-bin/IGSTK-rollup-dashboard.sh")
-#SET (CVS_WEB_URL "http://${DROP_SITE}/cgi-bin/cvsweb.cgi/IGSTK/")
+SET (CVS_WEB_URL "http://${DROP_SITE}/cgi-bin/cvsweb.cgi/IGSTK/")
 SET (CVS_WEB_CVSROOT "IGSTK")
+
 SET (USE_DOXYGEN "On")
 #SET (DOXYGEN_URL "http://${DROP_SITE}/IGSTK/index.html" )
-SET (GNATS_WEB_URL "http://public.kitware.com/Bug/index.php")
-SET (USE_GNATS "On")
 
-# copy over the testing logo
-#CONFIGURE_FILE(${PARAVIEW_SOURCE_DIR}/Web/Art/ParaViewLogo.gif ${PARAVIEW_BINARY_DIR}/Testing/HTML/TestingResults/Icons/Logo.gif COPYONLY)
+SET (USE_GNATS "On")
+SET (GNATS_WEB_URL "http://public.kitware.com/Bug/index.php")
+
+# Continuous email delivery variables
+SET (CONTINUOUS_FROM "luis.ibanez@kitware.com")
+SET (SMTP_MAILHOST "public.kitware.com")
+SET (CONTINUOUS_MONITOR_LIST "luis.ibanez@kitware.com")
+SET (CONTINUOUS_BASE_URL "http://public.kitware.com/IGSTK")
+
+SET (DELIVER_BROKEN_BUILD_EMAIL_WITH_TEST_FAILURES ON)
+SET (DELIVER_BROKEN_BUILD_EMAIL "Continuous Nightly")
+SET (EMAIL_FROM "cmake-dashboard@public.kitware.com")
+SET (DARTBOARD_BASE_URL "http://public.kitware.com/IGSTK")
+
+SET (DELIVER_BROKEN_BUILD_EMAIL_WITH_CONFIGURE_FAILURES 1)
+SET (DELIVER_BROKEN_BUILD_EMAIL_WITH_BUILD_ERRORS 1)
+SET (DELIVER_BROKEN_BUILD_EMAIL_WITH_BUILD_WARNINGS 1)
+SET (DELIVER_BROKEN_BUILD_EMAIL_WITH_TEST_NOT_RUNS 1)
+SET (DELIVER_BROKEN_BUILD_EMAIL_WITH_TEST_FAILURES 1)
 
