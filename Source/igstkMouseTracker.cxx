1511a2801e745406ad259607f7cda8b31fd85434
Sohan Ranjan <ranjan@isis.imac.georgetown.edu>
2004-12-08 11:40:19 -0500

    igstk::MouseTracker class added to repository.

---
+++ Source/igstkMouseTracker.cxx
@@ -0,0 +1,60 @@
+
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkMouseTracker.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISIS Georgetown University. All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more DEBUGrmation.
+
+=========================================================================*/
+
+#if defined(_MSC_VER)
+   //Warning about: identifier was truncated to '255' characters in the debug information (MVC6.0 Debug)
+#pragma warning( disable : 4786 )
+#endif
+
+#include <FL/Fl.H>
+
+#include "igstkMouseTracker.h"
+
+namespace igstk
+{
+
+void MouseTracker::Initialize( const char *fileName )
+{
+  igstkLogMacro( igstk::Logger::DEBUG, "MouseTracker::Initialize called ...\n");
+  Tracker::Initialize();
+  Fl::run();
+}
+    
+void MouseTracker::SetUpToolsProcessing( void )
+{
+    igstkLogMacro( igstk::Logger::DEBUG, "MouseTracker::SetUpToolsProcessing called ...\n");
+    TrackerPortType    port;
+    igstk::TrackerTool tool;
+    tool.SetError( 0 );
+    port.AddTool( tool );
+    this->AddPort( port );
+}
+    
+void MouseTracker::UpdateStatusProcessing( void )
+{
+    igstkLogMacro( igstk::Logger::DEBUG, "MouseTracker::UpdateStatusProcessing called ...\n");
+    typedef itk::Point< double, 3 >  PositionType;
+    PositionType  pos;
+    pos[0] = Fl::event_x();
+    pos[1] = Fl::event_y();
+    pos[2] = 0;
+    std::cout << "-> ( " << Fl::event_x() << "," << Fl::event_y() << "," << pos[2] << ")" << std::endl;
+    this->SetToolPosition( 0, 0, pos );
+}
+    
+}
---
+++ Source/igstkMouseTracker.h
@@ -0,0 +1,55 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkMouseTracker.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISIS Georgetown University. All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __igstk_MouseTracker_h_
+#define __igstk_MouseTracker_h_
+
+#include "igstkTracker.h"
+
+namespace igstk
+{
+/** \class MouseTracker
+    \brief Implementation of the Mouse Tracker class.
+
+*/
+
+class MouseTracker : public igstk::Tracker
+{
+public:
+    typedef MouseTracker                        Self;
+    typedef itk::SmartPointer<Self>             Pointer;
+    typedef itk::SmartPointer<const Self>       ConstPointer;
+
+    /** Method for creation of a reference counted object. */
+    NewMacro( MouseTracker );  
+
+    void Initialize( const char *fileName = NULL );
+
+protected:
+
+    virtual void SetUpToolsProcessing( void );
+
+    /** The "UpdateStatusProcessing" method updates mouse status. */
+    virtual void UpdateStatusProcessing( void );
+
+private:
+
+};
+
+}
+
+#endif //__igstk_MouseTracker_h_
