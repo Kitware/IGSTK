21328358a77416db1638901c262f518c866a9cfb
Julien Jomier <julien.jomier@kitware.com>
2004-12-09 14:02:56 -0500

    BUG: Removed Fl::run()

--- Source/igstkMouseTracker.cxx
+++ Source/igstkMouseTracker.cxx
@@ -28,32 +28,33 @@
 namespace igstk
 {
 
+
+
 void MouseTracker::Initialize( const char *fileName )
 {
   igstkLogMacro( igstk::Logger::DEBUG, "MouseTracker::Initialize called ...\n");
   Tracker::Initialize();
-  Fl::run();
 }
     
 void MouseTracker::SetUpToolsProcessing( void )
 {
-    igstkLogMacro( igstk::Logger::DEBUG, "MouseTracker::SetUpToolsProcessing called ...\n");
-    TrackerPortType    port;
-    igstk::TrackerTool tool;
-    tool.SetError( 0 );
-    port.AddTool( tool );
-    this->AddPort( port );
+  igstkLogMacro( igstk::Logger::DEBUG, "MouseTracker::SetUpToolsProcessing called ...\n");
+  TrackerPortType    port;
+  igstk::TrackerTool tool;
+  tool.SetError( 0 );
+  port.AddTool( tool );
+  this->AddPort( port );
 }
     
 void MouseTracker::UpdateStatusProcessing( void )
 {
-    igstkLogMacro( igstk::Logger::DEBUG, "MouseTracker::UpdateStatusProcessing called ...\n");
-    typedef itk::Point< double, 3 >  PositionType;
-    PositionType  position;
-    position[0] = Fl::event_x();
-    position[1] = Fl::event_y();
-    position[2] = 0;
-    this->SetToolPosition( 0, 0, position );
+  igstkLogMacro( igstk::Logger::DEBUG, "MouseTracker::UpdateStatusProcessing called ...\n");
+  typedef itk::Point< double, 3 >  PositionType;
+  PositionType  position;
+  position[0] = Fl::event_x();
+  position[1] = Fl::event_y();
+  position[2] = 0;
+  this->SetToolPosition( 0, 0, position );
 }
  
 void MouseTracker::GetPosition(PositionType& position)
