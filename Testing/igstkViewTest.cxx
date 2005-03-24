39b25caf7b432d2efc353e0d5e3fc5f85874b314
Luis Ibanez <luis.ibanez@kitware.com>
2005-03-23 23:23:28 -0500

    ENH: Add interactions with Scene and representation objects in order to      exercise more methods and satisfy REQUIREMENT: REQ 05.02.02: 100% coverage.

--- Testing/igstkViewTest.cxx
+++ Testing/igstkViewTest.cxx
@@ -24,13 +24,43 @@
 #include "igstkView2D.h"
 #include "igstkView3D.h"
 
+#include "igstkScene.h"
+#include "igstkEllipsoidObject.h"
+#include "igstkCylinderObject.h"
+#include "igstkEllipsoidObjectRepresentation.h"
+#include "igstkCylinderObjectRepresentation.h"
+
 int igstkViewTest( int, char * [] )
 {
+
   typedef igstk::View2D  View2DType;
   typedef igstk::View3D  View3DType;
 
   try
     {
+    // Create the scene
+    igstk::Scene::Pointer scene = igstk::Scene::New();
+
+    // Create the ellipsoid 
+    igstk::EllipsoidObject::Pointer ellipsoid = igstk::EllipsoidObject::New();
+    ellipsoid->SetRadius(1,1,1);
+    
+    // Create the ellipsoid representation
+    igstk::EllipsoidObjectRepresentation::Pointer ellipsoidRepresentation = igstk::EllipsoidObjectRepresentation::New();
+    ellipsoidRepresentation->RequestSetEllipsoidObject( ellipsoid );
+    ellipsoidRepresentation->SetColor(0.0,1.0,0.0);
+    ellipsoidRepresentation->SetOpacity(1.0);
+
+    // Create the cylinder 
+    igstk::CylinderObject::Pointer cylinder = igstk::CylinderObject::New();
+    cylinder->SetRadius(0.1);
+    cylinder->SetHeight(3);
+
+    // Create the cylinder representation
+    igstk::CylinderObjectRepresentation::Pointer cylinderRepresentation = igstk::CylinderObjectRepresentation::New();
+    cylinderRepresentation->RequestSetCylinderObject( cylinder );
+    cylinderRepresentation->SetColor(1.0,0.0,0.0);
+    cylinderRepresentation->SetOpacity(1.0);
 
     // Create an FLTK minimal GUI
     Fl_Window * form = new Fl_Window(600,300,"View3D Test");
@@ -49,6 +79,13 @@ int igstkViewTest( int, char * [] )
     view3D->RequestResetCamera();
     view3D->RequestEnableInteractions();
     
+ 
+    // Add the ellipsoid to the scene
+    scene->RequestAddObject(view2D,ellipsoidRepresentation);
+    
+    // Add the cylinder to the scene
+    scene->RequestAddObject(view3D,cylinderRepresentation);
+
     
     for(unsigned int i=0; i<100; i++)
       {
@@ -60,12 +97,27 @@ int igstkViewTest( int, char * [] )
     view2D->RequestDisableInteractions();
     view3D->RequestDisableInteractions();
 
+    // Remove the ellipsoid from the scene
+    scene->RequestRemoveObject(view2D,ellipsoidRepresentation);
+    
+    // Remove the cylinder from the scene
+    scene->RequestRemoveObject(view3D,cylinderRepresentation);
+
+
+    form->hide();
+
+    delete view2D;
+    delete view3D;
     }
   catch(...)
     {
     std::cerr << "Exception catched !!" << std::endl;
     return EXIT_FAILURE;
     }
+
+
+
+
 
   return EXIT_SUCCESS;
 }
