10e5b2a370bc354ff3240211bce6076d465db1ca
Sohan Ranjan <ranjan@isis.imac.georgetown.edu>
2005-04-26 18:55:45 -0400

    no message

--- Testing/igstkSandboxTests.cxx
+++ Testing/igstkSandboxTests.cxx
@@ -12,4 +12,11 @@ void RegisterTests()
   REGISTER_TEST(igstkMouseTrackerTest);
   REGISTER_TEST(igstkSerialCommunicationTest);
   REGISTER_TEST(igstkAtamaiNDITrackerTest);
+  REGISTER_TEST(itkRealtimeClockTest);
+  REGISTER_TEST(itkStdStreamLogOutputTest);
+  REGISTER_TEST(itkMultipleLogOutputTest);
+  REGISTER_TEST(itkLoggerTest);
+  REGISTER_TEST(itkThreadLoggerTest);
+  REGISTER_TEST(itkLoggerOutputTest);
+  REGISTER_TEST(itkLoggerManagerTest);
 }
