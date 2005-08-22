b943842b8b7322465c0c4ba1579696e4f05b4fee
Patrick Cheng <cheng@isis.georgetown.edu>
2005-08-22 14:31:59 -0400

    ENH: Add new application - a image guided tracking program with four views and racking within CT images. These four file are taken from the FourViewAndTracking directory. and renamed them with the new application name.

---
+++ Examples/FourViewsTrackingWithCT/main.cxx
@@ -0,0 +1,63 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    main.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISIS Georgetown University. All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+   //Warning about: identifier was truncated to '255' characters in the debug information (MVC6.0 Debug)
+#pragma warning( disable : 4284 )
+#endif
+
+#include "FourViewsTrackingWithCTImplementation.h"
+
+int main(int , char** )
+{ 
+
+  igstk::FourViewsTrackingWithCTImplementation   application;
+
+  application.Show();
+
+  // Create the ellipsoid 
+  igstk::EllipsoidObject::Pointer ellipsoid = igstk::EllipsoidObject::New();
+  ellipsoid->SetRadius(200,200,300); // about a human skull
+  
+  // Create the ellipsoid representation
+  igstk::EllipsoidObjectRepresentation::Pointer ellipsoidRepresentation = igstk::EllipsoidObjectRepresentation::New();
+  ellipsoidRepresentation->RequestSetEllipsoidObject( ellipsoid );
+  ellipsoidRepresentation->SetColor(0.0,1.0,0.0);
+  ellipsoidRepresentation->SetOpacity(1.0);
+
+  // Create the cylinder 
+  igstk::CylinderObject::Pointer cylinder = igstk::CylinderObject::New();
+  cylinder->SetRadius(1.0);
+  cylinder->SetHeight(300.0);  // about the size of a needle
+
+  // Create the cylinder representation
+  igstk::CylinderObjectRepresentation::Pointer cylinderRepresentation = igstk::CylinderObjectRepresentation::New();
+  cylinderRepresentation->RequestSetCylinderObject( cylinder );
+  cylinderRepresentation->SetColor(1.0,0.0,0.0);
+  cylinderRepresentation->SetOpacity(1.0);
+
+
+  // Add the ellipsoid representations to the views
+  application.AddEllipsoid( ellipsoidRepresentation );
+  application.AddCylinder(  cylinderRepresentation  );
+
+  // Associate the Spatial Object to the tracker
+  application.AttachObjectToTrack( cylinder );
+
+  Fl::run();
+
+  return EXIT_SUCCESS;
+}
