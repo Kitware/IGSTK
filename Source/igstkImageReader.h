89eb88c65f40a65f922f2f333a81a59f7e2c6580
Andinet Enquobahrie <andinet.enqu@kitware.com>
2005-08-31 12:42:26 -0400

    The imageReader class is now templated over type and dimension

--- Source/igstkImageReader.h
+++ Source/igstkImageReader.h
@@ -3,48 +3,59 @@
 #define __igstkImageReader_h
 
 #include "igstkMacros.h"
-#include "itkObject.h"
-#include "itkImageFileReader.h"
-#include "itkImageSeriesReader.h"
 
-#include "itkGDCMImageIO.h"
-#include "itkGDCMSeriesFileNames.h"
+#include "itkObject.h"
+#include "itkMacro.h"
+#include "itkImageFileReader.h"
+#include "itkLogger.h"
 
 #include "itkVTKImageExport.h"
 #include "itkVTKImageImport.h"
 
 #include "vtkImageImport.h"
 #include "vtkImageExport.h"
-
 #include "vtkImageData.h"
+
+
 
 namespace igstk
 {
 
 /** \class ImageReader
  * 
- * \brief This class reads image data stored in files and provide pointers to the image data for use in either a VTK or an ITK pipeline. The parameters of the object
- * are the ... Default ...
- * 
+ * \brief This class reads image data stored in files and provide
+ * pointers to the image data for use in either a VTK or an ITK
+ * pipeline. This class has pixeltype and dimension template parameters
+ *
  * \ingroup Object
  */
 
+template <class TPixelType, unsigned int Dimension>
 class ImageReader : public itk::Object
 {
 
 public:
+  /** typedef for LoggerType */
+  typedef itk::Logger                    LoggerType;
 
   /** Typedefs */
   typedef ImageReader                           Self;
   typedef itk::Object                           Superclass;
   typedef itk::SmartPointer<Self>               Pointer;
   typedef itk::SmartPointer<const Self>         ConstPointer;
-  itkStaticConstMacro(ImageDimension, unsigned int, 3);
-  typedef signed short                          PixelType;
-  typedef itk::Image<PixelType, ImageDimension> ImageType;
+
+  itkStaticConstMacro(ImageDimension, unsigned int, Dimension);
+
+  /** Some convenient typedefs for input image */
+  typedef TPixelType                            PixelType;
+  typedef itk::Image<PixelType,ImageDimension>  ImageType;
+  typedef typename ImageType::ConstPointer      ImagePointer;
+  typedef typename ImageType::RegionType        ImageRegionType; 
   typedef itk::ImageFileReader< ImageType >     ImageFileReaderType;
-  typedef itk::ImageSeriesReader< ImageType >   ImageSeriesReaderType;
   typedef itk::VTKImageExport< ImageType >      ExportFilterType;
+
+ /** Method to get the VTK image data */
+  const vtkImageData * GetVTKImageData() const;
 
   /**  Run-time type information (and related methods). */
   igstkTypeMacro( ImageReader, itk::Object );
@@ -55,10 +66,6 @@ public:
   /** method to get the ITK Image data */
 //  virtual const ImageReader::ImageType::Pointer ImageReader::GetITKImageData() const
 
-  /** Method to get the VTK image data */
-  const vtkImageData * GetVTKImageData() const;
-
-
   /** Set method for the DICOM file name */
   void SetFileName( const char *filename );
 
@@ -68,17 +75,29 @@ public:
 protected:
 
   ImageReader( void );
-  ~ImageReader( void );
+  ~ImageReader( void ) ;
 
   /** Print the object information in a stream. */
-  virtual void PrintSelf( std::ostream& os, itk::Indent indent ) const;
+  void PrintSelf( std::ostream& os, itk::Indent indent ) const;
+
+  /* Internal itkImageFileReader */
+  typename ImageFileReaderType::Pointer      m_ImageFileReader;
 
   /** Classes to connect an ITK pipeline to a VTK pipeline */
-  vtkImageImport                      * m_vtkImporter;
-  ExportFilterType::Pointer             m_itkExporter;
+  vtkImageImport                               * m_vtkImporter;
+  typename ExportFilterType::Pointer             m_itkExporter;
 
+  /** Declarations needed for the Logger */
+  igstkLoggerMacro();
+
+private:
+  
 };
 
 } // end namespace igstk
+
+#ifndef IGSTK_MANUAL_INSTANTIATION
+#include "igstkImageReader.txx"
+#endif
 
 #endif // __igstkImageReader_h
