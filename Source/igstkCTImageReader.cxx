c251aab181232255cc29364095d75d8e34f8fabb
Andinet Enquobahrie <andinet.enqu@kitware.com>
2005-08-31 12:36:35 -0400

    CTImageReader class is now derived from the DICOMImageReader class. The main image reading functionalities have been refactorized to the DICOMImageReader class

--- Source/igstkCTImageReader.cxx
+++ Source/igstkCTImageReader.cxx
@@ -1,4 +1,4 @@
-
+#include "igstkDICOMImageReader.h"
 #include "igstkCTImageReader.h"
 #include "igstkEvents.h"
 
@@ -8,57 +8,8 @@ namespace igstk
 /** Constructor */
 CTImageReader::CTImageReader()
 {
-  // Create the ITK image file reader
-  m_ImageFileReader = ImageFileReaderType::New();
 
-  m_names = itk::GDCMSeriesFileNames::New();
-  m_io = itk::GDCMImageIO::New();
-
-  // Create the ITK image series reader for reading groups of images
-  m_ImageSeriesReader = ImageSeriesReaderType::New();
 } 
-
-/** Destructor */
-CTImageReader::~CTImageReader()  
-{
-  if( m_ImageFileReader )
-    {
-    m_ImageFileReader->Delete();
-    }
-
-  if( m_ImageSeriesReader )
-    {
-    m_ImageSeriesReader->Delete();
-    }
-
-  if( m_names )
-    {
-    m_names->Delete();
-    }
-
-  if( m_io )
-    {
-    m_io->Delete();
-    }
-}
-
-void CTImageReader::SetDirectory( const char *directory )
-{
-  if( m_names && m_io && m_ImageSeriesReader )
-    {
-    m_names->SetInputDirectory( directory );
-    m_ImageSeriesReader->SetFileNames( m_names->GetInputFileNames() );
-    m_ImageSeriesReader->SetImageIO( m_io );
-    m_ImageSeriesReader->Update();
-
-    m_itkExporter->SetInput( m_ImageSeriesReader->GetOutput() );
-    }
-}
-
-CTImageReader::ImageType::ConstPointer CTImageReader::GetITKImageData() const
-{
-  return( m_ImageSeriesReader->GetOutput() );
-}
 
 /** Print Self function */
 void CTImageReader::PrintSelf( std::ostream& os, itk::Indent indent ) const
--- Source/igstkCTImageReader.h
+++ Source/igstkCTImageReader.h
@@ -4,66 +4,45 @@
 
 #include "igstkMacros.h"
 #include "igstkImageReader.h"
+#include "igstkDICOMImageReader.h"
 #include "itkObject.h"
 
 namespace igstk
 {
 
 /** \class CTImageReader
- * 
- * \brief This class reads CT data stored in DICOM files. The parameters of the object
- * are the ... Default ...
- * 
  * \ingroup Object
  */
 
-class CTImageReader : public ImageReader
+class CTImageReader : public DICOMImageReader<unsigned short>
 {
 
 public:
 
   /** Typedefs */
   typedef CTImageReader                         Self;
-  typedef ImageReader                           Superclass;
+  typedef DICOMImageReader<unsigned short>       Superclass;
   typedef itk::SmartPointer<Self>               Pointer;
   typedef itk::SmartPointer<const Self>         ConstPointer;
+
+
   itkStaticConstMacro(ImageDimension, unsigned int, 3);
-  typedef signed short                          PixelType;
-  typedef itk::Image<PixelType, ImageDimension> ImageType;
-  typedef itk::ImageFileReader< ImageType >     ImageFileReaderType;
-  typedef itk::ImageSeriesReader< ImageType >   ImageSeriesReaderType;
 
   /**  Run-time type information (and related methods). */
-  igstkTypeMacro( CTImageReader, ImageReader );
+  igstkTypeMacro( CTImageReader, DICOMImageReader );
 
   /** Method for creation of a reference counted object. */
   igstkNewMacro( Self );
 
-  /** Method to set the directory containing the CT image data */
-  void CTImageReader::SetDirectory( const char *directory );
-
-  /** Method to get the ITK Image data */
-  CTImageReader::ImageType::ConstPointer CTImageReader::GetITKImageData() const;
-
 protected:
 
   CTImageReader( void );
-  ~CTImageReader( void );
+  virtual ~CTImageReader( void ) {};
 
   /** Print the object information in a stream. */
   void PrintSelf( std::ostream& os, itk::Indent indent ) const; 
 
 private:
-
-  /** Internal itkImageFileReader */
-  ImageFileReaderType::Pointer          m_ImageFileReader;
-
-  /** Helper classes for the image series reader */
-  itk::GDCMSeriesFileNames::Pointer     m_names;
-  itk::GDCMImageIO::Pointer             m_io;
-
-  //* Internal itkImageSeriesReader */
-  ImageSeriesReaderType::Pointer        m_ImageSeriesReader;
 
 };
 
