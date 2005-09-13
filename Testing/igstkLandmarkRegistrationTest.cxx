0cdcd5e1bdee6cef2ca05c280fc19061c7915b3a
Andinet Enquobahrie <andinet.enqu@kitware.com>
2005-09-13 16:56:53 -0400

    Reinforced the test program to test the state machines that has been added

--- Testing/igstkLandmarkRegistrationTest.cxx
+++ Testing/igstkLandmarkRegistrationTest.cxx
@@ -22,25 +22,38 @@
 
 #include <iostream>
 #include "igstkLandmarkRegistration.h"
+#include "itkLogger.h"
+#include "itkStdStreamLogOutput.h"
 
 int igstkLandmarkRegistrationTest( int argv, char * argc[] )
 {
     std::cout << "Testing igstk::LandmarkRegistration" << std::endl;
 
+    typedef itk::Logger                   LoggerType;
+    typedef itk::StdStreamLogOutput       LogOutputType;
+    
     typedef igstk::LandmarkRegistration<3>                                 LandmarkRegistrationType;
     typedef igstk::LandmarkRegistration<3>::LandmarkPointContainerType     LandmarkPointContainerType;
-    typedef igstk::LandmarkRegistration<3>::LandmarkPointType              LandmarkPointType;
-    typedef LandmarkRegistrationType::TransformType::OutputVectorType  OutputVectorType;
+    typedef igstk::LandmarkRegistration<3>::LandmarkImagePointType         LandmarkImagePointType;
+    typedef igstk::LandmarkRegistration<3>::LandmarkTrackerPointType         LandmarkTrackerPointType;
+    typedef LandmarkRegistrationType::TransformType::OutputVectorType      OutputVectorType;
 
     LandmarkRegistrationType::Pointer landmarkRegister = LandmarkRegistrationType::New();    
     LandmarkPointContainerType  fpointcontainer;
     LandmarkPointContainerType  mpointcontainer;
-    LandmarkPointType fixedPoint;
-    LandmarkPointType movingPoint;
+    LandmarkImagePointType fixedPoint;
+    LandmarkTrackerPointType movingPoint;
 
+
+// logger object
+    LoggerType::Pointer   logger = LoggerType::New();
+    LogOutputType::Pointer logOutput = LogOutputType::New();
+    logOutput->SetStream( std::cout );
+    logger->AddLogOutput( logOutput );
+    logger->SetPriorityLevel( itk::Logger::DEBUG );
+    landmarkRegister->SetLogger( logger );
 
     // Define the 3D rigid body transformation 
-
     typedef itk::Rigid3DTransform< double > Rigid3DTransformType;
 
     Rigid3DTransformType::Pointer   rigid3DTransform = Rigid3DTransformType::New();
@@ -73,139 +86,76 @@ int igstkLandmarkRegistrationTest( int argv, char * argc[] )
     fixedPoint[1] =  1.0;
     fixedPoint[2] =  15.0;
 
-    fpointcontainer.push_back(fixedPoint);
+    landmarkRegister->RequestComputeTransform();
+
+    //fpointcontainer.push_back(fixedPoint);
+    landmarkRegister->RequestAddImageLandmarkPoint(fixedPoint);
   
     movingPoint = rigid3DTransform->TransformPoint(fixedPoint);
-    mpointcontainer.push_back(movingPoint);
+    //mpointcontainer.push_back(movingPoint);
+    landmarkRegister->RequestAddTrackerLandmarkPoint(movingPoint);
+
 
     fixedPoint[0] =  15.0;
     fixedPoint[1] =  21.0;
     fixedPoint[2] =  17.0;
 
-    fpointcontainer.push_back(fixedPoint);
+    //fpointcontainer.push_back(fixedPoint);
+    landmarkRegister->RequestAddImageLandmarkPoint(fixedPoint);
 
     movingPoint = rigid3DTransform->TransformPoint(fixedPoint);
-    mpointcontainer.push_back(movingPoint);
+    //mpointcontainer.push_back(movingPoint);
+    landmarkRegister->RequestAddTrackerLandmarkPoint(movingPoint);
 
     fixedPoint[0] =  14.0;
     fixedPoint[1] =  25.0;
     fixedPoint[2] =  11.0;
 
-    fpointcontainer.push_back(fixedPoint);
+    //fpointcontainer.push_back(fixedPoint);
+    landmarkRegister->RequestAddImageLandmarkPoint(fixedPoint);
 
     movingPoint = rigid3DTransform->TransformPoint(fixedPoint);
-    mpointcontainer.push_back(movingPoint);
+    //mpointcontainer.push_back(movingPoint);
+    landmarkRegister->RequestAddTrackerLandmarkPoint(movingPoint);
 
     fixedPoint[0] =  10.0;
     fixedPoint[1] =  11.0;
     fixedPoint[2] =  8.0;
 
-    fpointcontainer.push_back(fixedPoint);
-
+    landmarkRegister->RequestAddImageLandmarkPoint(fixedPoint);
     movingPoint = rigid3DTransform->TransformPoint(fixedPoint);
-    mpointcontainer.push_back(movingPoint);
+    landmarkRegister->RequestAddTrackerLandmarkPoint(movingPoint);
 
-    //Set the tracker and modality image coordinates
-    landmarkRegister->SetTrackerImageLandmarks(fpointcontainer);
-    landmarkRegister->SetModalityImageLandmarks(mpointcontainer);
+    // Calculate transform
+    landmarkRegister->RequestComputeTransform();
 
-
-    //Get the tracker and modailty image coordinates and compare with
+    //Get the tracker and image coordinates and compare with
     LandmarkPointContainerType  trackerLandmarkPointcontainer;
-    LandmarkPointContainerType  modalityLandmarkPointcontainer;
+    LandmarkPointContainerType  imageLandmarkPointcontainer;
 
-    trackerLandmarkPointcontainer=landmarkRegister->GetTrackerImageLandmarks();
-    modalityLandmarkPointcontainer=landmarkRegister->GetModalityImageLandmarks();
-
+    trackerLandmarkPointcontainer=landmarkRegister->GetTrackerLandmarks();
+    imageLandmarkPointcontainer=landmarkRegister->GetImageLandmarks();
 
     OutputVectorType error;
     OutputVectorType::RealValueType tolerance = 0.00001;
     bool failed = false;
-
-    LandmarkRegistrationType::PointsContainerConstIterator
-      trackerlandmarkitb = fpointcontainer.begin();
-
-    LandmarkRegistrationType::PointsContainerConstIterator
-      trackerlandmarkita = trackerLandmarkPointcontainer.begin();
-
-    while( trackerlandmarkitb != fpointcontainer.end() )
-      {
-      std::cout<<"Trackerlandmark passed:"<<*trackerlandmarkitb<<std::endl;
-      std::cout<<"Trackerlandmark read "<<*trackerlandmarkita<<std::endl;
-      error = *trackerlandmarkitb - *trackerlandmarkita;
-      if( error.GetNorm() > tolerance )
-        {
-        failed = true;
-        }
-      ++trackerlandmarkitb;
-      ++trackerlandmarkita;
-      }
-
-     if( failed )
-      {
-      std::cout << "Landmark coordinates are not properly set" << std::endl;
-      std::cout << "  [FAILED]" << std::endl;
-      return EXIT_FAILURE;
-      }
-    else
-      {
-      std::cout << "  Tracker landmark coordinates are properly set [PASSED]" << std::endl;
-      }
-
-
-    LandmarkRegistrationType::PointsContainerConstIterator
-      modalitylandmarkitb = mpointcontainer.begin();
-
-    LandmarkRegistrationType::PointsContainerConstIterator
-      modalitylandmarkita = modalityLandmarkPointcontainer.begin();
-
-
-    while( modalitylandmarkitb != mpointcontainer.end() )
-      {
-      std::cout<<"Modalitylandmark passed:"<<*modalitylandmarkitb<<std::endl;
-      std::cout<<"Modalitylandmark read "<<*modalitylandmarkita<<std::endl;
-      error = *modalitylandmarkitb - *modalitylandmarkita;
-      if( error.GetNorm() > tolerance )
-        {
-        failed = true;
-        }
-      ++modalitylandmarkitb;
-      ++modalitylandmarkita;
-      }
-
-     if( failed )
-      {
-      std::cout << "Landmark coordinates are not properly set" << std::endl;
-      std::cout << "  [FAILED]" << std::endl;
-      return EXIT_FAILURE;
-      }
-    else
-      {
-      std::cout << "  Modality landmark coordinates are properly set [PASSED]" << std::endl;
-      }
-
     //Print out the two sets of coordinates 
     landmarkRegister->Print(std::cout);
-
-    //Determine the transformation parameters
-    landmarkRegister->EvaluateTransform();
-
     landmarkRegister->GetTransform()->DebugOn();
 
     //Check if the transformation parameters were evaluated correctely
     LandmarkRegistrationType::PointsContainerConstIterator
-      fitr = fpointcontainer.begin();
+      fitr = trackerLandmarkPointcontainer.begin();
     LandmarkRegistrationType::PointsContainerConstIterator
-      mitr = mpointcontainer.begin();
-
+      mitr = imageLandmarkPointcontainer.begin();
 
     OutputVectorType errortr;
     tolerance = 0.1;
     failed = false;
 
-    while( mitr != mpointcontainer.end() )
+    while( mitr != imageLandmarkPointcontainer.end() )
       {
-      std::cout << "  Tracker image Landmark: " << *fitr << " Modality Image landmark " << *mitr
+      std::cout << "  Tracker image Landmark: " << *fitr << " Image Image landmark " << *mitr
         << " Transformed trackerLandmark : " <<
         landmarkRegister->GetTransform()->TransformPoint( *fitr ) << std::endl;
 
@@ -222,7 +172,7 @@ int igstkLandmarkRegistrationTest( int argv, char * argc[] )
       {
       // Hang heads in shame
       std::cout << "  Tracker landmarks transformed by the transform did not match closely "
-        << " enough with the modality image landmarks.  The transform computed was: ";
+        << " enough with the image image landmarks.  The transform computed was: ";
       landmarkRegister->GetTransform()->Print(std::cout);
       std::cout << "  [FAILED]" << std::endl;
       return EXIT_FAILURE;
