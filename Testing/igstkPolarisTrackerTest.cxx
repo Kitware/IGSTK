fe31d7b6238a11c1947a9a8997a63829cc43097e
David Gobbi <david.gobbi@gmail.com>
2005-10-03 09:45:35 -0400

    BUG: 2321. Change all member variables to private, add a new state AttemptToUpdate, remove WaitForNextUpdate method

--- Testing/igstkPolarisTrackerTest.cxx
+++ Testing/igstkPolarisTrackerTest.cxx
@@ -187,7 +187,6 @@ int igstkPolarisTrackerTest( int, char * [] )
 
   for(int i=0; i<10; i++)
   {
-    tracker->WaitForNextTransform(1000);
     tracker->UpdateStatus();
     tracker->GetToolTransform( 0, 0, transitions );
     position = transitions.GetTranslation();
