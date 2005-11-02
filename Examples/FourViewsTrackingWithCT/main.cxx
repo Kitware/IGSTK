d82afa06c4db162466f970453c7687b29d65c93b
Patrick Cheng <cheng@isis.georgetown.edu>
2005-11-02 17:18:13 -0500

    First check in, remove former classes

--- Examples/FourViewsTrackingWithCT/main.cxx
+++ Examples/FourViewsTrackingWithCT/main.cxx
@@ -19,51 +19,11 @@
 #pragma warning( disable : 4284 )
 #endif
 
-#include "FourViewsTrackingWithCTImplementation.h"
+#include "FourViewsTrackingWithCT.h"
 
 int main(int , char** )
 { 
-
-  igstk::FourViewsTrackingWithCTImplementation   application;
-
-  application.Show();
-
-  // Create the ellipsoid 
-  igstk::EllipsoidObject::Pointer ellipsoid = igstk::EllipsoidObject::New();
-  ellipsoid->SetRadius(200,200,300); // about a human skull
-  
-  // Create the ellipsoid representation
-  igstk::EllipsoidObjectRepresentation::Pointer ellipsoidRepresentation = igstk::EllipsoidObjectRepresentation::New();
-  ellipsoidRepresentation->RequestSetEllipsoidObject( ellipsoid );
-  ellipsoidRepresentation->SetColor(0.0,1.0,0.0);
-  ellipsoidRepresentation->SetOpacity(1.0);
-
-  // Create the cylinder 
-  igstk::CylinderObject::Pointer cylinder = igstk::CylinderObject::New();
-  cylinder->SetRadius(1.0);
-  cylinder->SetHeight(300.0);  // about the size of a needle
-
-  // Create the cylinder representation
-  igstk::CylinderObjectRepresentation::Pointer cylinderRepresentation = igstk::CylinderObjectRepresentation::New();
-  cylinderRepresentation->RequestSetCylinderObject( cylinder );
-  cylinderRepresentation->SetColor(1.0,0.0,0.0);
-  cylinderRepresentation->SetOpacity(1.0);
-
-
-  // Add the ellipsoid representations to the views
-  application.AddEllipsoid( ellipsoidRepresentation );
-  application.AddCylinder(  cylinderRepresentation  );
-
-  // Reset the camera position
-  application.DisplayAxial->RequestResetCamera();
-  application.DisplayCoronal->RequestResetCamera();
-  application.DisplaySagittal->RequestResetCamera();
-  application.Display3D->RequestResetCamera();
-
-  // Associate the Spatial Object to the tracker
-  application.AttachObjectToTrack( cylinder );
+  igstk::FourViewsTrackingWithCT::Pointer application = igstk::FourViewsTrackingWithCT::New();
 
   Fl::run();
-
-  return EXIT_SUCCESS;
 }
