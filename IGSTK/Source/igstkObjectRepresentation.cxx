cb6110b7637207defc2a3d52aa4c976922f8b798
Patrick Cheng <cheng@isis.georgetown.edu>
2006-02-11 08:19:14 -0500

    fixed bugs in timestamp, Simplify the state machine in ObjectRepresentation and add conditions for displaying static object

---
+++ IGSTK/Source/igstkObjectRepresentation.cxx
@@ -0,0 +1,352 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkObjectRepresentation.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISIS Georgetown University. All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "igstkObjectRepresentation.h" 
+#include "vtkMatrix4x4.h"
+#include "igstkEvents.h"
+#include "vtkActor.h"
+#include "vtkProp3D.h"
+#include "vtkProperty.h"
+
+namespace igstk 
+{ 
+
+/** Constructor */
+ObjectRepresentation::ObjectRepresentation():m_StateMachine(this),m_Logger(NULL)
+{
+  m_Color[0] = 1.0;
+  m_Color[1] = 1.0;
+  m_Color[2] = 1.0;
+  m_Opacity = 1.0;
+  m_SpatialObject = NULL;
+  m_LastMTime = 0;
+
+  igstkAddInputMacro( ValidSpatialObject );
+  igstkAddInputMacro( NullSpatialObject  );
+  igstkAddInputMacro( UpdateActorsPosition );
+  igstkAddInputMacro( UpdateRepresentation );
+  igstkAddInputMacro( ValidTimeStamp );
+  igstkAddInputMacro( InvalidTimeStamp );
+  igstkAddInputMacro( SpatialObjectTransform );
+  igstkAddInputMacro( RequestUpdatePosition );
+
+  igstkAddStateMacro( NullSpatialObject );
+  igstkAddStateMacro( ValidSpatialObjectAndVisible );
+  igstkAddStateMacro( ValidSpatialObjectAndInvisible );
+  //igstkAddStateMacro( ValidTimeStamp );
+  //igstkAddStateMacro( InvalidTimeStamp );
+  //igstkAddStateMacro( AttemptingUpdatePositionAndVisible );
+  //igstkAddStateMacro( AttemptingUpdatePositionAndInvisible );
+
+  igstkAddTransitionMacro( NullSpatialObject, NullSpatialObject, NullSpatialObject,  No );
+  igstkAddTransitionMacro( NullSpatialObject, ValidSpatialObject, ValidSpatialObjectAndVisible,  SetSpatialObject );
+  igstkAddTransitionMacro( NullSpatialObject, UpdateActorsPosition, NullSpatialObject,  ReportInvalidRequest );
+  igstkAddTransitionMacro( NullSpatialObject, UpdateRepresentation, NullSpatialObject,  No );
+  igstkAddTransitionMacro( NullSpatialObject, ValidTimeStamp, NullSpatialObject,  ReportInvalidRequest );
+  igstkAddTransitionMacro( NullSpatialObject, InvalidTimeStamp, NullSpatialObject,  ReportInvalidRequest );
+  igstkAddTransitionMacro( NullSpatialObject, RequestUpdatePosition, NullSpatialObject,  ReportInvalidRequest );
+  igstkAddTransitionMacro( NullSpatialObject, SpatialObjectTransform, NullSpatialObject,  ReportInvalidRequest );
+
+  igstkAddTransitionMacro( ValidSpatialObjectAndVisible, NullSpatialObject, NullSpatialObject,  No ); 
+  igstkAddTransitionMacro( ValidSpatialObjectAndVisible, UpdateActorsPosition, ValidSpatialObjectAndVisible,  UpdateActorsPosition );
+  igstkAddTransitionMacro( ValidSpatialObjectAndVisible, ValidSpatialObject, ValidSpatialObjectAndVisible,  No ); 
+  igstkAddTransitionMacro( ValidSpatialObjectAndVisible, UpdateRepresentation, ValidSpatialObjectAndVisible,  UpdateRepresentation );
+  igstkAddTransitionMacro( ValidSpatialObjectAndVisible, ValidTimeStamp, ValidSpatialObjectAndVisible,  No );
+  igstkAddTransitionMacro( ValidSpatialObjectAndVisible, InvalidTimeStamp, ValidSpatialObjectAndInvisible,  MakeObjectsInvisible );
+  igstkAddTransitionMacro( ValidSpatialObjectAndVisible, RequestUpdatePosition, ValidSpatialObjectAndVisible,  RequestUpdatePosition );
+  igstkAddTransitionMacro( ValidSpatialObjectAndVisible, SpatialObjectTransform, ValidSpatialObjectAndVisible,  ReceiveSpatialObjectTransform );
+
+  igstkAddTransitionMacro( ValidSpatialObjectAndInvisible, NullSpatialObject, NullSpatialObject,  No ); 
+  igstkAddTransitionMacro( ValidSpatialObjectAndInvisible, UpdateActorsPosition, ValidSpatialObjectAndInvisible,  UpdateActorsPosition );
+  igstkAddTransitionMacro( ValidSpatialObjectAndInvisible, ValidSpatialObject, ValidSpatialObjectAndInvisible,  No ); 
+  igstkAddTransitionMacro( ValidSpatialObjectAndInvisible, UpdateRepresentation, ValidSpatialObjectAndInvisible,  UpdateRepresentation );
+  igstkAddTransitionMacro( ValidSpatialObjectAndInvisible, ValidTimeStamp, ValidSpatialObjectAndVisible,  MakeObjectsVisible );
+  igstkAddTransitionMacro( ValidSpatialObjectAndInvisible, InvalidTimeStamp, ValidSpatialObjectAndInvisible,  No );
+  igstkAddTransitionMacro( ValidSpatialObjectAndInvisible, RequestUpdatePosition, ValidSpatialObjectAndInvisible,  RequestUpdatePosition );
+  igstkAddTransitionMacro( ValidSpatialObjectAndInvisible, SpatialObjectTransform, ValidSpatialObjectAndInvisible,  ReceiveSpatialObjectTransform );
+/*
+  igstkAddTransitionMacro( ValidTimeStamp, NullSpatialObject, NullSpatialObject,  No ); 
+  igstkAddTransitionMacro( ValidTimeStamp, UpdateActorsPosition, ValidTimeStamp,  UpdateActorsPosition );
+  igstkAddTransitionMacro( ValidTimeStamp, ValidSpatialObject, ValidSpatialObjectAndInvisible,  No ); 
+  igstkAddTransitionMacro( ValidTimeStamp, UpdateRepresentation, ValidTimeStamp,  UpdateRepresentation );
+  igstkAddTransitionMacro( ValidTimeStamp, ValidTimeStamp, ValidTimeStamp,  No );
+  igstkAddTransitionMacro( ValidTimeStamp, InvalidTimeStamp, InvalidTimeStamp,  MakeObjectsInvisible );
+  igstkAddTransitionMacro( ValidTimeStamp, RequestUpdatePosition, AttemptingUpdatePositionAndVisible,  RequestUpdatePosition );
+  igstkAddTransitionMacro( ValidTimeStamp, SpatialObjectTransform, InvalidTimeStamp,  ReceiveSpatialObjectTransform );
+
+  igstkAddTransitionMacro( InvalidTimeStamp, NullSpatialObject, NullSpatialObject,  No ); 
+  igstkAddTransitionMacro( InvalidTimeStamp, UpdateActorsPosition, InvalidTimeStamp,  UpdateActorsPosition );
+  igstkAddTransitionMacro( InvalidTimeStamp, ValidSpatialObject, ValidSpatialObjectAndInvisible,  No ); 
+  igstkAddTransitionMacro( InvalidTimeStamp, UpdateRepresentation, ValidSpatialObjectAndInvisible,  UpdateRepresentation );
+  igstkAddTransitionMacro( InvalidTimeStamp, ValidTimeStamp, ValidTimeStamp,  MakeObjectsVisible );
+  igstkAddTransitionMacro( InvalidTimeStamp, InvalidTimeStamp, InvalidTimeStamp,  No );
+  igstkAddTransitionMacro( InvalidTimeStamp, RequestUpdatePosition, AttemptingUpdatePositionAndInvisible,  RequestUpdatePosition );
+  igstkAddTransitionMacro( InvalidTimeStamp, SpatialObjectTransform, InvalidTimeStamp,  ReceiveSpatialObjectTransform );
+
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndInvisible, NullSpatialObject, NullSpatialObject,  No ); 
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndInvisible, UpdateActorsPosition, AttemptingUpdatePositionAndInvisible,  ReportInvalidRequest );
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndInvisible, ValidSpatialObject, ValidSpatialObjectAndInvisible,  No ); 
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndInvisible, UpdateRepresentation, ValidSpatialObjectAndInvisible,  UpdateRepresentation );
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndInvisible, ValidTimeStamp, AttemptingUpdatePositionAndInvisible,  ReportInvalidRequest );
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndInvisible, InvalidTimeStamp, AttemptingUpdatePositionAndInvisible,  ReportInvalidRequest );
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndInvisible, RequestUpdatePosition, AttemptingUpdatePositionAndInvisible,  RequestUpdatePosition );
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndInvisible, SpatialObjectTransform, InvalidTimeStamp,  ReceiveSpatialObjectTransform );
+
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndVisible, NullSpatialObject, NullSpatialObject,  No ); 
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndVisible, UpdateActorsPosition, AttemptingUpdatePositionAndVisible,  ReportInvalidRequest );
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndVisible, ValidSpatialObject, ValidSpatialObjectAndInvisible,  No ); 
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndVisible, UpdateRepresentation, AttemptingUpdatePositionAndVisible,  No );
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndVisible, ValidTimeStamp, AttemptingUpdatePositionAndVisible,  ReportInvalidRequest );
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndVisible, InvalidTimeStamp, AttemptingUpdatePositionAndVisible,  ReportInvalidRequest );
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndVisible, RequestUpdatePosition, AttemptingUpdatePositionAndVisible,  RequestUpdatePosition );
+  igstkAddTransitionMacro( AttemptingUpdatePositionAndVisible, SpatialObjectTransform, ValidSpatialObjectAndVisible,  ReceiveSpatialObjectTransform );
+*/
+  igstkSetInitialStateMacro( NullSpatialObject );
+
+  m_StateMachine.SetReadyToRun();
+
+} 
+
+/** Destructor */
+ObjectRepresentation::~ObjectRepresentation()  
+{ 
+  // This must be invoked in order to prevent Memory Leaks.
+  this->DeleteActors();
+}
+
+/** Add an actor to the actors list */
+void ObjectRepresentation::AddActor( vtkProp * actor )
+{
+  m_Actors.push_back( actor );
+}
+
+/** Empty the list of actors */
+void ObjectRepresentation::DeleteActors()
+{
+  ActorsListType::iterator it = m_Actors.begin();
+  while(it != m_Actors.end())
+    {
+    (*it)->Delete();
+    it++;
+    }
+
+  // Reset the list of actors
+  m_Actors.clear();
+}
+
+/** Has the object been modified */
+bool ObjectRepresentation::IsModified() const
+{
+  if( m_LastMTime < this->GetMTime() )
+    {
+    return true;
+    }
+  return false;
+}
+
+
+/** Set the Spatial Object */
+void ObjectRepresentation::RequestSetSpatialObject( const SpatialObjectType * spatialObject )
+{
+  // This const_cast is done because the ObjectRepresentation class invoke
+  // Request methods in the SpatialObject, and those methods modify the state
+  // of its internal StateMachine. It is however desirable to keep the outside
+  // API of this class refering to a const object.
+  m_SpatialObjectToAdd = const_cast< SpatialObjectType *>( spatialObject );
+  if( !m_SpatialObjectToAdd )
+    {
+    igstkPushInputMacro( NullSpatialObject );
+    m_StateMachine.ProcessInputs();
+    }
+  else
+    {
+    igstkPushInputMacro( ValidSpatialObject );
+    m_StateMachine.ProcessInputs();
+    }
+
+}
+
+
+/** Set the Spatial Object */
+void ObjectRepresentation::SetSpatialObjectProcessing()
+{
+  m_SpatialObject = m_SpatialObjectToAdd;
+  this->ObserveTransformModifiedEvent( m_SpatialObject );
+
+}
+
+
+
+/** Set the color */
+void ObjectRepresentation::SetColor(float r, float g, float b)
+{
+  if(m_Color[0] == r && m_Color[1] == g && m_Color[2] == b)
+    {
+    return;
+    }
+  m_Color[0] = r;
+  m_Color[1] = g;
+  m_Color[2] = b;
+
+  // Update all the actors
+  ActorsListType::iterator it = m_Actors.begin();
+  while(it != m_Actors.end())
+    {
+    vtkActor * va = static_cast<vtkActor*>(*it);
+    va->GetProperty()->SetColor(m_Color[0], m_Color[1], m_Color[2]); 
+    it++;
+    }
+  this->Modified();
+}
+
+
+/** Request Update the object representation (i.e vtkActors). */
+void ObjectRepresentation::RequestUpdateRepresentation( const TimeStamp & time )
+{
+  m_TimeToRender = time; 
+  igstkPushInputMacro( UpdateRepresentation );
+  m_StateMachine.ProcessInputs();
+}
+
+
+/** Request Update the object position (i.e vtkActors). */
+void ObjectRepresentation::RequestUpdatePosition( const TimeStamp & time )
+{
+  m_TimeToRender = time; 
+  igstkPushInputMacro( RequestUpdatePosition );
+  m_StateMachine.ProcessInputs();
+}
+
+
+/** Process the request for updating the transform from the SpatialObject. */
+void ObjectRepresentation::RequestUpdatePositionProcessing()
+{
+  m_SpatialObject->RequestGetTransform();  // The response should be sent back in an event
+}
+
+
+/** Receive the Transform from the SpatialObject via a transduction macro. */
+void ObjectRepresentation::ReceiveSpatialObjectTransformProcessing()
+{
+  m_SpatialObjectTransform = m_SpatialObjectTransformToBeSet;
+
+  igstkPushInputMacro( UpdateActorsPosition );
+  m_StateMachine.ProcessInputs();
+}
+
+/** Null operation for a State Machine transition */
+void ObjectRepresentation::NoProcessing()
+{
+}
+
+
+/** Update the object representation (i.e vtkActors). */
+void ObjectRepresentation::UpdateActorsPositionProcessing()
+{
+
+  vtkMatrix4x4* vtkMatrix = vtkMatrix4x4::New();
+
+  m_SpatialObjectTransform.ExportTransform( *vtkMatrix );
+
+  // Update all the actors
+  ActorsListType::iterator it = m_Actors.begin();
+  while( it != m_Actors.end() )
+    {  
+    vtkProp3D::SafeDownCast(*it)->SetUserMatrix(vtkMatrix);
+    it++;
+    }
+
+  vtkMatrix->Delete();
+
+  // Update the modified time
+  m_LastMTime = this->GetMTime();
+}
+
+
+/** Update the object representation (i.e vtkActors). 
+ *  It checks the transform expiration time. */
+void ObjectRepresentation::RequestVerifyTimeStamp()
+{
+  if ( m_SpatialObjectTransform.GetExpirationTime() <= m_SpatialObjectTransform.GetStartTime())
+    {
+    igstkPushInputMacro( ValidTimeStamp );
+    m_StateMachine.ProcessInputs();
+    }
+  else
+    {
+    if( m_TimeToRender.GetExpirationTime() < m_SpatialObjectTransform.GetStartTime() ||
+        m_TimeToRender.GetStartTime()      > m_SpatialObjectTransform.GetExpirationTime() )
+      {
+      igstkPushInputMacro( InvalidTimeStamp );
+      m_StateMachine.ProcessInputs();
+      }
+    else
+      {
+      igstkPushInputMacro( ValidTimeStamp );
+      m_StateMachine.ProcessInputs();
+      }
+    }
+}
+
+
+/** Make Objects Invisible. This method is called when the Transform time stamp
+ * has expired with respect to the requested rendering time. */
+void ObjectRepresentation::MakeObjectsInvisibleProcessing()
+{
+  igstkLogMacro( WARNING, "MakeObjectsInvisibleProcessing at " << m_TimeToRender );
+  
+  ActorsListType::iterator it = m_Actors.begin();
+  while(it != m_Actors.end())
+    {
+    (*it)->VisibilityOff();
+    it++;
+    }
+}
+
+
+/** Make Objects Visible. This method is called when the Transform time stamp
+ * is valid with respect to the requested rendering time. */
+void ObjectRepresentation::MakeObjectsVisibleProcessing()
+{
+  igstkLogMacro( WARNING, "MakeObjectsVisibleProcessing at " << m_TimeToRender );
+
+  ActorsListType::iterator it = m_Actors.begin();
+  while(it != m_Actors.end())
+    {
+    (*it)->VisibilityOn();
+    it++;
+    }
+}
+
+
+/** Report an invalid request made to the State Machine */
+void ObjectRepresentation::ReportInvalidRequestProcessing()
+{
+  igstkLogMacro( WARNING, "Invalid request in ObjectRepresentation");
+}
+
+
+/** Print Self function */
+void ObjectRepresentation::PrintSelf( std::ostream& os, itk::Indent indent ) const
+{
+  Superclass::PrintSelf(os, indent);
+  os << indent << "Color: " << m_Color[0] << " : " << m_Color[1] << " : " << m_Color[2] << std::endl;
+  os << indent << "Opacity: " << m_Opacity << std::endl;
+}
+
+} // end namespace igstk
+
---
+++ IGSTK/Source/igstkTimeStamp.cxx
@@ -0,0 +1,154 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkTimeStamp.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISIS Georgetown University. All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "igstkTimeStamp.h"
+#include "igstkRealTimeClock.h"
+
+namespace igstk
+{
+
+  
+TimeStamp
+::TimeStamp()
+{
+  this->m_StartTime       = 0;
+  this->m_ExpirationTime  = 0;
+}
+
+
+TimeStamp
+::~TimeStamp()
+{
+  this->m_StartTime       = 0;
+  this->m_ExpirationTime  = 0;
+}
+
+
+const TimeStamp &
+TimeStamp
+::operator=( const TimeStamp & inputTimeStamp )
+{
+  this->m_StartTime      = inputTimeStamp.m_StartTime;
+  this->m_ExpirationTime = inputTimeStamp.m_ExpirationTime;
+  return *this;
+}
+   
+
+void
+TimeStamp
+::SetStartTimeNowAndExpireAfter(double millisecondsToExpire) 
+{
+  this->m_StartTime      = RealTimeClock::GetTimeStamp();
+  this->m_ExpirationTime = this->m_StartTime + millisecondsToExpire/1000;
+}
+
+
+
+double 
+TimeStamp
+::GetStartTime() const 
+{
+  return this->m_StartTime;
+}
+
+
+
+double 
+TimeStamp
+::GetExpirationTime() const 
+{
+  return this->m_ExpirationTime;
+}
+
+
+
+
+bool 
+TimeStamp
+::IsValidAtTime( double milliseconds ) const
+{
+  if( this->m_StartTime > milliseconds )
+    {
+    return false;
+    }
+
+  if( this->m_ExpirationTime < milliseconds )
+    {
+    return false;
+    }
+
+  return true;
+}
+
+
+void 
+TimeStamp
+::Print(std::ostream& os, itk::Indent indent) const
+{
+  this->PrintHeader(os, indent); 
+  this->PrintSelf(os, indent.GetNextIndent());
+  this->PrintTrailer(os, indent);
+}
+
+
+/**
+ * Define a default print header for all objects.
+ */
+void 
+TimeStamp
+::PrintHeader(std::ostream& os, itk::Indent indent) const
+{
+  os << indent << "TimeStamp" << " (" << this << ")\n";
+}
+
+
+/**
+ * Define a default print trailer for all objects.
+ */
+void 
+TimeStamp
+::PrintTrailer(std::ostream& itkNotUsed(os), itk::Indent itkNotUsed(indent)) const
+{
+}
+
+
+/**
+ * This operator allows all subclasses of LightObject to be printed via <<.
+ * It in turn invokes the Print method, which in turn will invoke the
+ * PrintSelf method that all objects should define, if they have anything
+ * interesting to print out.
+ */
+std::ostream& operator<<(std::ostream& os, const TimeStamp& o)
+{
+  o.Print(os, 0);
+  return os;
+}
+
+
+/** Print Self function */
+void TimeStamp::PrintSelf( std::ostream& os, itk::Indent indent ) const
+{
+  os << indent << "RTTI typeinfo:    " << typeid( *this ).name() << std::endl;
+  os << indent << "Start Time      = " << this->m_StartTime << std::endl;
+  os << indent << "Expiration Time = " << this->m_ExpirationTime << std::endl;
+}
+
+
+
+} // end namespace igstk
+
+
