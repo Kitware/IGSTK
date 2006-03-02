035eb3666a4c40d5ebed3d7f6e02cbb47453e072
Andinet Enquobahrie <andinet.enqu@kitware.com>
2006-03-02 16:44:17 -0500

    ENH: beefed up the test

--- Testing/igstkObliqueImageSpatialObjectRepresentationTest.cxx
+++ Testing/igstkObliqueImageSpatialObjectRepresentationTest.cxx
@@ -58,6 +58,70 @@ int igstkObliqueImageSpatialObjectRepresentationTest( int argc , char * argv [] 
   logger->AddLogOutput( logOutput );
   logger->SetPriorityLevel( itk::Logger::DEBUG );
 
+  //
+  typedef igstk::CTImageReader         ReaderType;
+
+  ReaderType::Pointer   reader = ReaderType::New();
+
+  reader->SetLogger( logger );
+
+  /* Read in a DICOM series */
+  std::cout << "Reading CT image : " << argv[1] << std::endl;
+
+  ReaderType::DirectoryNameType directoryName = argv[1];
+
+  reader->RequestSetDirectory( directoryName );
+ 
+  reader->RequestReadImage();
+
+  representation->SetLogger( logger );
+
+  representation->RequestSetImageSpatialObject( reader->GetOutput() );
+
+  // Exercise the TypeMacro() which defines the GetNameOfClass()
+  std::string name = representation->GetNameOfClass();
+
+  std::cout << "Name of class = " << name << std::endl;
+  
+  // Create an FLTK minimal GUI
+  Fl_Window * form = new Fl_Window(532,532,"CT Read View Test");
+    
+  typedef igstk::View2D  View2DType;
+
+  View2DType * view2D = new View2DType( 10,10,512,512,"2D View");
+
+  form->end();
+  form->show();
+  
+  // Set oblique plane parameters
+  
+  typedef RepresentationType::PointType  PointType;
+  typedef RepresentationType::VectorType VectorType;
+
+  PointType  point;
+  VectorType normal;
+
+  point[0] = 0.0;
+  point[1] = 0.0;
+  point[2] = 0.0;
+
+  normal[0] = 0.0;
+  normal[1] = 0.0;
+  normal[2] = 1.0;
+  
+  representation->RequestSetPointOnthePlane( point );
+  representation->RequestSetPlaneNormalVector( normal );
+  representation->RequestReslice();
+
+  representation->Print( std::cout );
+  
+  // igstk::PulseGenerator::CheckTimeouts();
+ 
+  view2D->Update();  // schedule redraw of the view
+  
+  delete view2D;
+  delete form;
+
   return EXIT_SUCCESS;
 }
 
