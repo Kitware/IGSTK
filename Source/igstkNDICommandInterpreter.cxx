736f06ef5d01a8148010d9bce6823f786d7f3fe8
David Gobbi <david.gobbi@gmail.com>
2006-03-08 13:03:12 -0500

    BUG: 2946. Addendum to previous bug fix.  The snprintf function is not yet available for MS compilers, but _snprintf is.  This is not an ideal solution.

--- Source/igstkNDICommandInterpreter.cxx
+++ Source/igstkNDICommandInterpreter.cxx
@@ -19,6 +19,14 @@
 #include <stdio.h>
 #include <string.h>
 
+// On MSVC and Borland, snprintf is not defined but _snprintf is.
+// This should probably be checked by CMake instead of here.
+#if defined(WIN32) || defined(_WIN32)
+#ifndef snprintf
+#define snprintf _snprintf
+#endif
+#endif
+
 namespace igstk
 {
 
