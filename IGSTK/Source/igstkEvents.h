4e1e84a1767184ff93d37ca43e5d81001833af77
Julien Jomier <julien.jomier@kitware.com>
2006-03-28 12:47:57 -0500

    ENH: Added support for passing igstkObject in events

---
+++ IGSTK/Source/igstkEvents.h
@@ -0,0 +1,118 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkEvents.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISIS Georgetown University. All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __igstk_Events_h_
+#define __igstk_Events_h_
+
+/** 
+ * This file contains the declarations of IGSTK Events used to communicate among components.
+ */
+
+#include "igstkTransform.h"
+#include "igstkPivotCalibration.h"
+
+namespace igstk 
+{
+#define igstkEventMacro( classname, superclassname ) \
+            itkEventMacro( classname, superclassname )
+
+#define igstkLoadedEventMacro( name, superclass, payloadtype ) \
+class  name : public superclass \
+{ \
+public:  \
+  typedef name Self; \
+  typedef superclass Superclass; \
+  typedef payloadtype PayloadType; \
+  name() {} \
+  virtual ~name() {} \
+  virtual const char * GetEventName() const { return #name; } \
+  virtual bool CheckEvent(const ::itk::EventObject* e) const \
+    { return dynamic_cast<const Self*>(e); } \
+  virtual ::itk::EventObject* MakeObject() const \
+    { return new Self; } \
+  name(const Self&s) :superclass(s){}; \
+  const PayloadType & Get() const \
+    { return m_Payload; }  \
+  void Set( const payloadtype & _var ) \
+    { m_Payload = _var; }  \
+private: \
+  void operator=(const Self&);  \
+  PayloadType  m_Payload; \
+};
+
+igstkEventMacro( IGSTKEvent,               itk::UserEvent );
+igstkEventMacro( PulseEvent,               IGSTKEvent );
+igstkEventMacro( RefreshEvent,             IGSTKEvent );
+igstkEventMacro( CompletedEvent,           IGSTKEvent );
+igstkEventMacro( InputOutputErrorEvent,    IGSTKEvent );
+igstkEventMacro( InputOutputTimeoutEvent,  IGSTKEvent );
+igstkEventMacro( OpenPortErrorEvent,       IGSTKEvent );
+igstkEventMacro( ClosePortErrorEvent,      IGSTKEvent );
+
+namespace EventHelperType 
+{
+typedef itk::Point< double, 3 >    PointType;
+typedef std::string                StringType;
+typedef unsigned int               UnsignedIntType;
+typedef signed int                 SignedIntType;
+typedef float                      FloatType;
+typedef double                     DoubleType;
+typedef struct {
+  unsigned int minimum;
+  unsigned int maximum;
+}                                  IntegerBoundsType;
+}
+
+igstkLoadedEventMacro( PointEvent, IGSTKEvent, EventHelperType::PointType );
+igstkLoadedEventMacro( TransformModifiedEvent, IGSTKEvent, Transform );
+igstkLoadedEventMacro( StringEvent, IGSTKEvent, EventHelperType::StringType );
+igstkLoadedEventMacro( UnsignedIntEvent, IGSTKEvent, EventHelperType::UnsignedIntType );
+igstkLoadedEventMacro( IntegerBoundsEvent, IGSTKEvent, EventHelperType::IntegerBoundsType );
+
+igstkEventMacro( AxialSliceBoundsEvent,      IntegerBoundsEvent );
+igstkEventMacro( SagittalSliceBoundsEvent,   IntegerBoundsEvent );
+igstkEventMacro( CoronalSliceBoundsEvent,    IntegerBoundsEvent );
+
+#define igstkLoadedObjectEventMacro( name, superclass, payloadtype ) \
+class  name : public superclass \
+{ \
+public:  \
+  typedef name Self; \
+  typedef superclass Superclass; \
+  typedef payloadtype PayloadType; \
+  name() {} \
+  virtual ~name() {} \
+  virtual const char * GetEventName() const { return #name; } \
+  virtual bool CheckEvent(const ::itk::EventObject* e) const \
+    { return dynamic_cast<const Self*>(e); } \
+  virtual ::itk::EventObject* MakeObject() const \
+    { return new Self; } \
+  name(const Self&s) :superclass(s){}; \
+  PayloadType::Pointer Get() const \
+    { return m_Payload; }  \
+  void Set( payloadtype * _var ) \
+    { m_Payload = _var; }  \
+private: \
+  void operator=(const Self&);  \
+  PayloadType::Pointer  m_Payload; \
+};
+
+igstkLoadedObjectEventMacro( CalibrationModifiedEvent, IGSTKEvent, PivotCalibration);
+
+}
+
+
+#endif
