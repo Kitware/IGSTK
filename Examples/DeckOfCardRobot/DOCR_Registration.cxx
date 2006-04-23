068b056d064d85f30f4e688ae2da7d266d9613f8
Patrick Cheng <cheng@isis.georgetown.edu>
2006-04-22 21:48:45 -0400

    Add IGSTK Copyright

--- Examples/DeckOfCardRobot/DOCR_Registration.cxx
+++ Examples/DeckOfCardRobot/DOCR_Registration.cxx
@@ -1,3 +1,20 @@
+/*=========================================================================
+
+Program:   Image Guided Surgery Software Toolkit
+Module:    DOCR_Registration.cxx
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) ISIS Georgetown University. All rights reserved.
+See IGSTKCopyright.txt or http://www.igstk.org/HTML/Copyright.htm for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
 #include "DOCR_Registration.h"
 
 DOCR_Registration::DOCR_Registration( USVolumeType *volume, 
--- Examples/DeckOfCardRobot/DOCR_Registration.h
+++ Examples/DeckOfCardRobot/DOCR_Registration.h
@@ -1,3 +1,20 @@
+/*=========================================================================
+
+Program:   Image Guided Surgery Software Toolkit
+Module:    DOCR_Registration.h
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) ISIS Georgetown University. All rights reserved.
+See IGSTKCopyright.txt or http://www.igstk.org/HTML/Copyright.htm for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
 // ------------------------------------------------------------------------------------
 // Deck of cards robot registration
 // 
