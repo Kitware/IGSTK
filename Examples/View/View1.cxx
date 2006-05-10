949e418e3d8548154bf19df20dcdf3fcefa5e7a7
Luis Ibanez <luis.ibanez@kitware.com>
2006-05-10 11:11:52 -0400

    ENH: Replacing the "Fl::run()" method with the appropriate combination of      calls to RequestSetRefreshRate(), RequestStart() and RequestStop() in      the view class, and the call to PulseGenerator::CheckTimeouts() in the      middle of the event loop.

--- Examples/View/View1.cxx
+++ Examples/View/View1.cxx
@@ -22,8 +22,10 @@
 #endif
 
 // BeginLatex
+// 
 // This example illustrates how to use the \doxygen{View3D} class to display
 // spatial objects.
+//
 // EndLatex
 
 #include <iostream>
@@ -41,14 +43,17 @@ int main( int, char * [] )
   igstk::RealTimeClock::Initialize();
 
   // BeginLatex
+  // 
   // First, a 3D view and other useful data types are defined 
+  //
   // EndLatex
+
   // BeginCodeSnippet
   typedef igstk::View3D            View3DType;
   typedef itk::Logger              LoggerType;
   typedef itk::StdStreamLogOutput  LogOutputType;
-
   // EndCodeSnippet
+
   // logger object created for logging mouse activities
   LoggerType::Pointer   logger = LoggerType::New();
   LogOutputType::Pointer logOutput = LogOutputType::New();
@@ -56,46 +61,58 @@ int main( int, char * [] )
   logger->AddLogOutput( logOutput );
   logger->SetPriorityLevel( itk::Logger::DEBUG );
 
-  //BeginLatex
-  // For debugging purpose, vtk window output can be redirected to a logger, 
-  // using \doxygen{VTKLoggerOutput} as follows.
-  //EndLatex
+  // BeginLatex
   //
-  //BeginCodeSnippet
+  // For debugging purpose, vtk window output can be redirected to a logger,
+  // using \doxygen{VTKLoggerOutput} as follows.
+  //
+  // EndLatex
+
+  // BeginCodeSnippet
   igstk::VTKLoggerOutput::Pointer vtkLoggerOutput = 
                                   igstk::VTKLoggerOutput::New();
   vtkLoggerOutput->OverrideVTKWindow();
   vtkLoggerOutput->SetLogger(logger);  
-  //EndCodeSnippet
+  // EndCodeSnippet
+
   try
     {
-    //BeginLatex
-    //In this example, we would like to display an ellipsoid object.
-    //To carry out this, an ellipsoid spatial object is first instantiated.
-    //EndLatex
-    //BeginCodeSnippet
+    // BeginLatex
+    // 
+    // In this example, we would like to display an ellipsoid object.  To carry
+    // out this, an ellipsoid spatial object is first instantiated.
+    // 
+    // EndLatex
+
+    // BeginCodeSnippet
     igstk::EllipsoidObject::Pointer ellipsoid = igstk::EllipsoidObject::New();
     ellipsoid->SetRadius(0.1,0.1,0.1);
-    //EndCodeSnippet
+    // EndCodeSnippet
   
-    //BeginLatex
-    //Next, a representation object is created using 
-    //\doxygen{EllipsoidObjectRepresentation } class.  The  
-    //representation class provides the mechanism to generate graphical 
-    //description of the spatial object for visualization in a VTK scene. 
+    // BeginLatex
+    // 
+    // Next, a representation object is created using
+    // \doxygen{EllipsoidObjectRepresentation } class. The  representation
+    // class provides the mechanism to generate graphical description of the
+    // spatial object for visualization in a VTK scene. 
     //
     //EndLatex 
-    //BeginCodeSnippet 
+
+    // BeginCodeSnippet 
     igstk::EllipsoidObjectRepresentation::Pointer ellipsoidRepresentation = 
                                   igstk::EllipsoidObjectRepresentation::New();
     ellipsoidRepresentation->RequestSetEllipsoidObject( ellipsoid );
     ellipsoidRepresentation->SetColor(0.0,1.0,0.0);
     ellipsoidRepresentation->SetOpacity(1.0);
-    //EndCodeSnippet
-    //BeginLatex
-    //Geometrical transformation can be applied to the ellipsoid spatial object
-    //as follows.
-    //EndLatex
+    // EndCodeSnippet
+
+    // BeginLatex
+    // 
+    // Geometrical transformation can be applied to the ellipsoid spatial
+    // object as follows.
+    // 
+    // EndLatex
+
     // BeginCodeSnippet
     const double validityTimeInMilliseconds = 1e300; // 100 seconds
     igstk::Transform transform;
@@ -110,38 +127,86 @@ int main( int, char * [] )
     transform.SetTranslationAndRotation( 
         translation, rotation, errorValue, validityTimeInMilliseconds );
     ellipsoid->RequestSetTransform( transform );
-    //EndCodeSnippet
+    // EndCodeSnippet
+
+    // BeginLatex
+    // 
+    // Next, FLTK window and a view object is instantiated.
     //
-    //BeginLatex
-    //Next, FLTK window and a view object is instantiated.
-    //EndLatex
-    //BeginCodeSnippet
+    // EndLatex
+
+    // BeginCodeSnippet
     Fl_Window * form = new Fl_Window(601,301,"View Test");
     View3DType * view3D = new View3DType(310,10,280,280,"3D View");
     form->end();
     form->show();
-    //EndCodeSnippet
+    // EndCodeSnippet
     
     view3D->RequestResetCamera();
     view3D->RequestEnableInteractions();
    
-    //BeginLatex
-    //The ellispoid is added to the scene using \code{RequestAddObject} method.
-    //EndLatex
-    //BeginCodeSnippet
-    view3D->RequestAddObject( ellipsoidRepresentation );
-    //EndCodeSnippet
-    //BeginLatex
+    // BeginLatex
+    // 
+    // The ellispoid is added to the scene using \code{RequestAddObject}
+    // method.
     //
-    //EndLatex
-    //BeginCodeSnippet
+    // EndLatex
+
+    // BeginCodeSnippet
+    view3D->RequestAddObject( ellipsoidRepresentation );
+    // EndCodeSnippet
+
+    // BeginLatex
+    //
+    // The View components are designed for refreshing their representation at
+    // regular intervals. The application developer must set the desired
+    // refresh rate in Hertz and should trigger the start of the process of
+    // internal generation of pulses that makes possible for the View class to
+    // refresh itself.
+    // 
+    // EndLatex
+
+    // BeginCodeSnippet
+    view3D->RequestSetRefreshRate( 30 );
+    view3D->RequestStart();
+    // EndCodeSnippet
+
+
+    // BeginLatex
+    // 
+    // At this point it is now possible to start the event loop that will drive
+    // the user interaction of the application. Inside the loop it is of
+    // fundamental importance to invoke the call to the
+    // \doxygen{PulseGenerator} method \code{CheckTimeouts()}. This methods
+    // ensures that the timers of pulse generators in all the autonomous IGSTK
+    // classes are checked to see if they should trigger timer events. The same
+    // for loop should have some form of wait or sleep instruction in order to
+    // prevent the loop from taking over the CPU time.
+    //
+    // EndLatex
+
+    // BeginCodeSnippet
     for(unsigned int i=0; i<10; i++)
       {
-      view3D->Update();  // schedule redraw of the view
+      Fl::wait( 0.01 );
+      igstk::PulseGenerator::CheckTimeouts();
       Fl::check();       // trigger FLTK redraws
       }
-    //EndCodeSnippet
+    // EndCodeSnippet
+
+
+    // BeginLatex
+    // 
+    // Once the event loop finishes, we should stop the refresh process of the
+    // view class, by calling the method \code{RequestStop()}.
     //
+    // EndLatex
+
+
+    // BeginCodeSnippet
+    view3D->RequestStop();
+    // EndCodeSnippet
+
     delete view3D;
     delete form;
     }
