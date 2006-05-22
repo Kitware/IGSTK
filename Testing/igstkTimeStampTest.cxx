54c78dbd2e13c265dcc1bb07beefe0c3712ada2f
Luis Ibanez <luis.ibanez@kitware.com>
2006-05-22 10:06:42 -0400

    BUG: 2914. Inconsitency between usage of seconds and milliseconds units in            the TimeStamp and PulseGenerator classes.

--- Testing/igstkTimeStampTest.cxx
+++ Testing/igstkTimeStampTest.cxx
@@ -16,7 +16,8 @@
 =========================================================================*/
 
 #if defined(_MSC_VER)
-   //Warning about: identifier was truncated to '255' characters in the debug information (MVC6.0 Debug)
+//  Warning about: identifier was truncated to '255' characters in the 
+// debug information (MVC6.0 Debug)
 #pragma warning( disable : 4786 )
 #endif
 
@@ -25,10 +26,9 @@
 #include "igstkTimeStamp.h"
 
 int igstkTimeStampTest( int, char * [] )
-{
-
+{ 
   igstk::RealTimeClock::Initialize();
-
+  igstk::RealTimeClock::Print(std::cout);
 
   try
     {
@@ -42,163 +42,181 @@ int igstkTimeStampTest( int, char * [] )
     
     std::cout << "Testing the first Set" << std::endl;
 
-    { // convenience block for local variable declarations.
+      { // convenience block for local variable declarations.
 
-    stamp.SetStartTimeNowAndExpireAfter( millisecondsToExpire );
+      stamp.SetStartTimeNowAndExpireAfter( millisecondsToExpire );
 
-    std::cout << stamp << std::endl;
+      std::cout << stamp << std::endl;
 
-    const double startingTime   = stamp.GetStartTime();
-    const double expirationTime = stamp.GetExpirationTime();
+      const double startingTime   = stamp.GetStartTime();
+      const double expirationTime = stamp.GetExpirationTime();
     
-    const double expectedExpirationTime = startingTime + millisecondsToExpire;
+      const double expectedExpirationTime = startingTime + millisecondsToExpire;
 
-    if( fabs( expectedExpirationTime - expirationTime ) > tolerance )
-      {
-      std::cerr << "Error in expiration time" << std::endl;
-      std::cerr << "Expected expiration time = " << expectedExpirationTime << std::endl;
-      std::cerr << "Actual   expiration time = " << expirationTime << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( fabs( expectedExpirationTime - expirationTime ) > tolerance )
+        {
+        std::cerr << "Error in expiration time" << std::endl;
+        std::cerr << "Expected expiration time = ";
+        std::cerr << expectedExpirationTime << std::endl;
+        std::cerr << "Actual   expiration time = ";
+        std::cerr << expirationTime << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    const double scheduledRenderTime1 = startingTime - 1000.0;
-    const double scheduledRenderTime2 = startingTime - 1.0;
-    const double scheduledRenderTime3 = startingTime + 1.0;
-    const double scheduledRenderTime4 = startingTime + millisecondsToExpire / 2.0;
-    const double scheduledRenderTime5 = expirationTime - 1.0;
-    const double scheduledRenderTime6 = expirationTime + 1.0;
-    const double scheduledRenderTime7 = expirationTime + 1000.0;
+      const double scheduledRenderTime1 = startingTime - 1000.0;
+      const double scheduledRenderTime2 = startingTime - 1.0;
+      const double scheduledRenderTime3 = startingTime + 1.0;
+      const double scheduledRenderTime4 = startingTime 
+                                          + millisecondsToExpire / 2.0;
+      const double scheduledRenderTime5 = expirationTime - 1.0;
+      const double scheduledRenderTime6 = expirationTime + 1.0;
+      const double scheduledRenderTime7 = expirationTime + 1000.0;
 
-    if( stamp.IsValidAtTime( scheduledRenderTime1 ) )
-      {
-      std::cerr << "Error in IsValidAtTime()." << std::endl; 
-      std::cerr << "Expected value was 'false', but returned 'true'" << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( stamp.IsValidAtTime( scheduledRenderTime1 ) )
+        {
+        std::cerr << "Error in IsValidAtTime()." << std::endl; 
+        std::cerr << "Expected value was 'false',";
+        std::cerr << "but returned 'true'" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    if( stamp.IsValidAtTime( scheduledRenderTime2 ) )
-      {
-      std::cerr << "Error in IsValidAtTime()." << std::endl; 
-      std::cerr << "Expected value was 'false', but returned 'true'" << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( stamp.IsValidAtTime( scheduledRenderTime2 ) )
+        {
+        std::cerr << "Error in IsValidAtTime()." << std::endl; 
+        std::cerr << "Expected value was 'false',";
+        std::cerr << "but returned 'true'" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    if( !stamp.IsValidAtTime( scheduledRenderTime3 ) )
-      {
-      std::cerr << "Error in IsValidAtTime()." << std::endl; 
-      std::cerr << "Expected value was 'true', but returned 'false'" << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( !stamp.IsValidAtTime( scheduledRenderTime3 ) )
+        {
+        std::cerr << "Error in IsValidAtTime()." << std::endl; 
+        std::cerr << "Expected value was 'true',";
+        std::cerr << "but returned 'false'" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    if( !stamp.IsValidAtTime( scheduledRenderTime4 ) )
-      {
-      std::cerr << "Error in IsValidAtTime()." << std::endl; 
-      std::cerr << "Expected value was 'true', but returned 'false'" << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( !stamp.IsValidAtTime( scheduledRenderTime4 ) )
+        {
+        std::cerr << "Error in IsValidAtTime()." << std::endl; 
+        std::cerr << "Expected value was 'true',";
+        std::cerr << "but returned 'false'" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    if( !stamp.IsValidAtTime( scheduledRenderTime5 ) )
-      {
-      std::cerr << "Error in IsValidAtTime()." << std::endl; 
-      std::cerr << "Expected value was 'true', but returned 'false'" << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( !stamp.IsValidAtTime( scheduledRenderTime5 ) )
+        {
+        std::cerr << "Error in IsValidAtTime()." << std::endl; 
+        std::cerr << "Expected value was 'true',";
+        std::cerr << "but returned 'false'" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    if( stamp.IsValidAtTime( scheduledRenderTime6 ) )
-      {
-      std::cerr << "Error in IsValidAtTime()." << std::endl; 
-      std::cerr << "Expected value was 'false', but returned 'true'" << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( stamp.IsValidAtTime( scheduledRenderTime6 ) )
+        {
+        std::cerr << "Error in IsValidAtTime()." << std::endl; 
+        std::cerr << "Expected value was 'false',";
+        std::cerr << "but returned 'true'" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    if( stamp.IsValidAtTime( scheduledRenderTime7 ) )
-      {
-      std::cerr << "Error in IsValidAtTime()." << std::endl; 
-      std::cerr << "Expected value was 'false', but returned 'true'" << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( stamp.IsValidAtTime( scheduledRenderTime7 ) )
+        {
+        std::cerr << "Error in IsValidAtTime()." << std::endl; 
+        std::cerr << "Expected value was 'false',";
+        std::cerr << "but returned 'true'" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    } // end of first block
+      } // end of first block
     
     std::cout << "Testing the second Set" << std::endl;
 
-    { // convenience block for local variable declarations.
+      { // convenience block for local variable declarations.
 
-    stamp.SetStartTimeNowAndExpireAfter( millisecondsToExpire );
+      stamp.SetStartTimeNowAndExpireAfter( millisecondsToExpire );
 
-    const double startingTime   = stamp.GetStartTime();
-    const double expirationTime = stamp.GetExpirationTime();
+      const double startingTime   = stamp.GetStartTime();
+      const double expirationTime = stamp.GetExpirationTime();
     
-    const double expectedExpirationTime = startingTime + millisecondsToExpire;
+      const double expectedExpirationTime = startingTime + millisecondsToExpire;
 
-    if( fabs( expectedExpirationTime - expirationTime ) > tolerance )
-      {
-      std::cerr << "Error in expiration time" << std::endl;
-      std::cerr << "Expected expiration time = " << expectedExpirationTime << std::endl;
-      std::cerr << "Actual   expiration time = " << expirationTime << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( fabs( expectedExpirationTime - expirationTime ) > tolerance )
+        {
+        std::cerr << "Error in expiration time" << std::endl;
+        std::cerr << "Expected expiration time = ";
+        std::cerr << expectedExpirationTime << std::endl;
+        std::cerr << "Actual   expiration time = ";
+        std::cerr << expirationTime << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    const double scheduledRenderTime1 = startingTime - 1000.0;
-    const double scheduledRenderTime2 = startingTime - 1.0;
-    const double scheduledRenderTime3 = startingTime + 1.0;
-    const double scheduledRenderTime4 = startingTime + millisecondsToExpire / 2.0;
-    const double scheduledRenderTime5 = expirationTime - 1.0;
-    const double scheduledRenderTime6 = expirationTime + 1.0;
-    const double scheduledRenderTime7 = expirationTime + 1000.0;
+      const double scheduledRenderTime1 = startingTime - 1000.0;
+      const double scheduledRenderTime2 = startingTime - 1.0;
+      const double scheduledRenderTime3 = startingTime + 1.0;
+      const double scheduledRenderTime4 = startingTime 
+                                          + millisecondsToExpire / 2.0;
+      const double scheduledRenderTime5 = expirationTime - 1.0;
+      const double scheduledRenderTime6 = expirationTime + 1.0;
+      const double scheduledRenderTime7 = expirationTime + 1000.0;
 
-    if( stamp.IsValidAtTime( scheduledRenderTime1 ) )
-      {
-      std::cerr << "Error in IsValidAtTime()." << std::endl; 
-      std::cerr << "Expected value was 'false', but returned 'true'" << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( stamp.IsValidAtTime( scheduledRenderTime1 ) )
+        {
+        std::cerr << "Error in IsValidAtTime()." << std::endl; 
+        std::cerr << "Expected value was 'false',";
+        std::cerr << "but returned 'true'" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    if( stamp.IsValidAtTime( scheduledRenderTime2 ) )
-      {
-      std::cerr << "Error in IsValidAtTime()." << std::endl; 
-      std::cerr << "Expected value was 'false', but returned 'true'" << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( stamp.IsValidAtTime( scheduledRenderTime2 ) )
+        {
+        std::cerr << "Error in IsValidAtTime()." << std::endl; 
+        std::cerr << "Expected value was 'false',";
+        std::cerr << "but returned 'true'" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    if( !stamp.IsValidAtTime( scheduledRenderTime3 ) )
-      {
-      std::cerr << "Error in IsValidAtTime()." << std::endl; 
-      std::cerr << "Expected value was 'true', but returned 'false'" << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( !stamp.IsValidAtTime( scheduledRenderTime3 ) )
+        {
+        std::cerr << "Error in IsValidAtTime()." << std::endl; 
+        std::cerr << "Expected value was 'true',";
+        std::cerr << "but returned 'false'" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    if( !stamp.IsValidAtTime( scheduledRenderTime4 ) )
-      {
-      std::cerr << "Error in IsValidAtTime()." << std::endl; 
-      std::cerr << "Expected value was 'true', but returned 'false'" << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( !stamp.IsValidAtTime( scheduledRenderTime4 ) )
+        {
+        std::cerr << "Error in IsValidAtTime()." << std::endl; 
+        std::cerr << "Expected value was 'true',";
+        std::cerr << "but returned 'false'" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    if( !stamp.IsValidAtTime( scheduledRenderTime5 ) )
-      {
-      std::cerr << "Error in IsValidAtTime()." << std::endl; 
-      std::cerr << "Expected value was 'true', but returned 'false'" << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( !stamp.IsValidAtTime( scheduledRenderTime5 ) )
+        {
+        std::cerr << "Error in IsValidAtTime()." << std::endl; 
+        std::cerr << "Expected value was 'true',";
+        std::cerr << "but returned 'false'" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    if( stamp.IsValidAtTime( scheduledRenderTime6 ) )
-      {
-      std::cerr << "Error in IsValidAtTime()." << std::endl; 
-      std::cerr << "Expected value was 'false', but returned 'true'" << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( stamp.IsValidAtTime( scheduledRenderTime6 ) )
+        {
+        std::cerr << "Error in IsValidAtTime()." << std::endl; 
+        std::cerr << "Expected value was 'false',";
+        std::cerr << "but returned 'true'" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    if( stamp.IsValidAtTime( scheduledRenderTime7 ) )
-      {
-      std::cerr << "Error in IsValidAtTime()." << std::endl; 
-      std::cerr << "Expected value was 'false', but returned 'true'" << std::endl;
-      return EXIT_FAILURE;
-      }
+      if( stamp.IsValidAtTime( scheduledRenderTime7 ) )
+        {
+        std::cerr << "Error in IsValidAtTime()." << std::endl; 
+        std::cerr << "Expected value was 'false',";
+        std::cerr << "but returned 'true'" << std::endl;
+        return EXIT_FAILURE;
+        }
 
-    } // end of first block
-
-
+      } // end of first block
     }
   catch(...)
     {
@@ -210,5 +228,3 @@ int igstkTimeStampTest( int, char * [] )
 
   return EXIT_SUCCESS;
 }
-
-
