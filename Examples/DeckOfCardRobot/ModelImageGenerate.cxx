d05e16c99f688827768599ee30eb666fe2c396e4
Andinet Enquobahrie <andinet.enqu@kitware.com>
2006-06-27 15:29:59 -0400

    ENH: small program to generate fiducial model image

---
+++ Examples/DeckOfCardRobot/ModelImageGenerate.cxx
@@ -0,0 +1,50 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    ModelImageGenerate.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISIS Georgetown University. All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+//Warning about: identifier was truncated to '255' characters 
+//in the debug information (MVC6.0 Debug)
+#pragma warning( disable : 4284 )
+#endif
+
+
+#include "itkImageFileWriter.h"
+#include "FiducialModel.h"
+
+int main(int argc , char * argv [] )
+{
+  typedef FiducialModel  FiducialModelType;
+
+  FiducialModelType::Pointer  fiducialModel = FiducialModelType::New();
+    
+  typedef igstk::CTImageSpatialObject::ImageType            ImageType; 
+  ImageType::Pointer image = ImageType::New();
+
+  fiducialModel->GenerateModelImage();
+
+  image = fiducialModel->GetModelImage();
+
+  typedef itk::ImageFileWriter< ImageType > ImageFileWriteType;
+
+  ImageFileWriteType::Pointer   writer = ImageFileWriteType::New();
+
+  writer->SetFileName ( argv[1] ) ;
+  writer->SetInput ( image ); 
+  writer->Update();  
+
+  return EXIT_SUCCESS;
+}
+
