882abc52b504e320e9c808276a8c04989534e304
Patrick Cheng <cheng@isis.georgetown.edu>
2006-06-29 12:51:53 -0400

    ENH: Add segmentation of fiducial and sub-volume, and calculate the initial transform from the PCA axis of both fiducial points and model points

--- Examples/DeckOfCardRobot/ModelBasedRegistration.cxx
+++ Examples/DeckOfCardRobot/ModelBasedRegistration.cxx
@@ -15,7 +15,13 @@ PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
 #include "ModelBasedRegistration.h"
-
+#include "FiducialSegmentation.h"
+#include "FiducialModel.h"
+#include "ModelBasedClustering.h"
+#include "itkRegionOfInterestImageFilter.h"
+#include "PCAOnPoints.h"
+#include "igstkLandmark3DRegistration.h"
+#include "itkVersorRigid3DTransform.h"
 
 ModelBasedRegistration::ModelBasedRegistration()
 {
@@ -28,6 +34,123 @@ void ModelBasedRegistration::PrintSelf(std::ostream& os, itk::Indent indent)
 }
 
 bool ModelBasedRegistration::Execute()
-{   
+{
+  FiducialSegmentation::PointsListType     fiducialPoints;
+  FiducialSegmentation::PointsListType   modelPoints;
+
+  // Segment the fiducial points
+  FiducialSegmentation::Pointer segmenter = FiducialSegmentation::New();
+  segmenter->SetITKImage( m_ITKImage );
+  segmenter->SetThreshold( 3000 );
+  segmenter->SetMaxSize( 20 );
+  segmenter->SetMinSize( 0 );
+  segmenter->SetMergeDistance( 1 );
+  segmenter->Execute();
+  fiducialPoints = segmenter->GetFiducialPoints();
+
+  // Generate the model points
+  FiducialModel::Pointer model = FiducialModel::New();
+  modelPoints = model->GetFiducialPoints();
+
+  // Cluster the points, eliminate outliers
+  ModelBasedClustering::Pointer cluster = ModelBasedClustering::New();
+  cluster->SetSamplePoints( fiducialPoints );
+  cluster->SetModelPoints( modelPoints );
+  cluster->Execute();
+  fiducialPoints = cluster->GetClusteredPoints();
+
+  // Calculate the bounding box of the current fiducial points
+  const double Margin = 5;
+  vnl_vector< double > minXYZ(3), maxXYZ(3);
+  minXYZ.fill( itk::NumericTraits<double>::max() );
+  maxXYZ.fill( itk::NumericTraits<double>::NonpositiveMin() );
+  for ( int i=0; i<fiducialPoints.size(); i++)
+    {
+    for ( int j=0; j<3; j++)
+      {
+      minXYZ(j) = vnl_math_min( minXYZ(j), fiducialPoints[i][j] );
+      maxXYZ(j) = vnl_math_max( maxXYZ(j), fiducialPoints[i][j] );
+      }
+    }
+  minXYZ -= Margin;
+  maxXYZ += Margin;
+  std::cout<< "Physical bounds: " << std::endl;
+  std::cout<< minXYZ << std::endl;
+  std::cout<< maxXYZ << std::endl;
+
+  ImageType::RegionType roi;
+  ImageType::IndexType  startIndex, endIndex;
+  ImageType::SizeType   size;
+  FiducialSegmentation::PointType p;
+  p[0] = minXYZ[0];  p[1] = minXYZ[1];  p[2] = minXYZ[2];
+  m_ITKImage->TransformPhysicalPointToIndex( p, startIndex );
+  p[0] = maxXYZ[0];  p[1] = maxXYZ[1];  p[2] = maxXYZ[2];
+  m_ITKImage->TransformPhysicalPointToIndex( p, endIndex );
+  size[0] = endIndex[0] - startIndex[0];
+  size[1] = endIndex[1] - startIndex[1];
+  size[2] = endIndex[2] - startIndex[2];
+
+  roi.SetIndex( startIndex );
+  roi.SetSize( size );
+
+  typedef itk::RegionOfInterestImageFilter< ImageType, ImageType > 
+  ROIFilterType;
+  ROIFilterType::Pointer roiFilter = ROIFilterType::New();
+  roiFilter->SetInput( m_ITKImage );
+  roiFilter->SetRegionOfInterest( roi );
+  roiFilter->Update();
+
+  // PCA on the points, get the initial transform
+  PCAOnPoints::Pointer pca = PCAOnPoints::New();
+  pca->SetSamplePoints( fiducialPoints );
+  pca->Execute();
+  PCAOnPoints::PointType  fiducialCenter = pca->GetCenter();
+  PCAOnPoints::VectorType fiducialAxis   = pca->GetPrincipleAxis();
+  std::cout<< "Centroid of the points: " << fiducialCenter << std::endl;
+  std::cout<< "Principle axis: " << fiducialAxis << std::endl;
+
+  pca->SetSamplePoints( modelPoints );
+  pca->Execute();
+  PCAOnPoints::PointType  modelCenter = pca->GetCenter();
+  PCAOnPoints::VectorType modelAxis   = pca->GetPrincipleAxis();
+  std::cout<< "Centroid of the points: " << modelCenter << std::endl;
+  std::cout<< "Principle axis: " << modelAxis << std::endl;
+
+  // Construct the initial transform
+  igstk::Transform               transform;
+  igstk::Transform::VersorType   rotation;
+  igstk::Transform::VectorType   translation, fAxis, mAxis, axis;
+  double                         angle;
+
+  for (int i=0; i<3; i++)
+    {
+    fAxis[i] = fiducialAxis[i];
+    mAxis[i] = modelAxis[i];
+    }
+  
+  if ( fAxis * mAxis < 0)
+    {
+    fAxis *= -1;
+    }
+
+  angle = acos( fAxis * mAxis );
+  axis = itk::CrossProduct( mAxis, fAxis );
+  axis.Normalize();
+
+  // Compose the new robot transform
+  for (int i=0; i<3; i++)
+    {
+    translation[i] = fiducialCenter[i] - modelCenter[i];
+    }
+
+  // Translate angle-axis into quaternion
+  rotation.Set( axis, angle );
+  rotation.Normalize();
+
+  transform.SetTranslationAndRotation( translation, rotation, 0.1, 1e300 );
+
+  
+
+
   return true;
 }
