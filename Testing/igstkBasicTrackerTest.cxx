51e97f235138cd6f3afb82e2e17294c41a376240
Julien Jomier <julien.jomier@kitware.com>
2006-10-29 19:20:28 -0500

    STYLE: Fixed style

--- Testing/igstkBasicTrackerTest.cxx
+++ Testing/igstkBasicTrackerTest.cxx
@@ -16,7 +16,8 @@
 =========================================================================*/
 
 #if defined(_MSC_VER)
-   //Warning about: identifier was truncated to '255' characters in the debug information (MVC6.0 Debug)
+// Warning about: identifier was truncated to '255' characters in the 
+// debug information (MVC6.0 Debug)
 #pragma warning( disable : 4786 )
 #endif
 
@@ -32,173 +33,175 @@
 
 namespace igstk
 {
-  class TestingTracker : public igstk::Tracker
-  {
-    public:
-      typedef TestingTracker                 Self;
-      typedef itk::SmartPointer<Self>        Pointer;
-      typedef itk::SmartPointer<const Self>  ConstPointer;
-      typedef igstk::Tracker::TransformType  TransformType;
-      typedef TransformType::VectorType      VectorType;
+  
+class TestingTracker : public igstk::Tracker
+{
+public:
+  typedef TestingTracker                 Self;
+  typedef itk::SmartPointer<Self>        Pointer;
+  typedef itk::SmartPointer<const Self>  ConstPointer;
+  typedef igstk::Tracker::TransformType  TransformType;
+  typedef TransformType::VectorType      VectorType;
 
-      igstkNewMacro(Self);
-      igstkTypeMacro(TestingTracker,Tracker);
+  igstkNewMacro(Self);
+  igstkTypeMacro(TestingTracker,Tracker);
 
-      void SetTranslation(VectorType &translation)
-        {
-        m_Translation = translation;
-        }
+  void SetTranslation(VectorType &translation)
+    {
+    m_Translation = translation;
+    }
 
-      bool TestThreadingEnableMethods()
-        {
-        this->SetThreadingEnabled(true);
-        if( !this->GetThreadingEnabled() )
-          {
-          return false;
-          }
-        this->SetThreadingEnabled(false);
-        if( this->GetThreadingEnabled() )
-          {
-          return false;
-          }
-        return true; 
-        }
-    protected:
+  bool TestThreadingEnableMethods()
+    {
+    this->SetThreadingEnabled(true);
+    if( !this->GetThreadingEnabled() )
+      {
+      return false;
+      }
+    this->SetThreadingEnabled(false);
+    if( this->GetThreadingEnabled() )
+      {
+      return false;
+      }
+    return true; 
+    }
 
-      TestingTracker()
-        {
-        typedef TrackerTool  TrackerToolType;
-        typedef TrackerPort  TrackerPortType;
-        TrackerToolType::Pointer trackerTool = TrackerToolType::New();
-        TrackerPortType::Pointer trackerPort = TrackerPortType::New();
-        trackerPort->AddTool( trackerTool );
-        this->AddPort( trackerPort );
+protected:
 
-        TrackerToolType::Pointer trackerTool2 = TrackerToolType::New();
-        TrackerPortType::Pointer trackerPort2 = TrackerPortType::New();
-        trackerPort2->AddTool( trackerTool2 );
-        this->AddPort( trackerPort2 );
-        igstk::Transform::VectorType translation;
-        igstk::Transform::VersorType rotation;
-        igstk::Transform transform;
-        translation[0] = -100;
-        translation[1] = -100;
-        translation[2] = -100;
-        rotation.SetRotationAroundZ(0.1);
-        transform.SetTranslationAndRotation(translation, rotation, 0.1, 10000);
-        SetToolTransform(1, 0, transform);
-        m_Translation = translation;
-        }
+  TestingTracker()
+    {
+    typedef TrackerTool  TrackerToolType;
+    typedef TrackerPort  TrackerPortType;
+    TrackerToolType::Pointer trackerTool = TrackerToolType::New();
+    TrackerPortType::Pointer trackerPort = TrackerPortType::New();
+    trackerPort->AddTool( trackerTool );
+    this->AddPort( trackerPort );
 
-      ~TestingTracker() 
-        {
-        this->ClearPorts();
-        }
+    TrackerToolType::Pointer trackerTool2 = TrackerToolType::New();
+    TrackerPortType::Pointer trackerPort2 = TrackerPortType::New();
+    trackerPort2->AddTool( trackerTool2 );
+    this->AddPort( trackerPort2 );
+    igstk::Transform::VectorType translation;
+    igstk::Transform::VersorType rotation;
+    igstk::Transform             transform;
+    translation[0] = -100;
+    translation[1] = -100;
+    translation[2] = -100;
+    rotation.SetRotationAroundZ(0.1);
+    transform.SetTranslationAndRotation(translation, rotation, 0.1, 10000);
+    SetToolTransform(1, 0, transform);
+    m_Translation = translation;
+    }
 
-      Tracker::ResultType InternalOpen( void )
-        {
-        static bool success = false;
-        if( !success )
-          {
-          success = true;
-          return FAILURE;
-          }
-        return SUCCESS;
-        }
+  ~TestingTracker() 
+    {
+    this->ClearPorts();
+    }
 
-      Tracker::ResultType InternalClose( void )
-        {
-        static bool success = false;
-        if( !success )
-          {
-          success = true;
-          return FAILURE;
-          }
-        return SUCCESS;
-        }
+  Tracker::ResultType InternalOpen( void )
+    {
+    static bool success = false;
+    if( !success )
+      {
+      success = true;
+      return FAILURE;
+      }
+    return SUCCESS;
+    }
 
-      Tracker::ResultType InternalReset( void )
-        {
-        static bool success = false;
-        if( !success )
-          {
-          success = true;
-          return FAILURE;
-          }
-        return SUCCESS;
-        }
+  Tracker::ResultType InternalClose( void )
+    {
+    static bool success = false;
+    if( !success )
+      {
+      success = true;
+      return FAILURE;
+      }
+    return SUCCESS;
+    }
 
-      Tracker::ResultType InternalActivateTools( void )
-        {
-        static bool success = false;
-        if( !success )
-          {
-          success = true;
-          return FAILURE;
-          }
-        return SUCCESS;
-        }
+  Tracker::ResultType InternalReset( void )
+    {
+    static bool success = false;
+    if( !success )
+      {
+      success = true;
+      return FAILURE;
+      }
+    return SUCCESS;
+    }
 
-      Tracker::ResultType InternalDeactivateTools( void )
-        {
-        static bool success = false;
-        if( !success )
-          {
-          success = true;
-          return FAILURE;
-          }
-        return SUCCESS;
-        }
+  Tracker::ResultType InternalActivateTools( void )
+    {
+    static bool success = false;
+    if( !success )
+      {
+      success = true;
+      return FAILURE;
+      }
+    return SUCCESS;
+    }
 
-      Tracker::ResultType InternalStartTracking( void )
-        {
-        static bool success = false;
-        if( !success )
-          {
-          success = true;
-          return FAILURE;
-          }
-        return SUCCESS;
-        }
+  Tracker::ResultType InternalDeactivateTools( void )
+    {
+    static bool success = false;
+    if( !success )
+      {
+      success = true;
+      return FAILURE;
+      }
+    return SUCCESS;
+    }
 
-      Tracker::ResultType InternalStopTracking( void )
-        {
-        static bool success = false;
-        if( !success )
-          {
-          success = true;
-          return FAILURE;
-          }
-        return SUCCESS;
-        }
+  Tracker::ResultType InternalStartTracking( void )
+    {
+    static bool success = false;
+    if( !success )
+      {
+      success = true;
+      return FAILURE;
+      }
+    return SUCCESS;
+    }
 
-      Tracker::ResultType InternalUpdateStatus( void )
-        {
-        static bool success = false;
-        if( !success )
-          {
-          success = true;
-          return FAILURE;
-          }
+  Tracker::ResultType InternalStopTracking( void )
+    {
+    static bool success = false;
+    if( !success )
+      {
+      success = true;
+      return FAILURE;
+      }
+    return SUCCESS;
+    }
 
-        const unsigned int toolNumber = 0;
-        const unsigned int portNumber = 0;
-        const double validityPeriod = 500.0; // valid for 500 milliseconds.
+  Tracker::ResultType InternalUpdateStatus( void )
+    {
+    static bool success = false;
+    if( !success )
+      {
+      success = true;
+      return FAILURE;
+      }
 
-        TransformType transform;
-        TransformType::ErrorType errorValue = 0.01; // 10 microns
+    const unsigned int toolNumber = 0;
+    const unsigned int portNumber = 0;
+    const double validityPeriod = 500.0; // valid for 500 milliseconds.
 
-        transform.SetTranslation( m_Translation, errorValue, validityPeriod );
-        this->SetToolTransform( toolNumber, portNumber, transform );
+    TransformType transform;
+    TransformType::ErrorType errorValue = 0.01; // 10 microns
 
-        return SUCCESS;
-        }
+    transform.SetTranslation( m_Translation, errorValue, validityPeriod );
+    this->SetToolTransform( toolNumber, portNumber, transform );
 
-    private:
+    return SUCCESS;
+    }
 
-      igstk::Transform::VectorType m_Translation;
+private:
+
+  igstk::Transform::VectorType m_Translation;
  
-  };
+};
 }   // namespace igstk
 
 int igstkBasicTrackerTest( int, char * [] )
@@ -234,7 +237,8 @@ int igstkBasicTrackerTest( int, char * [] )
   if( status == false )
     {
     std::cerr << " ERROR: inconsitency between ";
-    std::cerr << "SetThreadingEnabled() and GetThreadingEnabled() " << std::endl; 
+    std::cerr << "SetThreadingEnabled() and GetThreadingEnabled() " 
+              << std::endl; 
     return EXIT_FAILURE;
     }
 
@@ -258,7 +262,8 @@ int igstkBasicTrackerTest( int, char * [] )
   translation[1] = -4;
   translation[2] = -6;
   rotation.SetRotationAroundX(-1.0);
-  toolCalibrationTransform.SetTranslationAndRotation(translation, rotation, 0.1, 10000);
+  toolCalibrationTransform.SetTranslationAndRotation(translation, rotation, 
+                                                                   0.1, 10000);
   tracker->SetToolCalibrationTransform(0, 0, toolCalibrationTransform);
   toolCalibrationTransform.SetToIdentity(10000);
   toolCalibrationTransform = tracker->GetToolCalibrationTransform(0, 0);
@@ -270,10 +275,12 @@ int igstkBasicTrackerTest( int, char * [] )
   unsigned int refPort;
   bool bApplyRefTool;
   bApplyRefTool = tracker->GetReferenceTool(refPort, refTool);
-  std::cout << "ReferenceTool flag : " << bApplyRefTool << "\t port: " << refPort << ", tool: " << refTool << std::endl;
+  std::cout << "ReferenceTool flag : " << bApplyRefTool << "\t port: " 
+            << refPort << ", tool: " << refTool << std::endl;
   if( bApplyRefTool != true  ||  refTool != 0  ||  refPort != 1 )
     {
-    std::cerr << "SetReferenceTool() or GetReferenceTool() doesn't work correctly!" << std::endl;
+    std::cerr << "SetReferenceTool() or GetReferenceTool() doesn't"
+              << " work correctly!" << std::endl;
     return EXIT_FAILURE;
     }
 
@@ -337,4 +344,3 @@ int igstkBasicTrackerTest( int, char * [] )
   std::cout << "[PASSED]" << std::endl;
   return EXIT_SUCCESS;
 }
-
