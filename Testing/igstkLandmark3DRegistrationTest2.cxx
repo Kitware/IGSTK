f9841b39ad2bac5564fb8269db592d648d7d2671
Andinet Enquobahrie <andinet.enqu@kitware.com>
2007-01-30 04:07:15 -0500

    STYLE: add copyright header

--- Testing/igstkLandmark3DRegistrationTest2.cxx
+++ Testing/igstkLandmark3DRegistrationTest2.cxx
@@ -1,3 +1,20 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkLandmark3DRegistrationTest2.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISIS Georgetown University. All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
 #if defined(_MSC_VER)
 #pragma warning( disable : 4786 )
 #endif
