420947c4272f9624cfa3e5da89fe358ef25bc0f9
Andinet Enquobahrie <andinet.enqu@kitware.com>
2007-06-13 14:10:57 -0400

    BUG: 5179 add example to test IGSTK build/installation

---
+++ Examples/Installation/igstkImageReader.cxx
@@ -0,0 +1,59 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkImageReader.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISC  Insight Software Consortium.  All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+//  The following code is an implementation of a small igstk
+//  program. It tests including header files and linking with IGSTK
+//  library.
+/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "igstkImageReader.h"
+#include "igstkImageSpatialObject.h"
+
+int main( int , char* [] )
+{
+
+  igstk::RealTimeClock::Initialize();
+
+  typedef short    PixelType;
+  const unsigned int Dimension = 3;
+
+  typedef igstk::ImageSpatialObject< PixelType, Dimension > 
+                                                 ImageSpatialObjectType;
+  
+  class myTestReader : 
+    public igstk::ImageReader< ImageSpatialObjectType >
+    {
+  public:
+    typedef myTestReader                                 Self;
+    typedef igstk::ImageReader< ImageSpatialObjectType > Superclass;
+    igstkNewMacro( Self );
+  private:
+    typedef Superclass::ImageType ImageType;
+    virtual const ImageType * GetITKImage() const { return NULL; }
+    };
+
+  typedef myTestReader   ReaderType;
+
+  ReaderType::Pointer  reader = ReaderType::New();
+
+  reader->Print( std::cout );
+
+  return EXIT_SUCCESS;
+}
