c273295c231922401382ef3386193b8d79289a46
Andinet Enquobahrie <andinet.enqu@kitware.com>
2007-09-06 16:42:21 -0400

    ENH: re-check in c++ wrapper classes for the MTC library

---
+++ Utilities/MicronTracker/src/Vector.h
@@ -0,0 +1,36 @@
+/**************************************************************
+*
+*     Micron Tracker: Example C++ wrapper and Multi-platform demo
+*   
+*     Written by: 
+*     Shahram Izadyar, Robarts Research Institute - London- Ontario , www.robarts.ca
+*     Claudio Gatti, Claron Technology - Toronto -Ontario, www.clarontech.com
+*
+*     Copyright Claron Technology 2000-2003
+*
+***************************************************************/
+#ifndef __VECTOR_H__
+#define __VECTOR_H__
+
+#include "MTC.h"
+
+class Vector
+{
+public:
+  Vector(int h =0);
+  ~Vector();
+  int Handle();
+
+  int getEndPos2x3(double* result);
+  int setEndPos2x3(double* newPos2x3);
+  int getEndXPoints(double* result);
+
+
+
+private:
+  int m_handle;
+  bool ownedByMe;
+};
+
+#endif
+
