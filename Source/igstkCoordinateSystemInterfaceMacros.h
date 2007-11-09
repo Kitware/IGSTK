e6479474d7792a7129558db65fd14677f9a5c6fb
Matt Turek <matt.turek@kitware.com>
2007-11-08 19:09:47 -0500

    NEW: Contains macros which define the coordinate system API.

---
+++ Source/igstkCoordinateSystemInterfaceMacros.h
@@ -0,0 +1,69 @@
+
+#ifndef __igstkCoordinateSystemInterfaceMacros_h
+#define __igstkCoordinateSystemInterfaceMacros_h
+
+namespace igstk 
+{
+
+/**
+ *  igstkCoordinateSystemClassInterfaceMacro defines the coordinate system 
+ *  API used for managing spatial relationships between objects.
+ *  To keep the interface definition common & centralized, it has been 
+ *  placed in a macro.
+ *
+ *  If an object uses igstkCoordinateSystemClassInterfaceMacro() in 
+ *  its header file, igstkCoordinateSystemClassInterfaceConstructorMacro 
+ *  must be called in that object's constructor in order to initialize
+ *  objects declared by this macro.
+ */
+#define igstkCoordinateSystemClassInterfaceMacro() \
+public: \
+  template <class TTarget> \
+  void RequestComputeTransformTo(const TTarget * target) \
+    { \
+    m_CoordinateReferenceSystemDelegator->RequestComputeTransformTo(target);\
+    } \
+  void RequestGetTransformToParent() \
+    { \
+    m_CoordinateReferenceSystemDelegator->RequestGetTransformToParent(); \
+    } \
+  template < class TParent > \
+  void RequestSetTransformAndParent( const Transform & transformToParent, \
+                                     const TParent * parent ) \
+    { \
+    m_CoordinateReferenceSystemDelegator->RequestSetTransformAndParent( \
+                                     transformToParent, parent); \
+    } \
+private: \
+  CoordinateReferenceSystemDelegator::Pointer \
+                                  m_CoordinateReferenceSystemDelegator; \
+  typedef ::itk::ReceptorMemberCommand< Self >   ReceptorObserverType;  \
+  ReceptorObserverType::Pointer m_CoordinateReferenceSystemObserver; \
+  void ObserverCallback(const ::itk::EventObject & eventvar) \
+    { \
+    this->InvokeEvent( eventvar ); \
+    } \
+  const CoordinateReferenceSystem* GetCoordinateReferenceSystem() const \
+    { \
+    return \
+      igstk::Friends::CoordinateReferenceSystemHelper:: \
+        GetCoordinateReferenceSystem(  \
+                       m_CoordinateReferenceSystemDelegator.GetPointer() ); \
+    } \
+  igstkFriendClassMacro( igstk::Friends::CoordinateReferenceSystemHelper ); 
+
+/**
+ *  igstkCoordinateSystemClassInterfaceConstructorMacro initializes the 
+ *  observer and delegator used in the coordinate system API. This macro
+ *  should be called from the constructor of an object which has 
+ *  igstkCoordinateSystemClassInterfaceMacro in its class header.
+ */
+#define igstkCoordinateSystemClassInterfaceConstructorMacro() \
+  m_CoordinateReferenceSystemObserver = ReceptorObserverType::New(); \
+  m_CoordinateReferenceSystemDelegator = \
+                          CoordinateReferenceSystemDelegator::New(); \
+  m_CoordinateReferenceSystemDelegator->RequestSetReporter( this );
+
+}
+
+#endif // #ifndef __igstkCoordinateSystemInterfaceMacros_h
