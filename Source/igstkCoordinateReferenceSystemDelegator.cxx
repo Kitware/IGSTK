5f0095c02c106084507ac553cb9a6eafbcaa15ca
Matt Turek <matt.turek@kitware.com>
2007-11-08 19:11:49 -0500

    NEW: Contains a class which packages up the typical coordinate system functionality needed by a high-level object like SpatialObject or View.

---
+++ Source/igstkCoordinateReferenceSystemDelegator.cxx
@@ -0,0 +1,105 @@
+
+#include "igstkCoordinateReferenceSystemDelegator.h"
+
+/** Methods for new coordinate system stuff */
+
+namespace igstk
+{
+
+CoordinateReferenceSystemDelegator
+::CoordinateReferenceSystemDelegator() : m_StateMachine(this)
+{
+  m_Reporter = this;
+
+  m_CoordinateReferenceSystemObserver = ReceptorObserverType::New();
+  m_CoordinateReferenceSystemObserver->SetCallbackFunction(this, &Self::ObserverCallback);
+
+  m_CoordinateReferenceSystem = CoordinateReferenceSystem::New();
+ 
+  /** Add observer for coordinate reference system events */
+  m_CoordinateReferenceSystem->AddObserver( 
+    CoordinateReferenceSystemTransformToNullTargetEvent()
+    , m_CoordinateReferenceSystemObserver );
+
+  m_CoordinateReferenceSystem->AddObserver( 
+    CoordinateReferenceSystemTransformToDisconnectedEvent()
+    , m_CoordinateReferenceSystemObserver );
+
+  m_CoordinateReferenceSystem->AddObserver( 
+    CoordinateReferenceSystemNullParentEvent()
+    , m_CoordinateReferenceSystemObserver );
+
+  m_CoordinateReferenceSystem->AddObserver( 
+    CoordinateReferenceSystemThisParentEvent()
+    , m_CoordinateReferenceSystemObserver );
+
+  m_CoordinateReferenceSystem->AddObserver( 
+    CoordinateReferenceSystemParentCycleEvent()
+    , m_CoordinateReferenceSystemObserver );
+
+  m_CoordinateReferenceSystem->AddObserver( 
+    CoordinateReferenceSystemTransformToEvent()
+    , m_CoordinateReferenceSystemObserver );
+
+  igstkAddStateMacro( Idle );
+
+  igstkAddInputMacro( NullParent );
+  igstkAddInputMacro( NullTarget );
+
+  igstkAddTransitionMacro( Idle, NullParent,
+                           Idle, NullParent );
+  igstkAddTransitionMacro( Idle, NullTarget,
+                           Idle, NullTarget );
+
+  igstkSetInitialStateMacro( Idle );
+  m_StateMachine.SetReadyToRun();
+}
+
+CoordinateReferenceSystemDelegator::
+~CoordinateReferenceSystemDelegator()
+{
+
+}
+
+void 
+CoordinateReferenceSystemDelegator::
+ObserverCallback(const ::itk::EventObject & eventvar)
+{
+  /** Re-invoke the event */
+  this->m_Reporter->InvokeEvent( eventvar );
+}
+
+const CoordinateReferenceSystem * 
+CoordinateReferenceSystemDelegator::GetCoordinateReferenceSystem() const
+{
+  return m_CoordinateReferenceSystem;
+}
+
+void
+CoordinateReferenceSystemDelegator
+::NullTargetProcessing()
+{
+  igstkLogMacro( WARNING, "Null target!" );  
+}
+
+void
+CoordinateReferenceSystemDelegator
+::NullParentProcessing()
+{
+  igstkLogMacro( WARNING, "Null parent!" );
+}
+
+void
+CoordinateReferenceSystemDelegator
+::RequestSetReporter(::itk::Object::Pointer obj)
+{
+  /** State machine-ize? */
+  if (obj.IsNotNull())
+    {
+    m_Reporter = obj;
+    }
+}
+
+} // igstk
+
+
