4009551f5a70d02dc5e0dfc4ca2392acdb7c7e0e
Matt Turek <matt.turek@kitware.com>
2007-11-28 17:38:41 -0500

    ENH: Use LoggerType from igstk::Object, instead of assuming that the logger is itk::Logger.

--- IGSTK/Testing/igstkAuroraTrackerTest.cxx
+++ IGSTK/Testing/igstkAuroraTrackerTest.cxx
@@ -76,7 +76,7 @@ int igstkAuroraTrackerTest( int argc, char * argv[] )
 
   igstk::RealTimeClock::Initialize();
 
-  typedef itk::Logger                   LoggerType; 
+  typedef igstk::Object::LoggerType             LoggerType;
   typedef itk::StdStreamLogOutput       LogOutputType;
 
   igstk::AuroraTrackerTool::Pointer tool = igstk::AuroraTrackerTool::New();
