03cf34dbf95ab41ce7922e1918a28dc617238e8a
Matt Turek <matt.turek@kitware.com>
2007-11-28 16:53:24 -0500

    ENH: Use LoggerType from igstk::Object, instead of assuming that the logger is itk::Logger.

--- Examples/LandmarkRegistrationErrorEstimation/ErrorEstimation1.cxx
+++ Examples/LandmarkRegistrationErrorEstimation/ErrorEstimation1.cxx
@@ -147,7 +147,7 @@ int main( int argv, char * argc[] )
   std::cout << "Landmark3DRegistrationErrorEstimator" << std::endl;
 
 
-  typedef itk::Logger                   LoggerType;
+  typedef igstk::Object::LoggerType             LoggerType;
   typedef itk::StdStreamLogOutput       LogOutputType;
 
   typedef igstk::Landmark3DRegistration
