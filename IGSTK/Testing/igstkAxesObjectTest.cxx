eeb0784a407fb43a7095db54612af03470a20e16
Matt Turek <matt.turek@kitware.com>
2007-11-29 08:44:37 -0500

    ENH: Switched to igstkLogger include file.

--- IGSTK/Testing/igstkAxesObjectTest.cxx
+++ IGSTK/Testing/igstkAxesObjectTest.cxx
@@ -29,7 +29,7 @@
 #include "igstkView2D.h"
 #include "igstkRealTimeClock.h"
 #include "igstkVTKLoggerOutput.h"
-#include "itkLogger.h"
+#include "igstkLogger.h"
 #include "itkStdStreamLogOutput.h"
 
 #ifdef IGSTK_USE_COORDINATE_REFERENCE_SYSTEM
