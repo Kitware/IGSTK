cd7d27cda03081ac827511033d2d6fcd52184467
Matt Turek <matt.turek@kitware.com>
2007-11-29 08:48:12 -0500

    ENH: Switched to using the igstkLogger

--- Examples/OneViewAndTrackingUsingQTWidget/OneViewAndTrackingUsingQTWidget.cxx
+++ Examples/OneViewAndTrackingUsingQTWidget/OneViewAndTrackingUsingQTWidget.cxx
@@ -36,15 +36,15 @@ int main(int argc, char** argv)
 
   OneViewAndTrackingUsingQTWidgetGUI   mainWindow;
 
-  typedef itk::Logger              LoggerType;
-  typedef itk::StdStreamLogOutput  LogOutputType;
+  typedef igstk::Object::LoggerType              LoggerType;
+  typedef itk::StdStreamLogOutput             LogOutputType;
   
   // logger object created for logging mouse activities
   LoggerType::Pointer   logger = LoggerType::New();
   LogOutputType::Pointer logOutput = LogOutputType::New();
   logOutput->SetStream( std::cout );
   logger->AddLogOutput( logOutput );
-  logger->SetPriorityLevel( itk::Logger::DEBUG );
+  logger->SetPriorityLevel( LoggerType::DEBUG );
 
   // Create an igstk::VTKLoggerOutput and then test it.
   igstk::VTKLoggerOutput::Pointer vtkLoggerOutput = 
