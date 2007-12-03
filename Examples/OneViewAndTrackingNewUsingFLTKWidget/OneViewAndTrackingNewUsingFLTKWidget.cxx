32c9298acf2ebbd973fc0724a6474b4cc9c10c5c
Andinet Enquobahrie <andinet.enqu@kitware.com>
2007-12-03 10:00:23 -0500

    ENH: add a one view and tracking example using the newly refactored tracker      classes

---
+++ Examples/OneViewAndTrackingNewUsingFLTKWidget/OneViewAndTrackingNewUsingFLTKWidget.cxx
@@ -0,0 +1,122 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    OneViewAndTrackingNewUsingFLTKWidget.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISC  Insight Software Consortium.  All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+//  Warning about: identifier was truncated to '255' characters 
+//  in the debug information (MVC6.0 Debug)
+#pragma warning( disable : 4284 )
+#endif
+
+#include "OneViewAndTrackingNewUsingFLTKWidgetImplementation.h"
+#include "igstkViewNew3D.h"
+#include "igstkEllipsoidObject.h"
+#include "igstkCylinderObject.h"
+#include "igstkEllipsoidObjectRepresentation.h"
+#include "igstkCylinderObjectRepresentation.h"
+
+
+int main(int , char** )
+{ 
+
+  igstk::RealTimeClock::Initialize();
+
+  OneViewAndTrackingNewUsingFLTKWidgetImplementation   application;
+
+  // Create the ellipsoid 
+  igstk::EllipsoidObject::Pointer ellipsoid = igstk::EllipsoidObject::New();
+  ellipsoid->SetRadius(200,200,300); // about a human skull
+  
+  double validityTimeInMilliseconds = 1e20; // in seconds
+  igstk::Transform transform;
+  igstk::Transform::VectorType translation;
+  translation[0] = 0.0;
+  translation[1] = 0.0;
+  translation[2] = 0.0;
+  igstk::Transform::VersorType rotation;
+  rotation.Set( 0.0, 0.0, 0.0, 1.0 );
+  igstk::Transform::ErrorType errorValue = 0.01; // 10 microns
+
+  transform.SetTranslationAndRotation( 
+      translation, rotation, errorValue, validityTimeInMilliseconds );
+
+  std::cout << "Transform to static ellipsoid = " << transform << std::endl;
+
+
+  igstk::Transform transformToView;
+  igstk::Transform::VectorType translationToView;
+  translationToView[0] = 0.0;
+  translationToView[1] = 0.0;
+  translationToView[2] = 0.0;
+  igstk::Transform::VersorType rotationToView;
+  rotationToView.Set( 0.0, 1.0, 0.0, 1.0 );
+  transformToView.SetTranslationAndRotation(
+      translationToView, rotationToView, errorValue, validityTimeInMilliseconds );
+
+  // Create the ellipsoid representation
+  igstk::EllipsoidObjectRepresentation::Pointer 
+        ellipsoidRepresentation = igstk::EllipsoidObjectRepresentation::New();
+  ellipsoidRepresentation->RequestSetEllipsoidObject( ellipsoid );
+  ellipsoidRepresentation->SetColor(0.0,1.0,0.0);
+  ellipsoidRepresentation->SetOpacity(1.0);
+
+  // Create the cylinder 
+  igstk::CylinderObject::Pointer cylinder = igstk::CylinderObject::New();
+  cylinder->SetRadius(1.0);
+  cylinder->SetHeight(300.0);  // about the size of a needle
+
+  // Create the cylinder representation
+  igstk::CylinderObjectRepresentation::Pointer 
+          cylinderRepresentation = igstk::CylinderObjectRepresentation::New();
+  cylinderRepresentation->RequestSetCylinderObject( cylinder );
+  cylinderRepresentation->SetColor(1.0,0.0,0.0);
+  cylinderRepresentation->SetOpacity(1.0);
+
+  // instantiate a 3D view 
+  typedef igstk::ViewNew3D        ViewNew3DType;
+  ViewNew3DType::Pointer view3D = ViewNew3DType::New();
+
+  /** We pass the bare pointer so that the compiler can figure out the type
+      for the templated method. */
+  ellipsoid->RequestSetTransformAndParent( transformToView, 
+                                           view3D.GetPointer() );
+  cylinder->RequestSetTransformAndParent( transform, 
+                                          ellipsoid.GetPointer() );
+  view3D->RequestAddObject( ellipsoidRepresentation );
+  view3D->RequestAddObject( cylinderRepresentation );
+ 
+  // Set the refresh rate and start 
+  // the pulse generators of the views.
+
+  view3D->SetRefreshRate( 30 );
+  view3D->RequestStart();
+  view3D->SetCameraPosition(0.0, 0.0, -600.0);
+
+
+  application.Display3D->RequestSetView( view3D );
+
+  application.Show();
+
+  // Associate the Spatial Object to the tracker
+  application.AttachObjectToTrack( cylinder );
+
+  while( !application.HasQuitted() )
+    {
+    Fl::wait(0.001);
+    igstk::PulseGenerator::CheckTimeouts();
+    }
+
+  return EXIT_SUCCESS;
+}
---
+++ Examples/OneViewAndTrackingNewUsingFLTKWidget/OneViewAndTrackingNewUsingFLTKWidgetImplementation.h
@@ -0,0 +1,152 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    OneViewAndTrackingNewUsingFLTKWidgetImplementation.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISC  Insight Software Consortium.  All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __OneViewAndTrackingNewUsingFLTKWidgetImplementation_h
+#define __OneViewAndTrackingNewUsingFLTKWidgetImplementation_h
+
+#if defined(_MSC_VER)
+//Warning about: identifier was truncated to '255' characters in the debug
+//information (MVC6.0 Debug)
+#pragma warning( disable : 4284 )
+#endif
+
+#include "OneViewAndTrackingNewUsingFLTKWidgetGUI.h"
+#include "igstkPolarisTrackerNew.h"
+#ifdef WIN32
+#include "igstkSerialCommunicationForWindows.h"
+#else
+#include "igstkSerialCommunicationForPosix.h"
+#endif
+
+#include "igstkLogger.h"
+#include "itkStdStreamLogOutput.h"
+
+class OneViewAndTrackingNewUsingFLTKWidgetImplementation : 
+  public OneViewAndTrackingNewUsingFLTKWidgetGUI
+{
+public:
+
+  typedef igstk::Object::LoggerType             LoggerType;
+  typedef itk::StdStreamLogOutput  LogOutputType;
+
+  typedef igstk::PolarisTrackerNew     TrackerType;
+
+#ifdef WIN32
+  typedef igstk::SerialCommunicationForWindows  CommunicationType;
+#else
+  typedef igstk::SerialCommunicationForPosix    CommunicationType;
+#endif
+
+public:
+
+  OneViewAndTrackingNewUsingFLTKWidgetImplementation()
+    {
+    m_Tracker = TrackerType::New();
+
+    m_Logger = LoggerType::New();
+    m_LogOutput = LogOutputType::New();
+    m_LogFile.open("logOneViewAndTrackingNewUsingFLTKWidget.txt");
+    if( !m_LogFile.fail() )
+      {
+      m_LogOutput->SetStream( m_LogFile );
+      }
+    else
+      {
+      std::cerr << "Problem opening Log file, using cerr instead " 
+        << std::endl;
+      m_LogOutput->SetStream( std::cerr );
+      }
+    m_Logger->AddLogOutput( m_LogOutput );
+
+    // add stdout for debug purposes
+    LogOutputType::Pointer coutLogOutput = LogOutputType::New();
+    coutLogOutput->SetStream( std::cout );
+    m_Logger->AddLogOutput( coutLogOutput );
+
+    m_Logger->SetPriorityLevel( LoggerType::DEBUG );
+    m_Tracker->SetLogger( m_Logger );
+
+    m_Communication = CommunicationType::New();
+    m_Communication->SetLogger( m_Logger );
+    m_Communication->SetPortNumber( igstk::SerialCommunication::PortNumber0 );
+    m_Communication->SetParity( igstk::SerialCommunication::NoParity );
+    m_Communication->SetBaudRate( igstk::SerialCommunication::BaudRate9600 );
+    m_Communication->SetDataBits( igstk::SerialCommunication::DataBits8 );
+    m_Communication->SetStopBits( igstk::SerialCommunication::StopBits1 );
+    m_Communication->SetHardwareHandshake( 
+      igstk::SerialCommunication::HandshakeOff );
+    m_Tracker->SetCommunication(m_Communication);
+
+    m_Communication->OpenCommunication();
+
+    m_Tracker->RequestOpen();
+
+    m_Tracking = false;
+
+    //enable interaction by default
+    InteractionButton->set();    
+    }
+
+  ~OneViewAndTrackingNewUsingFLTKWidgetImplementation()
+    {
+    m_Tracker->RequestReset();
+    m_Tracker->RequestStopTracking();
+    m_Tracker->RequestClose();
+    }
+  
+  void EnableTracking()
+    {
+    m_Tracking = true;
+    m_Tracker->RequestStartTracking();
+    }
+  
+  void DisableTracking()
+    {
+    m_Tracker->RequestReset();
+    m_Tracker->RequestStopTracking();
+    m_Tracking = false;
+    }
+
+  void EnableInteraction()
+    {
+    Display3D->RequestEnableInteractions();
+    }
+ 
+  void DisableInteraction()
+    {
+    Display3D->RequestDisableInteractions();
+    }
+ 
+  void AttachObjectToTrack( igstk::SpatialObject * objectToTrack )
+    {
+    // Attach a spatial object
+    }
+
+private:
+
+  LoggerType::Pointer     m_Logger;
+  LogOutputType::Pointer  m_LogOutput;
+  TrackerType::Pointer    m_Tracker;
+
+  CommunicationType::Pointer m_Communication;
+
+  bool                    m_Tracking;
+  
+  std::ofstream           m_LogFile;
+};
+
+#endif
