bf23aef8d8ca8a42025c5f82772f22eff319e902
Luis Ibanez <luis.ibanez@kitware.com>
2007-12-21 08:50:07 -0500

    ENH: Added specialization of the TrackerTool class for being used along      with the QMouseTracker.

---
+++ Source/igstkQMouseTrackerTool.cxx
@@ -0,0 +1,65 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkQMouseTrackerTool.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISC  Insight Software Consortium.  All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#if defined(_MSC_VER)
+//  Warning about: identifier was truncated to '255' characters 
+// in the debug information (MVC6.0 Debug)
+#pragma warning( disable : 4786 )
+#endif
+
+#include "igstkQMouseTrackerTool.h"
+
+namespace igstk
+{
+
+/** Constructor (configures Polaris-specific tool values) */
+QMouseTrackerTool::QMouseTrackerTool():m_StateMachine(this)
+{
+  m_TrackerToolConfigured = false;
+}
+
+/** Destructor */
+QMouseTrackerTool::~QMouseTrackerTool()
+{
+}
+
+/** Request set mouse name */
+void QMouseTrackerTool::RequestSetMouseName( const MouseNameType & mouseName )
+{
+  this->m_MouseName = mouseName;
+  m_TrackerToolConfigured = true;
+
+  // For MouseTracker, mouse name is used as a unique identifier
+  this->SetTrackerToolIdentifier( m_MouseName ); 
+}
+
+/** The "CheckIfTrackerToolIsConfigured" method returns true if the tracker tool * is configured */ 
+bool
+QMouseTrackerTool::CheckIfTrackerToolIsConfigured( )
+{
+  igstkLogMacro( DEBUG, "igstk::PolarisTrackerTool::CheckIfTrackerToolIsConfigured called...\n");
+  return m_TrackerToolConfigured;
+}
+
+
+/** Print Self function */
+void QMouseTrackerTool::PrintSelf( std::ostream& os, itk::Indent indent ) const
+{
+  Superclass::PrintSelf(os, indent);
+}
+
+} //igstk namespace
---
+++ Source/igstkQMouseTrackerTool.h
@@ -0,0 +1,73 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkQMouseTrackerTool.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISC  Insight Software Consortium.  All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __igstkQMouseTrackerTool_h
+#define __igstkQMouseTrackerTool_h
+
+#include "igstkTrackerTool.h"
+
+namespace igstk
+{
+/** \class QMouseTrackerTool
+  * \brief A MouseTracker-specific TrackerTool class.
+  *
+  * \ingroup Tracker
+  *
+  */
+
+class QMouseTrackerTool : public TrackerTool
+{
+public:
+
+  /** Macro with standard traits declarations. */
+  igstkStandardClassTraitsMacro( QMouseTrackerTool, TrackerTool )
+
+  typedef std::string       MouseNameType;
+
+  /** Get Tracker tool mouse name */
+  igstkGetStringMacro( MouseName );
+
+  /** Set tracker tool mouse name */
+  virtual void RequestSetMouseName( const MouseNameType & MouseName ); 
+
+protected:
+
+  QMouseTrackerTool();
+  ~QMouseTrackerTool();
+
+ /** Print object information */
+  virtual void PrintSelf( std::ostream& os, ::itk::Indent indent ) const; 
+
+private:
+
+  /** Get boolean variable to check if the tracker tool is 
+   * configured or not */
+  virtual bool CheckIfTrackerToolIsConfigured(); 
+
+  QMouseTrackerTool(const Self&);   //purposely not implemented
+  void operator=(const Self&);       //purposely not implemented
+
+  MouseNameType   m_MouseName;
+
+  bool            m_TrackerToolConfigured;
+
+};  
+
+} // namespace igstk
+
+
+#endif  // __igstk_QMouseTrackerTool_h_
