1a6d5b8c037c5fd62818db8f0497cdf1182994ba
Luis Ibanez <luis.ibanez@kitware.com>
2007-12-21 15:50:19 -0500

    ENH: Adding test for the TrackerToolObserver.

--- Testing/igstkSandboxTests.cxx
+++ Testing/igstkSandboxTests.cxx
@@ -42,6 +42,7 @@ void RegisterTests()
   REGISTER_TEST(igstkGenericImageSpatialObjectTest);
   REGISTER_TEST(igstkDICOMGenericImageReaderTest);
   REGISTER_TEST(igstkPivotCalibrationAlgorithmTest);
+  REGISTER_TEST(igstkTrackerToolObserverToSocketRelayTest);
   REGISTER_TEST(igstkVascularNetworkObjectTest);
 
 #ifdef IGSTKSandbox_USE_FLTK
