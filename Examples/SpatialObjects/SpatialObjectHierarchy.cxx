082bf4ad86a3475b520a23b5b2c95c3cdc5083cd
Matt Turek <matt.turek@kitware.com>
2008-01-02 15:14:35 -0500

    ENH: Removed some deprecated spatial object methods. Using new coordinate system API.

--- Examples/SpatialObjects/SpatialObjectHierarchy.cxx
+++ Examples/SpatialObjects/SpatialObjectHierarchy.cxx
@@ -36,8 +36,9 @@
 #include "igstkGroupObject.h"
 // Software Guide : EndCodeSnippet
 
-igstkObserverMacro(Transform,
-    ::igstk::TransformModifiedEvent,::igstk::Transform)
+igstkObserverMacro(TransformToParent,
+                   ::igstk::CoordinateReferenceSystemTransformToEvent,
+                   ::igstk::CoordinateReferenceSystemTransformToResult)
 
 int main( int , char *[] )
 {
@@ -57,47 +58,21 @@ int main( int , char *[] )
 // Software Guide : EndCodeSnippet
 // Software Guide : BeginLatex
 //
-// We then add the second sphere to the first one by using the
-// \code{RequestAddObject()} method.  As a result, \code{sphere2} becomes a
-// child of \code{sphere1}:
+// We then set the second sphere to have a coordinate system
+// relative to sphere1.
 //
-// Software Guide : EndLatex 
-
-// Software Guide : BeginCodeSnippet 
-#ifdef USE_SPATIAL_OBJECT_DEPRECATED
-  sphere1->RequestAddObject(sphere2);
-#endif
-// Software Guide : EndCodeSnippet
-
-// Software Guide : BeginLatex
-//
-// A child object can be retrieved from its parent 
-// by using the \code{GetObject()} function, as follows:
-//
-// Software Guide : EndLatex 
-
-// Software Guide : BeginCodeSnippet
-#ifdef USE_SPATIAL_OBJECT_DEPRECATED
-  sphere1->GetObject(0)->Print(std::cout);
-#endif
-// Software Guide : EndCodeSnippet
-
-// Software Guide : BeginLatex
-//
-// Next, we assign a transformation to the object.
 // We first create a transformation and set the translation vector to be $10mm$
 // in each direction, with an error value of $0.001mm$ and a validity time
 // of $10ms$. Second, we assign the transform to the object via the 
-// \code{RequestSetTransform()} function. The commands appear as follows:
+// \code{RequestSetTransformAndParent()} function. The commands appear as follows:
 //
 // Software Guide : EndLatex 
 
 // Software Guide : BeginCodeSnippet
-  igstk::Transform transform;
-  transform.SetTranslation(10,0.001,10);
-#ifdef USE_SPATIAL_OBJECT_DEPRECATED  
-  sphere1->RequestSetTransform(transform);
-#endif
+  igstk::Transform transformSphere2ToSphere1;
+  transformSphere2ToSphere1.SetTranslation(10,0.001,10);
+  sphere2->RequestSetTransformAndParent( transformSphere2ToSphere1, sphere1.GetPointer() );
+
 // Software Guide : EndCodeSnippet
 
 // Software Guide : BeginLatex
@@ -105,38 +80,45 @@ int main( int , char *[] )
 // Then, in order to retrieve the transformation, we create an observer:
 //
 // \begin{verbatim}
-//   igstkObserverMacro(Transform,
-//   ::igstk::TransformModifiedEvent,::igstk::Transform)
+// igstkObserverMacro(TransformToParent,
+//                   ::igstk::CoordinateReferenceSystemTransformToEvent,
+//                   ::igstk::CoordinateReferenceSystemTransformToResult)
 // \end{verbatim}
 // We then add the observer to the object using the 
 // \code{AddObserver()} command:
 // Software Guide : EndLatex 
 
 // Software Guide : BeginCodeSnippet
-  TransformObserver::Pointer transformObserver 
-                           = TransformObserver::New();
-  sphere1->AddObserver( ::igstk::TransformModifiedEvent(), transformObserver );
+
+  TransformToParentObserver::Pointer transformToParentObserver 
+                           = TransformToParentObserver::New();
+        
+  sphere2->AddObserver( ::igstk::CoordinateReferenceSystemTransformToEvent(), 
+                        transformToParentObserver );
+
 // Software Guide : EndCodeSnippet
 
 // Software Guide : BeginLatex
 // 
-// Then, we request the transform using the \code{RequestGetTransform()}
+// Then, we request the transform using the 
+// \code{RequestGetTransformToParent()}
 // command:
 // 
 // Software Guide : EndLatex 
 
 // Software Guide : BeginCodeSnippet
-#ifdef USE_SPATIAL_OBJECT_DEPRECATED  
-  sphere1->RequestGetTransform();
-#endif
-  if( !transformObserver->GotTransform() )
+  sphere2->RequestGetTransformToParent();
+  if( !transformToParentObserver->GotTransformToParent() )
     {
     std::cerr << "Sphere1 did not returned a Transform event" << std::endl;
     return EXIT_FAILURE;
     }
       
-  igstk::Transform  transform2 = transformObserver->GetTransform();
+  igstk::Transform  transform2 = transformToParentObserver
+                                    ->GetTransformToParent().GetTransform();
 // Software Guide : EndCodeSnippet
+  std::cout << "Received transform from sphere2 to parent sphere1 : " 
+            << std::endl;
   std::cout << transform2 << std::endl;
 
 // Software Guide : BeginLatex
