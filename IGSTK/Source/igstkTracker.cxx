a506c0793e7563e3510a038e67550c83e3cea90c
Patrick Cheng <cheng@isis.georgetown.edu>
2008-02-09 20:15:04 -0500

    Detach reference tool from tracker first before setting it as Tracker's parent

--- IGSTK/Source/igstkTracker.cxx
+++ IGSTK/Source/igstkTracker.cxx
@@ -339,6 +339,7 @@ void Tracker::RequestSetReferenceTool( TrackerToolType * trackerTool )
       {
       m_ApplyingReferenceTool = true;
       m_ReferenceTool = trackerTool;
+      m_ReferenceTool->RequestDetach();
 
       //VERY IMPORTANT: Make the reference tracker tool the parent of the tracker
       this->RequestSetTransformAndParent( identityTransform, trackerTool );
@@ -787,8 +788,8 @@ void Tracker::UpdateStatusSuccessProcessing( void )
         
       // if a reference tracker tool has been specified, then if the tracker tool that is
       // being updated is the selected reference tracker tool, then update the
-      // transform that is from the tracker to the the reference tracker tool. Othewise,
-      // update the transfrom from the tracker tool to the tracker. 
+      // transform that is from the tracker to the the reference tracker tool. Otherwise,
+      // update the transform from the tracker tool to the tracker. 
       if ( m_ApplyingReferenceTool )
         {
         if ( (inputItr->first) == m_ReferenceTool->GetTrackerToolIdentifier())
