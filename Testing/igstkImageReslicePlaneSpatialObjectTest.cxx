055c9417fe1aeea54a0b3d1ca731490742936834
Andinet Enquobahrie <andinet.enqu@kitware.com>
2008-04-18 11:40:07 -0400

    ENH: Add axes object to act as a reference (world) coordinate system and use      cylinder spatial object as a tool spatial object

--- Testing/igstkImageReslicePlaneSpatialObjectTest.cxx
+++ Testing/igstkImageReslicePlaneSpatialObjectTest.cxx
@@ -21,17 +21,33 @@
 
 #include "igstkConfigure.h"
 #include "igstkImageReslicePlaneSpatialObject.h"
+#include "igstkAxesObject.h"
 #include "igstkImageSpatialObject.h"
+#include "igstkCTImageReader.h"
 #include "igstkVTKLoggerOutput.h"
 #include "igstkLogger.h"
 #include "itkStdStreamLogOutput.h"
 #include "igstkEvents.h"
-#include "igstkTubeObject.h"
+#include "igstkCylinderObject.h"
+#include "igstkTransform.h"
+
+namespace ImageReslicePlaneSpatialObjectTest
+{
+igstkObserverObjectMacro(CTImage,
+    ::igstk::CTImageReader::ImageModifiedEvent,::igstk::CTImageSpatialObject)
+}
 
 int igstkImageReslicePlaneSpatialObjectTest( int argc , char * argv [] )
 {
   igstk::RealTimeClock::Initialize();
 
+
+  if( argc < 2 )
+    {
+    std::cerr << " Missing arguments: " << argv[0]  \
+              << "\t Input image" << std::endl; 
+    return EXIT_FAILURE;
+    }
   typedef short    PixelType;
   const unsigned int Dimension = 3;
 
@@ -57,26 +73,84 @@ int igstkImageReslicePlaneSpatialObjectTest( int argc , char * argv [] )
   vtkLoggerOutput->OverrideVTKWindow();
   vtkLoggerOutput->SetLogger(logger);// redirect messages from VTK 
                                      // OutputWindow -> logger
-                                     //
-  ResliceSpatialObjectType::Pointer planeSpatialObject = ResliceSpatialObjectType::New();
 
-  planeSpatialObject->Print( std::cout );
+  // Create Axes object to act as a reference coordinate system
+  typedef igstk::AxesObject    AxesObjectType;
+  AxesObjectType::Pointer axesObject = AxesObjectType::New();
+  axesObject->SetSize(3.0, 3.0, 3.0);
 
-  // Perform orthogonal reslicing
-  planeSpatialObject->RequestSetReslicingMode( igstk::ImageReslicePlaneSpatialObject<ImageSpatialObjectType>::Orthogonal );
+  // Define identity transform
+  igstk::Transform identity;
+  identity.SetToIdentity( igstk::TimeStamp::GetLongestPossibleTime() );
 
-  // Select axial orientation
-  planeSpatialObject->RequestSetOrientationType( igstk::ImageReslicePlaneSpatialObject<ImageSpatialObjectType>::Axial );
+  typedef igstk::CTImageReader         ReaderType;
+  ReaderType::Pointer   reader = ReaderType::New();
+  reader->SetLogger( logger );
+
+  //set up CT image observer
+  typedef ImageReslicePlaneSpatialObjectTest::CTImageObserver 
+                                                        CTImageObserverType;
+  CTImageObserverType::Pointer ctImageObserver = CTImageObserverType::New(); 
+  reader->AddObserver(::igstk::CTImageReader::ImageModifiedEvent(),
+                            ctImageObserver);
+
+  /* Read in a DICOM series */
+  std::cout << "Reading CT image : " << argv[1] << std::endl;
+
+  ReaderType::DirectoryNameType directoryName = argv[1];
+
+  reader->RequestSetDirectory( directoryName );
+  reader->RequestReadImage();
+  reader->RequestGetImage();
+ 
+  if(!ctImageObserver->GotCTImage())
+    {
+    std::cout << "No CTImage!" << std::endl;
+    std::cout << "[FAILED]" << std::endl;
+    return EXIT_FAILURE;
+    }
 
   // Set input image spatial object
   ImageSpatialObjectType::Pointer imageSpatialObject = ImageSpatialObjectType::New(); 
+  imageSpatialObject = ctImageObserver->GetCTImage();
+
+  //Connect the image spatial object to the reference coordinate system
+  imageSpatialObject->RequestSetTransformAndParent( identity, axesObject );
+  ResliceSpatialObjectType::Pointer planeSpatialObject = ResliceSpatialObjectType::New();
+  planeSpatialObject->SetLogger( logger );
+
+  // Select Orthogonal reslicing mode
+  planeSpatialObject->RequestSetReslicingMode( 
+           igstk::ImageReslicePlaneSpatialObject<ImageSpatialObjectType>::Orthogonal );
+
+  // Select axial orientation
+  planeSpatialObject->RequestSetOrientationType(
+           igstk::ImageReslicePlaneSpatialObject<ImageSpatialObjectType>::Axial );
   planeSpatialObject->RequestSetImageSpatialObject( imageSpatialObject );
 
-  //
   // Set input tool spatial object
-  typedef igstk::TubeObject                          ToolSpatialObjectType;
+  typedef igstk::CylinderObject                           ToolSpatialObjectType;
   ToolSpatialObjectType::Pointer toolSpatialObject = ToolSpatialObjectType::New();  
+  toolSpatialObject->SetRadius( 0.1 );
+  toolSpatialObject->SetHeight( 2.0 );
+
+  // set tool transform
+  igstk::Transform toolTransform;
+  igstk::Transform::VectorType    translation;
+  igstk::Transform::VersorType    rotation;
+  translation[0] =    0;
+  translation[1] =    0;
+  translation[2] = -1.0;
+  rotation.Set(0.0, 0.0, 0.0, 1.0);
+  const double transformUncertainty = 1.0;
+  toolTransform.SetTranslation(
+                          translation,
+                          transformUncertainty,
+                          igstk::TimeStamp::GetLongestPossibleTime() );
+  toolSpatialObject->RequestSetTransformAndParent( toolTransform, axesObject );
   planeSpatialObject->RequestSetToolSpatialObject( toolSpatialObject );
+
+  vtkPlane * plane = planeSpatialObject->RequestGetReslicingPlane();
 
   if( vtkLoggerOutput->GetNumberOfErrorMessages()  > 0 )
     {
@@ -85,3 +159,4 @@ int igstkImageReslicePlaneSpatialObjectTest( int argc , char * argv [] )
  
   return EXIT_SUCCESS;
 }
+
