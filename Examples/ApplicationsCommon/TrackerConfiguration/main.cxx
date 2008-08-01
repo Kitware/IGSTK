9a05edc6e76e35915481957976e5889dda54f6dd
Haiying Liu <hliu@bwh.harvard.edu>
2008-08-01 09:38:37 -0400

    initial version.

---
+++ Examples/ApplicationsCommon/TrackerConfiguration/main.cxx
@@ -0,0 +1,33 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    main.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISC  Insight Software Consortium.  All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+// Warning about: identifier was truncated to '255' characters 
+// in the debug information (MVC6.0 Debug)
+#pragma warning( disable : 4284 )
+#endif
+
+
+#include <stdlib.h>
+#include <iostream>
+
+int main(int , char** )
+{ 
+
+  std::cout << "TrackerConfiguration.\n";
+
+  return EXIT_SUCCESS;
+}
