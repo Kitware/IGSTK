016bfc331c9933a55ef7acc99613c17efd5cb90e
Haiying Liu <hliu@bwh.harvard.edu>
2008-08-29 15:58:18 -0400

    initial version.

---
+++ Examples/ApplicationsCommon/TrackerConfiguration/igstkTrackerConfigurationParser.cxx
@@ -0,0 +1,82 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkTrackerConfigurationParser.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISC  Insight Software Consortium.  All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+//#include <ctype.h>
+#include "vtkObjectFactory.h"
+#include "igstkTrackerConfigurationParser.h"
+#include "igstkAuroraTrackerConfigurationComposer.h"
+
+
+vtkStandardNewMacro (igstkTrackerConfigurationParser);
+vtkCxxRevisionMacro (igstkTrackerConfigurationParser, "1.1" );
+
+ 
+igstkTrackerConfigurationParser::igstkTrackerConfigurationParser()
+{
+    this->TrackerConfig = NULL;    
+    this->Composer = NULL;
+}
+
+
+igstkTrackerConfigurationParser::~igstkTrackerConfigurationParser()
+{
+    istream *is = this->GetStream();
+    if (is)
+    {
+        delete is;
+    }
+
+    if (this->Composer)
+    {
+        delete this->Composer; 
+    }
+}
+
+
+
+// True: Update successful
+// False: Update failed
+bool igstkTrackerConfigurationParser::CreateConfiguration()
+{
+    bool parsed = this->Parse();
+    if (! parsed)
+    {
+        return false;
+    }
+
+    std::string value(this->GetRootElement()->GetAttributeValue(0));
+    if (value.compare("aurora") == 0) // aurora system
+    {
+        this->Composer =  new igstk::igstkAuroraTrackerConfigurationComposer; 
+    }
+
+    this->Composer->SetParser(this);
+    bool done = this->Composer->BuildConfiguration();
+    if (! done)
+    {
+        this->TrackerConfig = NULL; 
+        return false;
+    }
+    else
+    {
+        this->TrackerConfig = this->Composer->GetTrackerConfiguration();    
+    }
+ 
+    return true;
+}
+
---
+++ Examples/ApplicationsCommon/TrackerConfiguration/igstkTrackerConfigurationParser.h
@@ -0,0 +1,61 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkTrackerConfigurationParser.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISC  Insight Software Consortium.  All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef __igstkTrackerConfigurationParser_h
+#define __igstkTrackerConfigurationParser_h
+
+
+#include "vtkXMLDataParser.h"
+#include "igstkTrackerConfiguration.h"
+#include "igstkTrackerConfigurationComposer.h"
+
+
+class igstkTrackerConfigurationParser : public vtkXMLDataParser 
+{
+public:
+    enum TRACKING_SYSTEM {
+    POLARIS_VICRA = 0, 
+    POLARIS_HYBRID = 1, 
+    AURORA = 2, 
+    MICRON = 3, 
+    FLOCK_OF_BIRDS = 4
+    };
+
+
+    static igstkTrackerConfigurationParser* New ( );
+    vtkTypeRevisionMacro (igstkTrackerConfigurationParser, vtkXMLDataParser);
+
+    vtkGetObjectMacro(TrackerConfig, igstk::TrackerConfiguration);
+
+    // True: Update successful
+    // False: Update failed
+    bool CreateConfiguration();
+
+protected:
+    igstk::TrackerConfiguration *TrackerConfig;    
+    igstk::igstkTrackerConfigurationComposer *Composer;
+
+    igstkTrackerConfigurationParser();
+    virtual ~igstkTrackerConfigurationParser();
+
+
+};
+
+
+
+#endif
