02a8a9de4da6b607a147aa3197a21044498ca66b
Sebastian Ordas <sebastian.ordas@gmail.com>
2008-10-29 16:56:29 -0400

    BUG: updated the test to the refactored tracker. Formely it was not failing but was not actually working correctly.

--- Testing/igstkAuroraTrackerTest.cxx
+++ Testing/igstkAuroraTrackerTest.cxx
@@ -65,6 +65,77 @@ public:
     }
 };
 
+/**
+ * This class observes the TrackerToolTransformUpdateEvent for a specific tool.
+ * It checks that the event is for the relevant tool and then gets the tool's
+ * transform to its parent and prints it out.
+ */
+class ToolUpdatedObserver : public ::itk::Command
+{
+public:
+  typedef  ToolUpdatedObserver    Self;
+  typedef  ::itk::Command             Superclass;
+  typedef  ::itk::SmartPointer<Self>  Pointer;
+  itkNewMacro( Self );
+protected:
+  ToolUpdatedObserver() {
+    this->m_TransformObserver = igstk::TransformObserver::New();
+    this->m_NumberOfIterations = 10;
+  }
+
+  ~ToolUpdatedObserver() {}
+
+public:
+  void Initialize( igstk::TrackerTool::Pointer trackerTool, bool *stop )
+  {
+    this->m_Stop = stop;
+    this->m_Counter = 0;    
+    this->m_Tool = trackerTool;
+    this->m_TransformObserver->ObserveTransformEventsFrom( this->m_Tool );
+  }
+
+  void Execute(itk::Object *caller, const itk::EventObject & event)
+  {
+    const itk::Object * constCaller = caller;
+    this->Execute( constCaller, event );
+  }
+
+  void Execute(const itk::Object *caller, const itk::EventObject & event)
+  {               //do something only for the correct tool    
+
+    if( this->m_Tool.GetPointer() == caller )
+    {               //the tool transform has been updated, get it
+      if( dynamic_cast<const igstk::TrackerToolTransformUpdateEvent  *>( &event) )
+      { 
+        if ( this->m_Counter > this->m_NumberOfIterations )
+        {
+          *(this->m_Stop) = true;
+          return;
+        }
+                        //request to get the transform               
+        this->m_Tool->RequestGetTransformToParent();
+                       //check that we got it
+        if ( this->m_TransformObserver->GotTransform() )
+        {
+          igstk::Transform transform = this->m_TransformObserver->GetTransform();
+          igstk::Transform::VectorType t = transform.GetTranslation();
+          igstk::Transform::VersorType r = transform.GetRotation();
+          std::cout<<"\nt:"<<t[0]<<"\t"<<t[1]<<"\t"<<t[2]<<"\n";
+          std::cout<<"r:"<<r.GetX()<<"\t"<<r.GetY()<<"\t"<<r.GetZ()<<"\t"<<r.GetW()<<"\n";
+          this->m_Counter++;
+        }
+      }
+    }
+  }
+
+private:
+  bool *m_Stop;
+  unsigned int m_Counter;
+  unsigned int m_NumberOfIterations;
+  igstk::TransformObserver::Pointer m_TransformObserver; 
+  igstk::TrackerTool::Pointer  m_Tool;
+};
+
 int igstkAuroraTrackerTest( int argc, char * argv[] )
 {
 
@@ -72,19 +143,16 @@ int igstkAuroraTrackerTest( int argc, char * argv[] )
 
   typedef igstk::Object::LoggerType     LoggerType;
   typedef itk::StdStreamLogOutput       LogOutputType;
-  typedef igstk::TransformObserver      ObserverType;
 
   if( argc < 3 )
     {
     std::cerr << " Usage: " << argv[0] << "\t" 
                             << "Logger_Output_filename " <<"\t"
-                            << "Port number"  <<"\t" 
+                            << "Serial port number"  <<"\t" 
+                            << "Control box port number"  <<"\t" 
                             << std::endl;
     return EXIT_FAILURE;
     }
-
-
-  igstk::AuroraTrackerTool::Pointer tool = igstk::AuroraTrackerTool::New();
 
   igstk::SerialCommunication::Pointer 
                      serialComm = igstk::SerialCommunication::New();
@@ -137,24 +205,33 @@ int igstkAuroraTrackerTest( int argc, char * argv[] )
   std::cout << "RequestOpen()" << std::endl;
   tracker->RequestOpen();
 
-  typedef igstk::AuroraTrackerTool          TrackerToolType;
+  // todo: add error observer for coomunication/starting error here and return FAILED
+
+  typedef igstk::TrackerTool                TrackerToolType;
+  typedef igstk::AuroraTrackerTool          AuroraTrackerToolType;
   typedef TrackerToolType::TransformType    TransformType;
 
-  // instantiate tracker tool with 6DOF 
-  TrackerToolType::Pointer trackerTool = TrackerToolType::New();
-  trackerTool->SetLogger( logger );
-  trackerTool->RequestSelect6DOFTrackerTool();
-  //Set the port number 
-  unsigned int portNumber = atoi( argv[2] );
-  trackerTool->RequestSetPortNumber( portNumber );
-  //Configure
-  trackerTool->RequestConfigure();
-  //Attach to the tracker
-  trackerTool->RequestAttachToTracker( tracker );
-  //Add observer to listen to transform events 
-  ObserverType::Pointer coordSystemAObserver = ObserverType::New();
-  coordSystemAObserver->ObserveTransformEventsFrom( trackerTool );
+  // have a generic tracker tool
+  TrackerToolType::Pointer trackerTool;
 
+  // instantiate aurora tracker tool
+  AuroraTrackerToolType::Pointer auroraTrackerTool = AuroraTrackerToolType::New();
+
+  //we use a 5 DOF sensor
+  auroraTrackerTool->RequestSelect5DOFTrackerTool();
+
+  //Set the port number (we need to subtract by 1, as internally required in IGSTK [0 - 3])
+  unsigned int portNumber = atoi( argv[3] ) - 1;
+  auroraTrackerTool->RequestSetPortNumber( portNumber );
+
+  // as we use a 5 DOF we need to set a channel number [0-1]
+  auroraTrackerTool->RequestSetChannelNumber( 0 );
+
+  //Configure
+  auroraTrackerTool->RequestConfigure();
+
+  //Attach to the tracker
+  auroraTrackerTool->RequestAttachToTracker( tracker );
 
   //start tracking 
   tracker->RequestStartTracking();
@@ -163,31 +240,24 @@ int igstkAuroraTrackerTest( int argc, char * argv[] )
   typedef ::itk::Vector<double, 3>    VectorType;
   typedef ::itk::Versor<double>       VersorType;
 
+  trackerTool = auroraTrackerTool;
 
-  for(unsigned int i=0; i<400; i++)
-    {
-    igstk::PulseGenerator::CheckTimeouts(); 
+  bool stop = false;
+  // create an observer for the TrackerToolTransformUpdateEvent
+  ToolUpdatedObserver::Pointer toolUpdateObserver = ToolUpdatedObserver::New();
+  toolUpdateObserver->Initialize(trackerTool, &stop);
 
-    TransformType             transform;
-    VectorType                position;
+  // add the observer to the tracker tool
+  trackerTool->AddObserver( igstk::TrackerToolTransformUpdateEvent(), toolUpdateObserver );
 
-    coordSystemAObserver->Clear();
-    trackerTool->RequestGetTransformToParent();
-    if (coordSystemAObserver->GotTransform())
-      {
-      transform = coordSystemAObserver->GetTransform();
-      if ( transform.IsValidNow() )
-        {
-        position = transform.GetTranslation();
-        std::cout << "Trackertool transform using observer:" 
-                << trackerTool->GetTrackerToolIdentifier() 
-                << "\t\t  Position = (" << position[0]
-                << "," << position[1] << "," << position[2]
-                << ")" << std::endl;
-        }
-      }
-    }
-  
+  while (!stop)
+  {
+    // this indeed is not an infinite loop. The stop variable will be set to true by the 
+    // observer upon completed the number of iterations (m_NumberOfIterations)
+    igstk::PulseGenerator::Sleep(10);
+    igstk::PulseGenerator::CheckTimeouts();
+  }
+
   std::cout << "RequestStopTracking()" << std::endl;
   tracker->RequestStopTracking();
 
