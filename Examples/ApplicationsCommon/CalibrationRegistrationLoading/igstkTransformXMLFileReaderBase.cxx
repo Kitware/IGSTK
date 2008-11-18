7db027b7a1000532bb000499a474085817d3455e
Ziv Yaniv <zivy@isis.georgetown.edu>
2008-11-18 13:22:59 -0500

    fixed style

--- Examples/ApplicationsCommon/CalibrationRegistrationLoading/igstkTransformXMLFileReaderBase.cxx
+++ Examples/ApplicationsCommon/CalibrationRegistrationLoading/igstkTransformXMLFileReaderBase.cxx
@@ -10,8 +10,8 @@ namespace igstk
 int 
 TransformXMLFileReaderBase::CanReadFile(const char* name)
 {
-       //check that we have a valid file name, it is not a directory, and the 
-       //file exists
+  //check that we have a valid file name, it is not a directory, and the 
+  //file exists
   if(itksys::SystemTools::FileLength( name ) == 0 ||
      !itksys::SystemTools::FileExists( name ) ||
      itksys::SystemTools::FileIsDirectory( name ))
@@ -25,49 +25,54 @@ TransformXMLFileReaderBase::StartElement( const char * name,
                                           const char **atts )
 {
   if( itksys::SystemTools::Strucmp( name, "precomputed_transform" ) == 0 ) 
-  {
-                   //there can only be one "precomputed_transform" per xml file                  
-    if( HaveTransformData() )
     {
-      throw FileFormatException(
-      std::string( "The tag \"precomputed_transform\" appears multiple times, should only appear once." ) );
-    }
+    //there can only be one "precomputed_transform" per xml file
+    if( HaveTransformData() )
+      {
+        throw FileFormatException(
+        std::string( "The tag \"precomputed_transform\" appears multiple times,\
+        should only appear once." ) );
+      }
     this->m_ReadingTransformData = true;
-  }
+    }
   else if( itksys::SystemTools::Strucmp( name, "description" ) == 0      ||  
            itksys::SystemTools::Strucmp( name, "computation_date" ) == 0 ||
            itksys::SystemTools::Strucmp( name, "transformation" ) == 0 )
-  {
-    if( this->m_ReadingTagData )
-      throw FileFormatException( "Nested tags are not allowed." );
-    else
-      this->m_ReadingTagData = true;
-            //if this is the "transformation" tag then it can have an
-            //"estimation_error" attribute, check for it here
-    if( itksys::SystemTools::Strucmp( name, "transformation" ) == 0 )
     {
+    if( this->m_ReadingTagData )
+      {
+      throw FileFormatException( "Nested tags are not allowed." );
+      }
+    else
+      {
+      this->m_ReadingTagData = true;
+      }
+    //if this is the "transformation" tag then it can have an
+    //"estimation_error" attribute, check for it here
+    if( itksys::SystemTools::Strucmp( name, "transformation" ) == 0 )
+      {
       ProcessTransformationAttributes( atts );
+      }
     }
-  }
 }
 
 
 void 
 TransformXMLFileReaderBase::EndElement( const char *name )
 {
-                         //trim the tag's data string
+  //trim the tag's data string
   size_t startpos = this->m_CurrentTagData.find_first_not_of(" \t\n\r");
   size_t endpos = this->m_CurrentTagData.find_last_not_of(" \t\n\r");   
   if( startpos == std::string::npos || endpos == std::string::npos )
-  {
+    {
     this->m_CurrentTagData.clear();
-  }
+    }
   else
-  {
+    {
     this->m_CurrentTagData = this->m_CurrentTagData.substr( startpos, 
                                                             endpos-startpos+1 );
-  } 
-           //replace all newlines ('\n') and carriage returns ('\r') with space
+    } 
+  //replace all newlines ('\n') and carriage returns ('\r') with space
   std::replace( this->m_CurrentTagData.begin(), 
                 this->m_CurrentTagData.end(), 
                 '\n', ' ' );
@@ -76,36 +81,36 @@ TransformXMLFileReaderBase::EndElement( const char *name )
                 '\r', ' ' );
   
   if( itksys::SystemTools::Strucmp( name, "precomputed_transform" ) == 0 )
-  {
+    {
     this->m_ReadingTransformData = false;
-                 //see that we got all the data, perhaps replace with a more
-                //specific message with regard to the missing tag(s)
+    //see that we got all the data, perhaps replace with a more
+    //specific message with regard to the missing tag(s)
     if( !HaveTransformData() )
-    {
+      {
       throw FileFormatException( std::string( "Missing tag(s) in xml file." ) );
-    }
-  }            //tags are expected to be inside the "precomputed_transform" tag
+      }
+    }    //tags are expected to be inside the "precomputed_transform" tag
   if( this->m_ReadingTransformData )
-  {
-    if( itksys::SystemTools::Strucmp( name, "description" ) == 0 )
     {
+    if( itksys::SystemTools::Strucmp( name, "description" ) == 0 )
+      {
       ProcessDescription();
       this->m_HaveDescription = true;
-    }
+      }
     else if( itksys::SystemTools::Strucmp( name, "computation_date" ) == 0 )
-    {
+      {
       ProcessDate();
       this->m_HaveDate = true;
-    }
+      }
     else if( itksys::SystemTools::Strucmp( name, "transformation" ) == 0 )
-    {
+      {
       ProcessTransformation();
       this->m_HaveTransform = true;
-    }
-            //done processing, clear the current tag's data
+      }
+    //done processing, clear the current tag's data
     this->m_CurrentTagData.clear();
     this->m_ReadingTagData = false;
-  }
+    }
 }
 
 
@@ -143,35 +148,44 @@ TransformXMLFileReaderBase::GetTransformData()
 
 void 
 TransformXMLFileReaderBase::ProcessDescription() 
-  throw ( FileFormatException )
+throw ( FileFormatException )
 {
   if( this->m_CurrentTagData.empty() )
+    {
     throw FileFormatException("Transformation description is missing.");
+    }
   else
+    {
     this->m_Description = this->m_CurrentTagData;
+    }
 }
 
 
 void 
 TransformXMLFileReaderBase::ProcessDate() 
-    throw ( FileFormatException )
+throw ( FileFormatException )
 {
   if( this->m_CurrentTagData.empty() )
+    {
     throw FileFormatException("Computation date is missing.");
+    }
   else
+    {
     this->m_Date = this->m_CurrentTagData;
+    }
 }
 
 
 void 
-TransformXMLFileReaderBase::ProcessTransformationAttributes( const char ** atts )
-    throw ( FileFormatException )
+TransformXMLFileReaderBase::ProcessTransformationAttributes( 
+const char ** atts )
+throw ( FileFormatException )
 {
-            //go over all the attribute-value pairs
+  //go over all the attribute-value pairs
   for( int i=0; atts[i] != NULL; i+=2 ) 
-  {          
-    if( itksys::SystemTools::Strucmp( atts[i], "estimation_error" ) == 0 ) 
     {
+    if( itksys::SystemTools::Strucmp( atts[i], "estimation_error" ) == 0 ) 
+      {
       PrecomputedTransformData::ErrorType tmp;
       std::istringstream instr;  
       instr.str( atts[i+1] );
@@ -179,12 +193,16 @@ TransformXMLFileReaderBase::ProcessTransformationAttributes( const char ** atts 
       //check that we got to the end of the stream, we expect a single value
       //here (assumes that the string err has no trailing white spaces)
       if(!instr.eof())
+        {
         throw FileFormatException("Estimation error format is invalid.");
+        }
       else
+        {
         this->m_EstimationError = tmp;
-        this->m_HaveError = true;
+        }
+      this->m_HaveError = true;
+      }
     }
-  }
 }
 
 } //namespace
