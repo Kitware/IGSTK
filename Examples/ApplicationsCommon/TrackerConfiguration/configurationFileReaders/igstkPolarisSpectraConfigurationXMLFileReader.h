a8cae977f04ec093158ccc0c0d0a8d6f759167b8
Ziv Yaniv <zivy@isis.georgetown.edu>
2008-11-25 16:24:25 -0500

    fixed style

--- Examples/ApplicationsCommon/TrackerConfiguration/configurationFileReaders/igstkPolarisSpectraConfigurationXMLFileReader.h
+++ Examples/ApplicationsCommon/TrackerConfiguration/configurationFileReaders/igstkPolarisSpectraConfigurationXMLFileReader.h
@@ -8,36 +8,39 @@
 namespace igstk
 {
 
- 
+/**
+ * \class PolarisSpectraConfigurationXMLFileReader This class is used to read 
+ *        the xml configuration file for NDI's Polaris Spectra tracker.
+ */ 
 class PolarisSpectraConfigurationXMLFileReader : 
    public PolarisWirelessConfigurationXMLFileReader 
 {
 public:
 
-    //standard typedefs
-  typedef PolarisSpectraConfigurationXMLFileReader        Self;
+  //standard typedefs
+  typedef PolarisSpectraConfigurationXMLFileReader                Self;
   typedef PolarisWirelessConfigurationXMLFileReader               Superclass;
   typedef itk::SmartPointer<Self>                                 Pointer;
 
-           //run-time type information (and related methods)
+  //run-time type information (and related methods)
   itkTypeMacro( PolarisSpectraConfigurationXMLFileReader, 
                 PolarisWirelessConfigurationXMLFileReader );
 
-           //method for creation through the object factory
+  //method for creation through the object factory
   itkNewMacro( Self );
 
 protected:
           //this is the constructor that is called by the factory to 
          //create a new object
   PolarisSpectraConfigurationXMLFileReader() : 
-         PolarisWirelessConfigurationXMLFileReader()
-         {}
-  virtual ~PolarisSpectraConfigurationXMLFileReader() {}
+    PolarisWirelessConfigurationXMLFileReader() { }
 
- virtual double GetMaximalRefreshRate();
- virtual std::string GetSystemType();
- virtual igstk::PolarisWirelessTrackerConfiguration::Pointer 
-   GetPolarisConfiguration();
+  virtual ~PolarisSpectraConfigurationXMLFileReader() { }
+
+  virtual double GetMaximalRefreshRate();
+  virtual std::string GetSystemType();
+  virtual igstk::PolarisWirelessTrackerConfiguration::Pointer 
+    GetPolarisConfiguration();
 
 private:
   PolarisSpectraConfigurationXMLFileReader( 
