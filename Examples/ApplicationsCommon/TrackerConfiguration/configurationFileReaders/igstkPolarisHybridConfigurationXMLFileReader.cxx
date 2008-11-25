8b1edc213f964ca7734512a59ba5efa13e0e9810
Ziv Yaniv <zivy@isis.georgetown.edu>
2008-11-25 16:14:54 -0500

    fixed style

--- Examples/ApplicationsCommon/TrackerConfiguration/configurationFileReaders/igstkPolarisHybridConfigurationXMLFileReader.cxx
+++ Examples/ApplicationsCommon/TrackerConfiguration/configurationFileReaders/igstkPolarisHybridConfigurationXMLFileReader.cxx
@@ -13,16 +13,15 @@ PolarisHybridConfigurationXMLFileReader::StartElement(
   const char * name, 
   const char **atts )
 {
-          //let the super class try to analyze the current tag
+  //let the super class try to analyze the current tag
   Superclass::StartElement( name, atts );
                        
   if( itksys::SystemTools::Strucmp( name, "srom_file" ) == 0 ||
     itksys::SystemTools::Strucmp( name, "control_box_port" ) == 0 ) 
-  {
+    {
     if( !m_ReadingToolConfiguration )
       throw FileFormatException( "Tag not nested correctly in xml structure." );
-  }
-
+    }
 }
 
 
@@ -30,21 +29,21 @@ void
 PolarisHybridConfigurationXMLFileReader::EndElement( 
   const char *name )
 {
-          //let the super class try to analyze the current tag
+  //let the super class try to analyze the current tag
   Superclass::EndElement( name );
   
          
   if( m_ReadingToolConfiguration &&
     itksys::SystemTools::Strucmp( name, "srom_file" ) == 0 )
-  {
+    {
     ProcessSromFile();
-  }
+    }
   if( m_ReadingToolConfiguration &&
     itksys::SystemTools::Strucmp( name, "control_box_port" ) == 0 )
-  {
+    {
     ProcessControlBoxPort();
     this->m_HaveCurrentControlBoxPort = true;  
-  }
+    }
 }
 
 
@@ -83,11 +82,15 @@ PolarisHybridConfigurationXMLFileReader::ProcessControlBoxPort()
   instr>>controlBoxPort;
 
   if( !instr.eof() )
+    {
     throw FileFormatException( 
            "Error in control box port tag data, possibly non numeric values." );
+    }
   if( controlBoxPort<1 || controlBoxPort>12 )
+    {
     throw FileFormatException("Invalid value given for control box port."); 
-  this->m_CurrentControlBoxPort = controlBoxPort;    
+    }
+  this->m_CurrentControlBoxPort = controlBoxPort;
 }
 
 
@@ -102,37 +105,40 @@ PolarisHybridConfigurationXMLFileReader::ProcessToolData()
        this->m_CurrentSromFileName.empty() )
     throw FileFormatException( "Missing srom file and/or control box port." );
 
-              //if the tool section does not have a "calibration_file" tag 
-              //we assume the calibration is identity
+  //if the tool section does not have a "calibration_file" tag 
+  //we assume the calibration is identity
 
   TrackerToolConfiguration *toolConfig;
 
-                   //wireless tool
+  //wireless tool
   if( !this->m_HaveCurrentControlBoxPort )
-  {
+    {
     igstk::PolarisWirelessToolConfiguration *wirelessToolConfig = 
       new igstk::PolarisWirelessToolConfiguration();
-      wirelessToolConfig->SetSROMFile( this->m_CurrentSromFileName );
-      toolConfig = wirelessToolConfig;
-  }
+    wirelessToolConfig->SetSROMFile( this->m_CurrentSromFileName );
+    toolConfig = wirelessToolConfig;
+    }
   else //wired tool
-  {
+    {
     igstk::PolarisWiredToolConfiguration *wiredToolConfig = 
       new igstk::PolarisWiredToolConfiguration();
     wiredToolConfig->SetPortNumber( this->m_CurrentControlBoxPort );
-                       //srom is optional
+    //srom is optional
     if( !this->m_CurrentSromFileName.empty() )
-      wiredToolConfig->SetSROMFile( this->m_CurrentSromFileName );      
+      wiredToolConfig->SetSROMFile( this->m_CurrentSromFileName );
     toolConfig = wiredToolConfig;
-  }
+    }
   toolConfig->SetToolName( this->m_CurrentToolName );
   toolConfig->SetCalibrationTransform( this->m_CurrentToolCalibration );  
   if( this->m_CurrentToolIsReference )
+    {
     this->m_ReferenceTool = toolConfig;
+    }
   else
+    {
     this->m_TrackerToolList.push_back( toolConfig );
-
-                 //reset all tool data to initial state
+    }
+  //reset all tool data to initial state
   this->m_HaveCurrentControlBoxPort = false;
   this->m_CurrentToolIsReference = false;
   this->m_CurrentSromFileName.clear();
@@ -145,10 +151,10 @@ PolarisHybridConfigurationXMLFileReader::ProcessToolData()
 bool 
 PolarisHybridConfigurationXMLFileReader::HaveConfigurationData()
 {
-        //check that we have a refresh rate for the tracking system, at least 
-        //one tool, and all the serial communication settings
+  //check that we have a refresh rate for the tracking system, at least 
+  //one tool, and all the serial communication settings
   return ( this->m_HaveRefreshRate &&
-           this->m_TrackerToolList.size()!=0 &&
+           this->m_TrackerToolList.size() != 0 &&
            this->m_HaveComPort && this->m_HaveBaudRate && 
            this->m_HaveDataBits && this->m_HaveParity && 
            this->m_HaveStopBits && this->m_HaveHandshake );
@@ -162,12 +168,12 @@ throw ( FileFormatException )
   igstk::PolarisHybridTrackerConfiguration::Pointer trackerConfig = 
     GetPolarisConfiguration();
     
-                //this request is guaranteed to succeed as the refresh rate
-                //is validated in the TrackerConfigurationXMLReaderBase                
+  //this request is guaranteed to succeed as the refresh rate
+  //is validated in the TrackerConfigurationXMLReaderBase
   trackerConfig->RequestSetFrequency( this->m_RefreshRate );
-                  //the communication settings requests are guaranteed to
-                  //succeed as they are validated in the 
-                  //SerialCommunicatingTrackerConfigurationXMLFileReader
+  //the communication settings requests are guaranteed to
+  //succeed as they are validated in the 
+  //SerialCommunicatingTrackerConfigurationXMLFileReader
   trackerConfig->RequestSetCOMPort( this->m_COMPort );
   trackerConfig->RequestSetBaudRate( this->m_BaudRate );
   trackerConfig->RequestSetDataBits( this->m_DataBits );
@@ -175,7 +181,7 @@ throw ( FileFormatException )
   trackerConfig->RequestSetStopBits( this->m_StopBits );
   trackerConfig->RequestSetHandshake( this->m_Handshake );
 
-                     //add the tools
+  //add the tools
   std::vector<TrackerToolConfiguration *>::iterator it, end; 
   it = this->m_TrackerToolList.begin();
   end = this->m_TrackerToolList.end();  
@@ -185,24 +191,24 @@ throw ( FileFormatException )
     AddToolFailureObserver::New();
   trackerConfig->AddObserver( igstk::TrackerConfiguration::AddToolFailureEvent(),
                               failureObserver );
-  for( ; it!=end; it++ )
-  {
+  for(; it!=end; it++ )
+    {
     trackerConfig->RequestAddTool( *it );
     if( failureObserver->GotAddToolFailure() )
-    {      
+      {
       throw FileFormatException( failureObserver->GetAddToolFailure() );
+      }
     }
-  }
   if( this->m_ReferenceTool != NULL )
-  {
+    {
     trackerConfig->RequestAddReference( this->m_ReferenceTool );
     if( failureObserver->GotAddToolFailure() )
-    {
+      {
       throw FileFormatException( failureObserver->GetAddToolFailure() );
+      }
     }
-  }
  
-               //explicitly upcast to avoid the compiler warning
+  //explicitly upcast to avoid the compiler warning
   igstk::TrackerConfiguration::Pointer genericConfig;
   genericConfig  = trackerConfig;
   return genericConfig;
