dac44780554afea3ac7aa49d1e3be8143f9d4b86
Ziv Yaniv <zivy@isis.georgetown.edu>
2008-12-01 11:22:36 -0500

    fixed style

--- Examples/ApplicationsCommon/TrackerConfiguration/configurationFileReaders/igstkPolarisVicraConfigurationXMLFileReader.h
+++ Examples/ApplicationsCommon/TrackerConfiguration/configurationFileReaders/igstkPolarisVicraConfigurationXMLFileReader.h
@@ -7,35 +7,38 @@
 
 namespace igstk
 {
+/**
+ * \class PolarisVicraConfigurationXMLFileReader This class is used to read the 
+ *        xml configuration file for NDI's Polaris vicra tracker.
+ */  
 class PolarisVicraConfigurationXMLFileReader : 
    public PolarisWirelessConfigurationXMLFileReader 
 {
 public:
 
-    //standard typedefs
+  //standard typedefs
   typedef PolarisVicraConfigurationXMLFileReader                  Self;
   typedef PolarisWirelessConfigurationXMLFileReader               Superclass;
   typedef itk::SmartPointer<Self>                                 Pointer;
 
-           //run-time type information (and related methods)
+  //run-time type information (and related methods)
   itkTypeMacro( PolarisVicraConfigurationXMLFileReader, 
                 PolarisWirelessConfigurationXMLFileReader );
 
-           //method for creation through the object factory
+  //method for creation through the object factory
   itkNewMacro( Self );
 
 protected:
-          //this is the constructor that is called by the factory to 
-         //create a new object
+  //this is the constructor that is called by the factory to 
+  //create a new object
   PolarisVicraConfigurationXMLFileReader() : 
-         PolarisWirelessConfigurationXMLFileReader()
-         {}
-  virtual ~PolarisVicraConfigurationXMLFileReader() {}
+    PolarisWirelessConfigurationXMLFileReader() { }
+  virtual ~PolarisVicraConfigurationXMLFileReader() { }
 
- virtual double GetMaximalRefreshRate();
- virtual std::string GetSystemType();
- virtual igstk::PolarisWirelessTrackerConfiguration::Pointer 
-   GetPolarisConfiguration();
+  virtual double GetMaximalRefreshRate();
+  virtual std::string GetSystemType();
+  virtual igstk::PolarisWirelessTrackerConfiguration::Pointer 
+    GetPolarisConfiguration();
 
 
 private:
