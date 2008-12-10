68bc2aca3f901647cc550b06b8de75654c06aafd
Luis Ibanez <luis.ibanez@kitware.com>
2008-12-09 21:20:19 -0500

    COMP: Adding throw() and virtual modifiers to the destructor of a class deriving from std::exception.

--- Examples/ApplicationsCommon/TrackerConfiguration/configurationFileReaders/igstkTrackerConfigurationXMLFileReaderBase.h
+++ Examples/ApplicationsCommon/TrackerConfiguration/configurationFileReaders/igstkTrackerConfigurationXMLFileReaderBase.h
@@ -21,7 +21,7 @@ public:
   {
   public:
     UnexpectedTrackerTypeException(){ };
-    ~UnexpectedTrackerTypeException(){ };
+    virtual ~UnexpectedTrackerTypeException() throw() { };
   };
 
   /**
