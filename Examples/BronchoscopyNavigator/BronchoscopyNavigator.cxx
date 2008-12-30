fdbe051e1bfc2e9e1f1f37ffc20554c0f0895d52
Sebastian Ordas <sebastian.ordas@gmail.com>
2008-12-29 21:21:24 -0500

    ENH: unified "xx_SpatialObject_xx" and "xx_ObjectRepresentation_xx" nomenclature

--- Examples/BronchoscopyNavigator/BronchoscopyNavigator.cxx
+++ Examples/BronchoscopyNavigator/BronchoscopyNavigator.cxx
@@ -1744,7 +1744,7 @@ BronchoscopyNavigator::ReportSuccessAcceptingRegistrationProcessing()
   igstk::Transform identity;
   identity.SetToIdentity( igstk::TimeStamp::GetLongestPossibleTime() );
 
-  m_ToolProjection = igstk::ToolProjectionObject::New();
+  m_ToolProjection = ToolProjectionType::New();
   m_ToolProjection->SetSize(150);
   m_ToolProjection->RequestSetTransformAndParent( identity, m_WorldReference );
 
