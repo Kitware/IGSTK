bcaace2ecbb568a9234f589525cc37774a7dd1de
Ozgur Guler <oezguer.gueler@i-med.ac.at>
2009-03-17 15:50:51 -0400

    test for commit

---
+++ Examples/BronchoscopyNavigator/commitTest.cpp
@@ -0,0 +1,21 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    commitTest.cpp
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISC  Insight Software Consortium.  All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __Navigator_h
+#define __Navigator_h
+
+#endif
