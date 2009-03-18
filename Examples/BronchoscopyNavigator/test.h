cdbc1587d2658141d920c48ba1ead69af1207c28
Ozgur Guler <oezguer.gueler@i-med.ac.at>
2009-03-18 05:47:59 -0400

    test

---
+++ Examples/BronchoscopyNavigator/test.h
@@ -0,0 +1,17 @@
+/*
+ * test.h
+ *
+ *  Created on: Mar 18, 2009
+ *      Author: root
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+class test {
+public:
+  test();
+  virtual ~test();
+};
+
+#endif /* TEST_H_ */
