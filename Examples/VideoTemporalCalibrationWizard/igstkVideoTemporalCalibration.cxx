0d5ff58c793f0e8ac67e23449166220a122547f3
Ozgur Guler <oezguer.gueler@i-med.ac.at>
2009-03-18 10:51:37 -0400

    Container class

---
+++ Examples/VideoTemporalCalibrationWizard/igstkVideoTemporalCalibration.cxx
@@ -0,0 +1,34 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkVideoTemporalCalibration.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISC  Insight Software Consortium.  All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "igstkVideoTemporalCalibration.h"
+
+namespace igstk
+{
+
+  /** Constructor: Initializes all internal variables. */
+  VideoTemporalCalibration::VideoTemporalCalibration()
+  {
+
+  }
+
+  /** Destructor */
+  VideoTemporalCalibration::~VideoTemporalCalibration()
+  {
+
+  }
+} // end of namespace
