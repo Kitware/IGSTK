5ca98a6d80359c2e6a11f057103632d311897a2c
Ozgur Guler <oezguer.gueler@i-med.ac.at>
2009-03-18 10:20:29 -0400

    Configurations for ImagingSource converter

---
+++ Examples/ApplicationsCommon/ImagerConfiguration/igstkImagingSourceImagerConfiguration.h
@@ -0,0 +1,76 @@
+/*=========================================================================
+
+Program:   Image Guided Surgery Software Toolkit
+Module:    igstkImagingSourceImagerConfiguration.h
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) ISC  Insight Software Consortium.  All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/copyright.htm for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __igstkImagingSourceImagerConfiguration_h
+#define __igstkImagingSourceImagerConfiguration_h
+
+#include "igstkImagerConfiguration.h"
+
+namespace igstk
+{
+
+
+/**
+ * A ImagingSource imager tool.
+ */
+class ImagingSourceToolConfiguration : public ImagerToolConfiguration
+{
+public:
+  ImagingSourceToolConfiguration();
+  ImagingSourceToolConfiguration(const ImagingSourceToolConfiguration &other);
+  virtual ~ImagingSourceToolConfiguration();
+
+  virtual std::string GetToolTypeAsString();
+
+protected:
+
+};
+
+
+/**
+ * Configuration for the ImagingSource imager.
+ */
+class ImagingSourceImagerConfiguration : public ImagerConfiguration//SocketCommunicatingImagerConfiguration
+{
+public:
+  ImagingSourceImagerConfiguration();
+  virtual ~ImagingSourceImagerConfiguration();
+
+  igstkSetMacro( BufferSize, unsigned int );
+  igstkGetMacro( BufferSize, unsigned int );
+
+  /**
+   * Get the manufacturer specified maximal refresh rate.
+   */
+  virtual double GetMaximalRefreshRate();
+
+protected:
+  virtual void InternalAddTool( const ImagerToolConfiguration *tool );
+
+private:
+
+                  //manufacturer specified maximal refresh rate [Hz]
+  static const double MAXIMAL_REFERESH_RATE;
+
+  unsigned int m_BufferSize;
+
+
+};
+
+
+} // end of name space
+#endif
