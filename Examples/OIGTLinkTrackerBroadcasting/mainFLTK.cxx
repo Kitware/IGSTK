9f2d86eaa11b5d15c8ca539f7b8f5af2e07fa63e
Andinet Enquobahrie <andinet.enqu@kitware.com>
2009-03-20 13:52:54 -0400

    ENH: check in example code

---
+++ Examples/OIGTLinkTrackerBroadcasting/mainFLTK.cxx
@@ -0,0 +1,20 @@
+#include "OIGTLinkTrackingBroadcasterGUI.h"
+
+int main(int argc, char *argv[])
+{
+  OIGTLinkTrackingBroadcasterGUI app;
+
+                        //required by IGSTK for the time stamping used by the tracker
+  igstk::RealTimeClock::Initialize();
+
+  app.Show();
+  
+  while( !app.HasQuitted() )
+  {
+    Fl::wait(0.001);
+    igstk::PulseGenerator::CheckTimeouts();
+  }
+
+
+  return EXIT_SUCCESS;
+}
