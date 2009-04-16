32e23dd48827910d9516c53996ac78e20a64aa35
Patrick Cheng <cheng@isis.georgetown.edu>
2009-04-15 22:44:51 -0400

    BUG: 3624 The Book. Mismatch latex tags

--- Examples/TransformReaders/TransformReaderExample.cxx
+++ Examples/TransformReaders/TransformReaderExample.cxx
@@ -155,7 +155,7 @@ int main(int argc, char *argv[])
     // EndLatex
     // BeginCodeSnippet    
   transformFileReader->RequestRead();
-  // BeginCodeSnippet    
+  // EndCodeSnippet    
   if( readObserver->GotReadFailure() )
     {
     std::cerr<<readObserver->GetErrorMessage()<<"\n";
