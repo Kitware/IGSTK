fbdecd18059266b64900ba2bc7539cec6f58f79b
Ozgur Guler <oezguer.gueler@i-med.ac.at>
2009-06-17 18:00:26 -0400

    Code review

--- Source/igstkWebcamWinVideoImager.cxx
+++ Source/igstkWebcamWinVideoImager.cxx
@@ -21,7 +21,6 @@
 #pragma warning( disable : 4786 )
 #endif
 
-
 #include "igstkWebcamWinVideoImager.h"
 #include "igstkEvents.h"
 #include "vtkImageData.h"
@@ -46,18 +45,12 @@
 #define MAX_DEVICES 64
 #define MAX_FORMATS 64
 
-
 namespace igstk
 {
 
-
 //Initialize static variables
-std::map< unsigned int, std::string> WebcamWinVideoImager::m_ErrorCodeContainer;
-bool WebcamWinVideoImager::m_ErrorCodeListCreated = false;
-unsigned char* WebcamWinVideoImager::pixels[1] = {NULL};
-unsigned char* WebcamWinVideoImager::frameBuffer = NULL;
-itk::MutexLock::Pointer WebcamWinVideoImager::m_FrameBufferLock = itk::MutexLock::New();
-
+itk::MutexLock::Pointer WebcamWinVideoImager::m_FrameBufferLock = 
+                                                          itk::MutexLock::New();
 
 /** Constructor: Initializes all internal variables. */
 WebcamWinVideoImager::WebcamWinVideoImager(void):m_StateMachine(this)
@@ -71,109 +64,20 @@ WebcamWinVideoImager::WebcamWinVideoImager(void):m_StateMachine(this)
   // with the imager to the main thread.
   m_BufferLock = itk::MutexLock::New();
 
-
-
-  // Create error code list if it hasn't been
-  // created.
-  if( ! m_ErrorCodeListCreated )
-    {
-    this->CreateErrorCodeList();
-    m_ErrorCodeListCreated = true;
-    }
-
-  this->capture = 0;
-  this->cvframe = 0;
-  key = 0;
-
-
+  this->m_Capture = 0;
+  this->m_Cvframe = 0;
 }
 
 /** Destructor */
 WebcamWinVideoImager::~WebcamWinVideoImager(void)
 {
-  cvReleaseCapture( &capture );
-}
-
-/** Create a map data structure containing MTC error code and description */
-void WebcamWinVideoImager::CreateErrorCodeList()
-{
-  ErrorCodeContainerType & ecc = m_ErrorCodeContainer;
-
-  ecc[0]  = "OK";
-  ecc[1]  = "Invalid object handle";
-  ecc[2]  = "Reentrant access - library is not thread-safe";
-  ecc[3]  = "Internal WebcamWinVideoImager software error";
-  ecc[4]  = "Null pointer parameter";
-  ecc[5]  = "Out of memory";
-  ecc[6]  = "Parameter out of range";
-  ecc[7]  = "String parameter too long";
-  ecc[8]  = "Insufficient space allocated by the client to the output buffer";
-  ecc[9]  = "Camera not initialized";
-  ecc[10] = "Camera already initialized - cannot be initialized twice";
-  ecc[11] = "Camera initialization failed";
-  ecc[12] = "MTC is incompatible with a software module it calls";
-  ecc[13] = "Calibration file error: unrecognized camera model";
-  ecc[14] = "Path not set";
-  ecc[15] = "Cannot access the directory specified";
-  ecc[16] = "Write to file failed";
-  ecc[17] = "Invalid Index parameter";
-  ecc[18] = "Invalid SideI parameter";
-  ecc[19] = "Invalid Divisor parameter";
-  ecc[20] = "Attempting to access an item of an empty IntCollection";
-  ecc[21] = "Insufficient samples";
-  ecc[22] = "Insufficient samples that fit within the acceptance tolerance";
-  ecc[23] = "Odd number of vector samples";
-  ecc[24] = "Less than 2 vectors";
-  ecc[25] = "More than maximum vectors per facet";
-  ecc[26] = "Error exceeds tolerance";
-  ecc[27] = "Insufficient angle between vectors";
-  ecc[28] = "First vector is shorter than the second";
-  ecc[29] = "Vector lengths are too similar";
-  ecc[30] = "Template vector has 0 length";
-  ecc[31] = "The template has not been created or loaded";
-  ecc[32] = "Template file is corrupt";
-  ecc[33] = "Maximum number of marker templates allowed exceeded";
-  ecc[34] = "Geometries of different facets are too similar";
-  ecc[35] = "Noncompliant facet definition";
-  ecc[36] = "The SampledVectorPairsCollection contains non-Vector handles";
-  ecc[37] = "Empty pixels buffer";
-  ecc[38] = "Dimensions do not match";
-  ecc[39] = "File open failed";
-  ecc[40] = "File read failed";
-  ecc[41] = "File write failed";
-  ecc[42] = "Cannot open calibration file "
-            "(typically named [driver]_[ser num].calib";
-  ecc[43] = "Not a calibration file";
-  ecc[44] = "Calibration file contents corrupt";
-  ecc[45] = "Calibration file was not generated from this camera";
-  ecc[46] = "Calibration file not loaded";
-  ecc[47] = "Incorrect file version";
-  ecc[48] = "Input image location is out of bounds of the measurement volume";
-  ecc[49] = "Input image locations do not triangulate to a valid 3-D point";
-  ecc[50] = "Transform between coordinate spaces is unknown";
-  ecc[51] = "The given camera object was not found in the cameras array";
-  ecc[52] = "Feature Data unavailable for the current frame";
-  ecc[53] = "Feature Data is corrupt or incompatible with the current version";
-  ecc[54] = "XYZ position is outside of calibrated field of view";
-  ecc[55] = "Grab frame error";
-}
-
-std::string
-WebcamWinVideoImager::GetErrorDescription( unsigned int code )
-{
-  if ( code >= 0 && code <= 55 )
-    {
-    return WebcamWinVideoImager::m_ErrorCodeContainer[code];
-    }
-  else
-    {
-    return "Unknown error code";
-    }
+  cvReleaseCapture( &m_Capture );
 }
 
 WebcamWinVideoImager::ResultType WebcamWinVideoImager::InternalOpen( void )
 {
-  igstkLogMacro( DEBUG, "igstk::WebcamWinVideoImager::InternalOpen called ...\n");
+  igstkLogMacro( DEBUG, 
+                      "igstk::WebcamWinVideoImager::InternalOpen called ...\n");
 
   if( ! this->Initialize() )
     {
@@ -189,10 +93,11 @@ bool WebcamWinVideoImager::Initialize( void )
 {
   igstkLogMacro( DEBUG, "igstk::WebcamWinVideoImager::Initialize called ...\n");
 
-    capture = cvCaptureFromCAM( 0 );
-    if ( !capture ) {
-        fprintf( stderr, "Cannot open initialize webcam!\n" );
-        return 1;
+  m_Capture = cvCaptureFromCAM( 0 );
+  if ( !m_Capture ) 
+    {
+    fprintf( stderr, "Cannot open initialize webcam!\n" );
+    return 1;
     }
 
   return SUCCESS;
@@ -204,7 +109,7 @@ WebcamWinVideoImager
 ::VerifyVideoImagerToolInformation( const VideoImagerToolType * imagerTool )
 {
   igstkLogMacro( DEBUG,
-    "igstk::WebcamWinVideoImager::VerifyVideoImagerToolInformation called ...\n");
+  "igstk::WebcamWinVideoImager::VerifyVideoImagerToolInformation called ...\n");
 
   return SUCCESS;
 }
@@ -212,29 +117,32 @@ WebcamWinVideoImager
 /** Detach camera. */
 WebcamWinVideoImager::ResultType WebcamWinVideoImager::InternalClose( void )
 {
-  igstkLogMacro( DEBUG, "igstk::WebcamWinVideoImager::InternalClose called ...\n");
+  igstkLogMacro( DEBUG, 
+                     "igstk::WebcamWinVideoImager::InternalClose called ...\n");
 
-  cvReleaseCapture( &capture );
+  cvReleaseCapture( &m_Capture );
 
   return SUCCESS;
 }
 
 /** Put the imaging device into imaging mode. */
-WebcamWinVideoImager::ResultType WebcamWinVideoImager::InternalStartImaging( void )
+WebcamWinVideoImager::ResultType 
+WebcamWinVideoImager::InternalStartImaging( void )
 {
   igstkLogMacro( DEBUG,
-    "igstk::WebcamWinVideoImager::InternalStartImaging called ...\n");
+              "igstk::WebcamWinVideoImager::InternalStartImaging called ...\n");
 
-    igstk::DoubleTypeEvent evt;
-    evt.Set( 1.0 );
-    this->InvokeEvent( evt );
-    std::cout << "Start capture on device!" << std::endl;
+  igstk::DoubleTypeEvent evt;
+  evt.Set( 1.0 );
+  this->InvokeEvent( evt );
+  std::cout << "Start capture on device!" << std::endl;
 
   return SUCCESS;
 }
 
 /** Take the imaging device out of imaging mode. */
-WebcamWinVideoImager::ResultType WebcamWinVideoImager::InternalStopImaging( void )
+WebcamWinVideoImager::ResultType 
+WebcamWinVideoImager::InternalStopImaging( void )
 {
   igstkLogMacro( DEBUG,
     "igstk::WebcamWinVideoImager::InternalStopImaging called ...\n");
@@ -247,7 +155,8 @@ WebcamWinVideoImager::ResultType WebcamWinVideoImager::InternalStopImaging( void
 /** Reset the imaging device to put it back to its original state. */
 WebcamWinVideoImager::ResultType WebcamWinVideoImager::InternalReset( void )
 {
-  igstkLogMacro( DEBUG, "igstk::WebcamWinVideoImager::InternalReset called ...\n");
+  igstkLogMacro( DEBUG, 
+                     "igstk::WebcamWinVideoImager::InternalReset called ...\n");
   return SUCCESS;
 }
 
@@ -281,8 +190,9 @@ WebcamWinVideoImager::ResultType WebcamWinVideoImager::InternalUpdateStatus()
     // only report tools that have useful data
     if (! this->m_ToolStatusContainer[inputItr->first])
     {
-      igstkLogMacro( DEBUG, "igstk::WebcamWinVideoImager::InternalUpdateStatus: " <<
-              "tool " << inputItr->first << " failed\n");
+      igstkLogMacro( DEBUG, 
+                        "igstk::WebcamWinVideoImager::InternalUpdateStatus: " <<
+                                     "tool " << inputItr->first << " failed\n");
 
       // report to the imager tool that the imager is not available
       this->ReportImagingToolNotAvailable(
@@ -310,13 +220,14 @@ WebcamWinVideoImager::ResultType WebcamWinVideoImager::InternalUpdateStatus()
   return SUCCESS;
 }
 
-/** Update the shared memory buffer and the tool's internal frame. This function
- *  is called by the thread that communicates with the imager while
- *  the imager is in the Imaging state. */
+/**Update the shared memory buffer and the tool's internal frame. This 
+ * function is called by the thread that communicates with the imager while
+ * the imager is in the Imaging state. */
 WebcamWinVideoImager::ResultType
 WebcamWinVideoImager::InternalThreadedUpdateStatus( void )
 {
-  igstkLogMacro( DEBUG, "igstk::WebcamWinVideoImager::InternalThreadedUpdateStatus called ...\n");
+  igstkLogMacro( DEBUG, 
+      "igstk::WebcamWinVideoImager::InternalThreadedUpdateStatus called ...\n");
 
   cout << " ";
 
@@ -329,13 +240,13 @@ WebcamWinVideoImager::InternalThreadedUpdateStatus( void )
   InputConstIterator inputEnd = this->m_ToolFrameBuffer.end();
 
   while( inputItr != inputEnd )
-  {
+    {
     this->m_ToolStatusContainer[inputItr->first] = 0;
     ++inputItr;
-  }
+    }
 
   try
-  {
+    {
     igstkLogMacro( DEBUG, "InternalThreadedUpdateStatus Receive passed" );
     // Check if an imager tool was added with this device name
     typedef VideoImagerToolFrameContainerType::iterator InputIterator;
@@ -348,23 +259,25 @@ WebcamWinVideoImager::InternalThreadedUpdateStatus( void )
 
       // create the frame
       VideoImagerToolsContainerType imagerToolContainer =
-                                              this->GetVideoImagerToolContainer();
+                                            this->GetVideoImagerToolContainer();
 
       FrameType* frame = new FrameType();
-      frame = this->GetVideoImagerToolFrame( imagerToolContainer[deviceItr->first]);
+      frame = this->GetVideoImagerToolFrame( 
+                                         imagerToolContainer[deviceItr->first]);
 
       unsigned int frameDims[3];
       imagerToolContainer[deviceItr->first]->GetFrameDimensions(frameDims);
 
       WebcamWinVideoImager::m_FrameBufferLock->Lock();
 
-      cvframe = cvQueryFrame( capture );
+      m_Cvframe = cvQueryFrame( m_Capture );
 
-      if( !cvframe )
+      if( !m_Cvframe )
         cout << "frame failed ";
 
       memcpy(frame->GetImagePtr(),
-          (unsigned char*)cvframe->imageData,frameDims[0]*frameDims[1]*frameDims[2]);
+             (unsigned char*)m_Cvframe->imageData,
+             frameDims[0]*frameDims[1]*frameDims[2]);
 
       WebcamWinVideoImager::m_FrameBufferLock->Unlock();
 
