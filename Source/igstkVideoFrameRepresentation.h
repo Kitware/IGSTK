f385bfa38736c41b1fdbbd5110f61ba8c6d640fa
Ozgur Guler <oezguer.gueler@i-med.ac.at>
2009-06-17 18:10:31 -0400

    Code review

--- Source/igstkVideoFrameRepresentation.h
+++ Source/igstkVideoFrameRepresentation.h
@@ -9,9 +9,6 @@
   Copyright (c) ISC  Insight Software Consortium.  All rights reserved.
   See IGSTKCopyright.txt or http://www.igstk.org/copyright.htm for details.
 
-  Made by SINTEF Health Research - Medical technology:
-  http://www.sintef.no/medtech
-
      This software is distributed WITHOUT ANY WARRANTY; without even
      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
      PURPOSE.  See the above copyright notices for more information.
