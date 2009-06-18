2f1e50a26951e3be200ca7bc8166ccd4903b90b1
Ozgur Guler <oezguer.gueler@i-med.ac.at>
2009-06-18 05:29:38 -0400

    Code Review

--- Source/igstkOpenIGTLinkVideoImager.cxx
+++ Source/igstkOpenIGTLinkVideoImager.cxx
@@ -36,18 +36,9 @@
 namespace igstk
 {
 
-//Initialize static variables
-std::map< unsigned int, std::string> 
-                                   OpenIGTLinkVideoImager::m_ErrorCodeContainer;
-bool OpenIGTLinkVideoImager::m_ErrorCodeListCreated = false;
-
 /** Constructor: Initializes all internal variables. */
 OpenIGTLinkVideoImager::OpenIGTLinkVideoImager(void):m_StateMachine(this)
 {
-  this->m_NumberOfTools = 0;
-
-  this->m_FirstFrame = true;
-
   this->SetThreadingEnabled( true );
 
   // Lock for the data buffer that is used to transfer the
@@ -55,97 +46,11 @@ OpenIGTLinkVideoImager::OpenIGTLinkVideoImager(void):m_StateMachine(this)
   // with the imager to the main thread.
   m_BufferLock = itk::MutexLock::New();
 
-  // Create error code list if it hasn't been
-  // created.
-  //
-  if ( ! m_ErrorCodeListCreated )
-    {
-    this->CreateErrorCodeList();
-    m_ErrorCodeListCreated = true;
-    }
 }
 
 /** Destructor */
 OpenIGTLinkVideoImager::~OpenIGTLinkVideoImager(void)
 {
-
-}
-
-/** Create a map data structure containing MTC error code and description */
-void OpenIGTLinkVideoImager::CreateErrorCodeList()
-{
-  ErrorCodeContainerType & ecc = m_ErrorCodeContainer;
-
-  ecc[0]  = "OK";
-  ecc[1]  = "Invalid object handle";
-  ecc[2]  = "Reentrant access - library is not thread-safe";
-  ecc[3]  = "Internal OpenIGTLinkVideoImager software error";
-  ecc[4]  = "Null pointer parameter";
-  ecc[5]  = "Out of memory";
-  ecc[6]  = "Parameter out of range";
-  ecc[7]  = "String parameter too long";
-  ecc[8]  = "Insufficient space allocated by the client to the output buffer";
-  ecc[9]  = "Camera not initialized";
-  ecc[10] = "Camera already initialized - cannot be initialized twice";
-  ecc[11] = "Camera initialization failed";
-  ecc[12] = "MTC is incompatible with a software module it calls";
-  ecc[13] = "Calibration file error: unrecognized camera model";
-  ecc[14] = "Path not set";
-  ecc[15] = "Cannot access the directory specified";
-  ecc[16] = "Write to file failed";
-  ecc[17] = "Invalid Index parameter";
-  ecc[18] = "Invalid SideI parameter";
-  ecc[19] = "Invalid Divisor parameter";
-  ecc[20] = "Attempting to access an item of an empty IntCollection";
-  ecc[21] = "Insufficient samples";
-  ecc[22] = "Insufficient samples that fit within the acceptance tolerance";
-  ecc[23] = "Odd number of vector samples";
-  ecc[24] = "Less than 2 vectors";
-  ecc[25] = "More than maximum vectors per facet";
-  ecc[26] = "Error exceeds tolerance";
-  ecc[27] = "Insufficient angle between vectors";
-  ecc[28] = "First vector is shorter than the second";
-  ecc[29] = "Vector lengths are too similar";
-  ecc[30] = "Template vector has 0 length";
-  ecc[31] = "The template has not been created or loaded";
-  ecc[32] = "Template file is corrupt";
-  ecc[33] = "Maximum number of marker templates allowed exceeded";
-  ecc[34] = "Geometries of different facets are too similar";
-  ecc[35] = "Noncompliant facet definition";
-  ecc[36] = "The SampledVectorPairsCollection contains non-Vector handles";
-  ecc[37] = "Empty pixels buffer";
-  ecc[38] = "Dimensions do not match";
-  ecc[39] = "File open failed";
-  ecc[40] = "File read failed";
-  ecc[41] = "File write failed";
-  ecc[42] = "Cannot open calibration file "
-            "(typically named [driver]_[ser num].calib";
-  ecc[43] = "Not a calibration file";
-  ecc[44] = "Calibration file contents corrupt";
-  ecc[45] = "Calibration file was not generated from this camera";
-  ecc[46] = "Calibration file not loaded";
-  ecc[47] = "Incorrect file version";
-  ecc[48] = "Input image location is out of bounds of the measurement volume";
-  ecc[49] = "Input image locations do not triangulate to a valid 3-D point";
-  ecc[50] = "Transform between coordinate spaces is unknown";
-  ecc[51] = "The given camera object was not found in the cameras array";
-  ecc[52] = "Feature Data unavailable for the current frame";
-  ecc[53] = "Feature Data is corrupt or incompatible with the current version";
-  ecc[54] = "XYZ position is outside of calibrated field of view";
-  ecc[55] = "Grab frame error";
-}
-
-std::string
-OpenIGTLinkVideoImager::GetErrorDescription( unsigned int code )
-{
-  if ( code >= 0 && code <= 55 )
-    {
-    return OpenIGTLinkVideoImager::m_ErrorCodeContainer[code];
-    }
-  else
-    {
-    return "Unknown error code";
-    }
 }
 
 OpenIGTLinkVideoImager::ResultType OpenIGTLinkVideoImager::InternalOpen( void )
@@ -166,12 +71,9 @@ OpenIGTLinkVideoImager::ResultType OpenIGTLinkVideoImager::InternalOpen( void )
 /** Initialize socket */
 bool OpenIGTLinkVideoImager::Initialize( void )
 {
-  igstkLogMacro( DEBUG, 
+  igstkLogMacro( DEBUG,
                       "igstk::OpenIGTLinkVideoImager::Initialize called ...\n");
-
-  bool result = true;
-
-  return result;
+  return true;
 }
 
 /** Set the communication object*/
@@ -195,16 +97,16 @@ OpenIGTLinkVideoImager
 }
 
 /** Detach camera. */
-OpenIGTLinkVideoImager::ResultType 
+OpenIGTLinkVideoImager::ResultType
 OpenIGTLinkVideoImager::InternalClose( void )
 {
-  igstkLogMacro( DEBUG, 
+  igstkLogMacro( DEBUG,
                    "igstk::OpenIGTLinkVideoImager::InternalClose called ...\n");
   return SUCCESS;
 }
 
 /** Put the tracking device into tracking mode. */
-OpenIGTLinkVideoImager::ResultType 
+OpenIGTLinkVideoImager::ResultType
 OpenIGTLinkVideoImager::InternalStartImaging( void )
 {
   igstkLogMacro( DEBUG,
@@ -242,7 +144,8 @@ OpenIGTLinkVideoImager::InternalStartImaging( void )
 
   if ( times>=10 )
   {
-    std::cout << " Could not find a client!" << std::endl;
+    igstkLogMacro( DEBUG,
+        "igstk::OpenIGTLinkVideoImager: Could not find a client!\n");
     return FAILURE;
   }
   else
@@ -256,7 +159,7 @@ OpenIGTLinkVideoImager::InternalStartImaging( void )
 }
 
 /** Take the tracking device out of tracking mode. */
-OpenIGTLinkVideoImager::ResultType 
+OpenIGTLinkVideoImager::ResultType
 OpenIGTLinkVideoImager::InternalStopImaging( void )
 {
   igstkLogMacro( DEBUG,
@@ -266,17 +169,17 @@ OpenIGTLinkVideoImager::InternalStopImaging( void )
 }
 
 /** Reset the tracking device to put it back to its original state. */
-OpenIGTLinkVideoImager::ResultType 
+OpenIGTLinkVideoImager::ResultType
 OpenIGTLinkVideoImager::InternalReset( void )
 {
-  igstkLogMacro( DEBUG, 
+  igstkLogMacro( DEBUG,
                    "igstk::OpenIGTLinkVideoImager::InternalReset called ...\n");
   return SUCCESS;
 }
 
 
 /** Update the status and the transforms for all VideoImagerTools. */
-OpenIGTLinkVideoImager::ResultType 
+OpenIGTLinkVideoImager::ResultType
 OpenIGTLinkVideoImager::InternalUpdateStatus()
 {
   igstkLogMacro( DEBUG,
@@ -306,9 +209,9 @@ OpenIGTLinkVideoImager::InternalUpdateStatus()
     // only report tools that have useful data
     if (! this->m_ToolStatusContainer[inputItr->first])
     {
-      igstkLogMacro( DEBUG, "igstk::OpenIGTLinkVideoImager" 
-      << "::InternalUpdateStatus: tool " 
-      << inputItr->first << " is not in view\n");
+      igstkLogMacro( DEBUG, "igstk::OpenIGTLinkVideoImager"
+      << "::InternalUpdateStatus: tool "
+      << inputItr->first << " is not available\n");
       // report to the imager tool that the imager is not available
       this->ReportImagingToolNotAvailable(
         imagerToolContainer[inputItr->first]);
@@ -335,13 +238,13 @@ OpenIGTLinkVideoImager::InternalUpdateStatus()
   return SUCCESS;
 }
 
-/** Update the shared memory buffer and the tool's internal frame. 
- * This function is called by the thread that communicates with the imager 
+/** Update the shared memory buffer and the tool's internal frame.
+ * This function is called by the thread that communicates with the imager
  * while the imager is in the Imaging state. */
 OpenIGTLinkVideoImager::ResultType
 OpenIGTLinkVideoImager::InternalThreadedUpdateStatus( void )
 {
-  igstkLogMacro( DEBUG, 
+  igstkLogMacro( DEBUG,
     "igstk::OpenIGTLinkVideoImager::InternalThreadedUpdateStatus called ...\n");
 
   igstkLogMacro( DEBUG, "InternalThreadedUpdateStatus entered" );
@@ -367,7 +270,7 @@ OpenIGTLinkVideoImager::InternalThreadedUpdateStatus( void )
     m_HeaderMsg->InitPack();
     // Receive generic header from the socket
     int r = 0;
-    r = this->m_Socket->Receive(m_HeaderMsg->GetPackPointer(), 
+    r = this->m_Socket->Receive(m_HeaderMsg->GetPackPointer(),
                                                     m_HeaderMsg->GetPackSize());
     if (r != m_HeaderMsg->GetPackSize())
       {
@@ -375,6 +278,7 @@ OpenIGTLinkVideoImager::InternalThreadedUpdateStatus( void )
       m_BufferLock->Unlock();
       return FAILURE;
       }
+
     igstkLogMacro( DEBUG, "InternalThreadedUpdateStatus Receive passed" );
     // Deserialize the header
     m_HeaderMsg->Unpack();
@@ -384,15 +288,12 @@ OpenIGTLinkVideoImager::InternalThreadedUpdateStatus( void )
       // Create a message buffer to receive transform data
       igtl::ImageMessage::Pointer imgMsg;
       imgMsg = igtl::ImageMessage::New();
-      // if (m_FirstFrame)
-      // {
+
       imgMsg->SetMessageHeader(m_HeaderMsg);
       imgMsg->AllocatePack();
-      m_FirstFrame = false;
-      // }
 
       // Receive transform data from the socket
-      this->m_Socket->Receive(imgMsg->GetPackBodyPointer(), 
+      this->m_Socket->Receive(imgMsg->GetPackBodyPointer(),
                                                      imgMsg->GetPackBodySize());
 
       // Deserialize the transform data
@@ -408,18 +309,17 @@ OpenIGTLinkVideoImager::InternalThreadedUpdateStatus( void )
 
       // Check if an imager tool was added with this device name
       typedef VideoImagerToolFrameContainerType::iterator InputIterator;
-      //TODO name
       InputIterator deviceItr =
-            this->m_ToolFrameBuffer.find("Camera");//imgMsg->GetDeviceName() );
+            this->m_ToolFrameBuffer.find("Camera");
 
       if( deviceItr != this->m_ToolFrameBuffer.end() )
-        {   
+        {
         // create the frame
         VideoImagerToolsContainerType imagerToolContainer =
         this->GetVideoImagerToolContainer();
-           
+
         FrameType* frame = new FrameType();
-        frame = this->GetVideoImagerToolFrame( 
+        frame = this->GetVideoImagerToolFrame(
                                         imagerToolContainer[deviceItr->first] );
 
         unsigned int frameDims[3];
@@ -436,10 +336,10 @@ OpenIGTLinkVideoImager::InternalThreadedUpdateStatus( void )
 
         memcpy(frame->GetImagePtr(),
         imgMsg->GetScalarPointer(),frameDims[0]*frameDims[1]*frameDims[2]);
-   
+
         //update frame validity time
         frame->SetTimeToExpiration(this->GetValidityTime());
-          
+
         this->m_ToolFrameBuffer[ deviceItr->first ] = frame;
         this->m_ToolStatusContainer[ deviceItr->first ] = 1;
         }
@@ -519,7 +419,6 @@ OpenIGTLinkVideoImager::ValidateSpecifiedFrequency( double frequencyInHz )
     }
   return SUCCESS;
 }
-
 
 /** Print Self function */
 void OpenIGTLinkVideoImager
