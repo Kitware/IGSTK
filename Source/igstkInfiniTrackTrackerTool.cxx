8a0efcf90a5b6160e3b6a25c501085ffe2a70f5d
Andinet Enquobahrie <andinet.enqu@kitware.com>
2009-08-18 12:44:45 -0400

    BUG: 9433 InfiniTrack

---
+++ Source/igstkInfiniTrackTrackerTool.cxx
@@ -0,0 +1,140 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkInfiniTrackTrackerTool.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISC  Insight Software Consortium.  All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#if defined(_MSC_VER)
+//  Warning about: identifier was truncated to '255' characters 
+// in the debug information (MVC6.0 Debug)
+#pragma warning( disable : 4786 )
+#endif
+
+#include "igstkInfiniTrackTrackerTool.h"
+#include "igstkInfiniTrackTracker.h"
+
+namespace igstk
+{
+
+/** Constructor (configures InfiniTrack-specific tool values)  */
+InfiniTrackTrackerTool::InfiniTrackTrackerTool()
+                      : m_StateMachine(this)
+                      , m_bTrackerToolConfigured (false)
+{
+
+  // States
+  igstkAddStateMacro (Idle);
+  igstkAddStateMacro (MarkerNameSpecified);
+
+  // Set the input descriptors
+  igstkAddInputMacro (ValidMarkerName);
+  igstkAddInputMacro (InValidMarkerName);
+
+  // Add transitions
+  //
+  // Transitions from idle state
+  igstkAddTransitionMacro (Idle,
+                           ValidMarkerName,
+                           MarkerNameSpecified,
+                           SetMarkerName);
+
+  igstkAddTransitionMacro (Idle,
+                           InValidMarkerName,
+                           Idle,
+                           ReportInvalidMarkerNameSpecified);
+
+  // Transitions from MarkerName specified
+  igstkAddTransitionMacro (MarkerNameSpecified,
+                           ValidMarkerName,
+                           MarkerNameSpecified,
+                           ReportInvalidRequest);
+
+  igstkAddTransitionMacro (MarkerNameSpecified,
+                           InValidMarkerName,
+                           MarkerNameSpecified,
+                           ReportInvalidRequest);
+
+  // Inputs to the state machine
+  igstkSetInitialStateMacro (Idle);
+
+  this->m_StateMachine.SetReadyToRun ();
+}
+
+/** Destructor */
+InfiniTrackTrackerTool::~InfiniTrackTrackerTool()
+{
+}
+
+
+/** Set tracker tool marker name */
+void InfiniTrackTrackerTool::RequestSetMarkerName (const std::string & markerName)
+{
+  igstkLogMacro(DEBUG, "igstk::InfiniTrackTrackerTool::RequestSetMarkerName called ...\n");
+
+  if (markerName == "")
+  {
+    this->m_StateMachine.PushInput( this->m_InValidMarkerNameInput );
+    this->m_StateMachine.ProcessInputs();
+  } else 
+  {
+    this->m_MarkerNameToBeSet = markerName;
+    this->m_StateMachine.PushInput( this->m_ValidMarkerNameInput );
+    this->m_StateMachine.ProcessInputs();
+  }
+}
+
+/** Print Self function */
+void InfiniTrackTrackerTool::PrintSelf( std::ostream& os, itk::Indent indent ) const
+{
+  Superclass::PrintSelf(os, indent);
+
+  os << indent << "Marker name : "         << this->m_MarkerName << std::endl;
+  os << indent << "TrackerToolConfigured:" << this->m_bTrackerToolConfigured << std::endl;
+}
+
+/** Report invalid marker specified */
+void InfiniTrackTrackerTool::ReportInvalidMarkerNameSpecifiedProcessing ()
+{
+  igstkLogMacro( DEBUG, "igstk::InfiniTrackTrackerTool::ReportInvalidMarkerNameSpecifiedProcessing called ...\n");
+
+  igstkLogMacro( CRITICAL, "Invalid marker specified ");
+}
+
+/** Report any invalid request to the tool */
+void InfiniTrackTrackerTool::ReportInvalidRequestProcessing ()
+{
+  igstkLogMacro (WARNING, "ReportInvalidRequestProcessing() called ...\n");
+}
+
+/** Set the marker processing */
+void InfiniTrackTrackerTool::SetMarkerNameProcessing ()
+{
+  igstkLogMacro (DEBUG, "igstk::InfiniTrackTrackerTool::SetMarkerNameProcessing called ...\n");
+
+  this->m_MarkerName = this->m_MarkerNameToBeSet;
+  this->m_bTrackerToolConfigured = true;
+
+  // For InfiniTrackTracker, marker name is used as a unique identifier
+  SetTrackerToolIdentifier (this->m_MarkerName); 
+}
+
+/** The "CheckIfTrackerToolIsConfigured" method returns true if the tracker tool is configured */
+bool InfiniTrackTrackerTool::CheckIfTrackerToolIsConfigured () const
+{
+  igstkLogMacro (DEBUG, "igstk::InfiniTrackTrackerTool::CheckIfTrackerToolIsConfigured called...\n");
+  return this->m_bTrackerToolConfigured;
+}
+
+}
---
+++ Source/igstkInfiniTrackTrackerTool.h
@@ -0,0 +1,113 @@
+/*=========================================================================
+
+  Program:   Image Guided Surgery Software Toolkit
+  Module:    igstkInfiniTrackTrackerTool.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) ISC  Insight Software Consortium.  All rights reserved.
+  See IGSTKCopyright.txt or http://www.igstk.org/copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef __igstkInfiniTrackTrackerTool_h
+#define __igstkInfiniTrackTrackerTool_h
+
+#ifdef _MSC_VER
+#pragma warning ( disable : 4018 )
+//Warning about: identifier was truncated to '255' characters in the debug
+//information (MVC6.0 Debug)
+#pragma warning( disable : 4284 )
+#endif
+
+#include "igstkTrackerTool.h"
+
+
+namespace igstk
+{
+
+  class igstkInfiniTrackTracker;
+
+/** \class InfiniTrackTrackerTool
+  * \brief A InfiniTrack-specific TrackerTool class.
+  *
+  * This class is a for providing InfiniTrack-specific functionality
+  * for TrackerTools, and also to allow compile-time type enforcement
+  * for other classes and functions that specifically require
+  * an InfiniTrackTrackertool.
+  *
+  * \sa http://www.atracsys.com/
+  *
+  * \ingroup Tracker
+  *
+  */
+  
+  class InfiniTrackTrackerTool : public TrackerTool
+  {
+  
+    public:
+
+      /** Macro with standard traits declarations. */
+      igstkStandardClassTraitsMacro (InfiniTrackTrackerTool, TrackerTool )
+
+      /** Get Tracker tool marker name */
+      igstkGetStringMacro (MarkerName);
+
+      /** Set tracker tool marker name */
+      void RequestSetMarkerName (const std::string & markerName); 
+
+    protected:
+
+      /** Constructor (configures InfiniTrack-specific tool values)  */
+      InfiniTrackTrackerTool();
+
+      /** Destructor */
+      virtual ~InfiniTrackTrackerTool();
+
+      /** Print object information */
+      virtual void PrintSelf( std::ostream& os, ::itk::Indent indent ) const;
+
+    private:
+      InfiniTrackTrackerTool(const Self&);   //purposely not implemented
+      void operator=(const Self&);       //purposely not implemented
+
+      /** Get boolean variable to check if the tracker tool is 
+          configured or not */
+      virtual bool CheckIfTrackerToolIsConfigured () const;
+
+      /** Report invalid marker specified */
+      void ReportInvalidMarkerNameSpecifiedProcessing ();
+
+      /** Report any invalid request to the tool */
+      void ReportInvalidRequestProcessing ();
+
+      /** Set the marker processing */
+      void SetMarkerNameProcessing ();
+
+      /** States for the State Machine */
+      igstkDeclareStateMacro (Idle);
+      igstkDeclareStateMacro (MarkerNameSpecified);
+
+      /** Inputs to the State Machine */
+      igstkDeclareInputMacro (ValidMarkerName);
+      igstkDeclareInputMacro (InValidMarkerName);
+
+      /** The marker */
+      std::string m_MarkerName;
+      std::string m_MarkerNameToBeSet;
+
+      /** boolean to indicate if the tracker tool is configured */
+      bool m_bTrackerToolConfigured;
+
+};  
+
+} // namespace igstk
+
+
+#endif  // __igstk_InfiniTrackTrackerTool_h_
