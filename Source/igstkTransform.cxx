fb5bb9be8dff06b3e86e27dcede53bb1cae2a8af
Patrick Cheng <cheng@isis.georgetown.edu>
2010-11-17 13:13:30 -0500

    BUG: 0011457. Found a logic bug while fixing unused parameter warning

--- Source/igstkTransform.cxx
+++ Source/igstkTransform.cxx
@@ -175,7 +175,7 @@ bool
 Transform
 ::IsNumericallyEquivalent( const Transform& inputTransform, double tol ) const
 {
-  Transform shouldBeIdentity = TransformCompose( *this, this->GetInverse() );
+  Transform shouldBeIdentity = TransformCompose( inputTransform, this->GetInverse() );
   bool isEquivalent = shouldBeIdentity.IsIdentity( tol );
   return isEquivalent;
 }
